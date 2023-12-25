567787154e84848221db29ddedf35e1bc6152425
MirageFairy <idolm@ster>
2023-12-26 01:54:46 +0900

    internal: add: registerColorProvider

--- src/main/kotlin/miragefairy2024/ClientProxy.kt
+++ src/main/kotlin/miragefairy2024/ClientProxy.kt
@@ -1,12 +1,29 @@
 package miragefairy2024
 
 import net.minecraft.block.Block
+import net.minecraft.block.BlockState
 import net.minecraft.entity.player.PlayerEntity
+import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
 import net.minecraft.text.Text
+import net.minecraft.util.math.BlockPos
+import net.minecraft.world.BlockView
 
 interface ClientProxy {
     fun registerItemTooltipCallback(block: (stack: ItemStack, lines: MutableList<Text>) -> Unit)
     fun registerCutoutRenderLayer(block: Block)
     fun getClientPlayer(): PlayerEntity?
+    fun getBlockColorProvider(block: Block): BlockColorProvider?
+    fun registerBlockColorProvider(block: Block, provider: BlockColorProvider)
+    fun getFoliageBlockColorProvider(): BlockColorProvider
+    fun getItemColorProvider(item: Item): ItemColorProvider?
+    fun registerItemColorProvider(item: Item, provider: ItemColorProvider)
+}
+
+fun interface BlockColorProvider {
+    operator fun invoke(blockState: BlockState, world: BlockView?, blockPos: BlockPos?, tintIndex: Int): Int
+}
+
+fun interface ItemColorProvider {
+    operator fun invoke(itemStack: ItemStack, tintIndex: Int): Int
 }
--- src/main/kotlin/miragefairy2024/util/Rendering.kt
+++ src/main/kotlin/miragefairy2024/util/Rendering.kt
@@ -1,10 +1,32 @@
 package miragefairy2024.util
 
+import miragefairy2024.BlockColorProvider
+import miragefairy2024.ItemColorProvider
 import miragefairy2024.MirageFairy2024
+import mirrg.kotlin.hydrogen.castOrThrow
 import net.minecraft.block.Block
+import net.minecraft.item.BlockItem
+import net.minecraft.item.Item
 
 fun Block.registerCutoutRenderLayer() {
     MirageFairy2024.onClientInit += {
         it.registerCutoutRenderLayer(this)
     }
+}
+
+fun Block.registerColorProvider(provider: BlockColorProvider) = MirageFairy2024.onClientInit.plusAssign {
+    it.registerBlockColorProvider(this, provider)
+}
+
+fun Block.registerFoliageColorProvider() = this.registerColorProvider { blockState, world, blockPos, tintIndex ->
+    MirageFairy2024.clientProxy!!.getFoliageBlockColorProvider().invoke(blockState, world, blockPos, tintIndex)
+}
+
+fun Item.registerColorProvider(provider: ItemColorProvider) = MirageFairy2024.onClientInit.plusAssign {
+    it.registerItemColorProvider(this, provider)
+}
+
+fun BlockItem.registerRedirectColorProvider() = this.registerColorProvider { itemStack, tintIndex ->
+    val block = itemStack.item.castOrThrow<BlockItem>().block
+    MirageFairy2024.clientProxy!!.getBlockColorProvider(block)!!.invoke(block.defaultState, null, null, tintIndex)
 }
