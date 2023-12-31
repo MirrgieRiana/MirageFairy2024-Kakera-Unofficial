af7ba7c7355e900a69db811eb58c2a7c6def77b6
MirageFairy <idolm@ster>
2023-12-31 12:50:46 +0900

    internal: add: register*RecipeGeneration

--- src/main/kotlin/miragefairy2024/util/Recipe.kt
+++ src/main/kotlin/miragefairy2024/util/Recipe.kt
@@ -1,5 +1,6 @@
 package miragefairy2024.util
 
+import miragefairy2024.MirageFairy2024DataGenerator
 import miragefairy2024.mod.recipeGroupRegistry
 import net.fabricmc.fabric.api.loot.v2.LootTableEvents
 import net.fabricmc.fabric.api.registry.FuelRegistry
@@ -7,6 +8,8 @@ import net.minecraft.block.Blocks
 import net.minecraft.block.ComposterBlock
 import net.minecraft.data.server.recipe.CraftingRecipeJsonBuilder
 import net.minecraft.data.server.recipe.RecipeProvider
+import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder
+import net.minecraft.data.server.recipe.ShapelessRecipeJsonBuilder
 import net.minecraft.enchantment.Enchantments
 import net.minecraft.item.Item
 import net.minecraft.item.Items
@@ -17,16 +20,68 @@ import net.minecraft.loot.function.ApplyBonusLootFunction
 import net.minecraft.loot.function.ExplosionDecayLootFunction
 import net.minecraft.predicate.entity.LocationPredicate
 import net.minecraft.predicate.item.ItemPredicate
+import net.minecraft.recipe.book.RecipeCategory
 import net.minecraft.registry.RegistryKey
 import net.minecraft.registry.tag.TagKey
+import net.minecraft.util.Identifier
 import net.minecraft.world.biome.Biome
+
+// Crafting
 
 fun <T : CraftingRecipeJsonBuilder> T.criterion(item: Item) = this.also { it.criterion("has_${item.getIdentifier().path}", RecipeProvider.conditionsFromItem(item)) }
 fun <T : CraftingRecipeJsonBuilder> T.criterion(tagKey: TagKey<Item>) = this.also { it.criterion("has_${tagKey.id.path}", RecipeProvider.conditionsFromTag(tagKey)) }
 fun <T : CraftingRecipeJsonBuilder> T.group(item: Item) = this.also { it.group(recipeGroupRegistry[item] ?: "${item.getIdentifier()}") }
 
+class RecipeGenerationSettings<T> {
+    val listeners = mutableListOf<(T) -> Unit>()
+    val idModifiers = mutableListOf<(Identifier) -> Identifier>()
+    var recipeCategory = RecipeCategory.MISC
+}
 
-// Init
+infix fun <T : CraftingRecipeJsonBuilder> RecipeGenerationSettings<T>.on(item: Item): RecipeGenerationSettings<T> {
+    listeners += { it.criterion(item) }
+    return this
+}
+
+infix fun <T : CraftingRecipeJsonBuilder> RecipeGenerationSettings<T>.from(item: Item): RecipeGenerationSettings<T> {
+    idModifiers += { it concat "_from_" concat item.getIdentifier().path }
+    return this
+}
+
+fun <T : CraftingRecipeJsonBuilder> registerRecipeGeneration(
+    creator: (RecipeCategory, Item, Int) -> T,
+    item: Item,
+    count: Int = 1,
+    block: T.() -> Unit = {},
+): RecipeGenerationSettings<T> {
+    val settings = RecipeGenerationSettings<T>()
+    MirageFairy2024DataGenerator.recipeGenerators {
+        val builder = creator(settings.recipeCategory, item, count)
+        builder.group(item)
+        settings.listeners.forEach { listener ->
+            listener(builder)
+        }
+        block(builder)
+        val identifier = settings.idModifiers.fold(item.getIdentifier()) { id, idModifier -> idModifier(id) }
+        builder.offerTo(it, identifier)
+    }
+    return settings
+}
+
+fun registerShapedRecipeGeneration(
+    item: Item,
+    count: Int = 1,
+    block: ShapedRecipeJsonBuilder.() -> Unit = {},
+): RecipeGenerationSettings<ShapedRecipeJsonBuilder> = registerRecipeGeneration(ShapedRecipeJsonBuilder::create, item, count, block)
+
+fun registerShapelessRecipeGeneration(
+    item: Item,
+    count: Int = 1,
+    block: ShapelessRecipeJsonBuilder.() -> Unit = {},
+): RecipeGenerationSettings<ShapelessRecipeJsonBuilder> = registerRecipeGeneration(ShapelessRecipeJsonBuilder::create, item, count, block)
+
+
+// Others
 
 fun Item.registerGrassDrop(amount: Float = 1.0F, biome: (() -> RegistryKey<Biome>)? = null) {
     LootTableEvents.MODIFY.register { _, _, id, tableBuilder, source ->
