9b0377c718696adaae3aa7fde3962b4344035334
MirageFairy <idolm@ster>
2024-01-08 18:54:29 +0900

    internal: ItemGroup: add

--- src/main/kotlin/miragefairy2024/util/ItemGroup.kt
+++ src/main/kotlin/miragefairy2024/util/ItemGroup.kt
@@ -3,10 +3,19 @@ package miragefairy2024.util
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents
 import net.minecraft.item.Item
 import net.minecraft.item.ItemGroup
+import net.minecraft.item.ItemStack
 import net.minecraft.registry.RegistryKey
 
 fun Item.registerItemGroup(itemGroup: RegistryKey<ItemGroup>) {
     ItemGroupEvents.modifyEntriesEvent(itemGroup).register {
         it.add(this)
+    }
+}
+
+fun Item.registerItemGroup(itemGroup: RegistryKey<ItemGroup>, supplier: () -> List<ItemStack>) {
+    ItemGroupEvents.modifyEntriesEvent(itemGroup).register {
+        supplier().forEach { itemStack ->
+            it.add(itemStack)
+        }
     }
 }
