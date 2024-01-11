bbe7339a3a65a9b7c830591443cd1f501b476ad5
MirageFairy <idolm@ster>
2024-01-11 22:57:47 +0900

    internal: BlockMaterialsModule: 機能追加

--- src/main/kotlin/miragefairy2024/mod/BlockMaterialsModule.kt
+++ src/main/kotlin/miragefairy2024/mod/BlockMaterialsModule.kt
@@ -20,6 +20,7 @@ import net.minecraft.item.BlockItem
 import net.minecraft.item.Item
 import net.minecraft.registry.Registries
 import net.minecraft.registry.tag.BlockTags
+import net.minecraft.registry.tag.TagKey
 import net.minecraft.util.Identifier
 
 enum class BlockMaterialCard(
@@ -28,20 +29,26 @@ enum class BlockMaterialCard(
     val jaName: String,
     val poemList: PoemList,
     mapColor: MapColor,
+    hardness: Float,
+    resistance: Float,
+    requiresTool: Boolean,
+    val mineableTags: List<TagKey<Block>>,
 ) {
     MIRANAGITE_BLOCK(
         "miranagite_block", "Miranagite Block", "蒼天石ブロック",
         PoemList(1).poem("Passivation confines discontinuous space", "虚空に導かれし、霊界との接合点。"),
-        MapColor.LAPIS_BLUE,
-    )
+        MapColor.LAPIS_BLUE, 3.0F, 3.0F, requiresTool = true, listOf(BlockTags.PICKAXE_MINEABLE, BlockTags.NEEDS_STONE_TOOL),
+    ),
     ;
 
     val identifier = Identifier(MirageFairy2024.modId, path)
-    val block = AbstractBlock.Settings.create()
-        .mapColor(mapColor)
-        .requiresTool()
-        .strength(3.0F, 3.0F)
-        .let { Block(it) }
+    val block = run {
+        val settings = AbstractBlock.Settings.create()
+        settings.mapColor(mapColor)
+        if (requiresTool) settings.requiresTool()
+        settings.strength(hardness, resistance)
+        Block(settings)
+    }
     val item = BlockItem(block, Item.Settings())
 }
 
@@ -61,8 +68,9 @@ fun initBlockMaterialsModule() {
 
         card.block.registerDefaultLootTableGeneration()
 
-        card.block.registerTagGeneration(BlockTags.PICKAXE_MINEABLE)
-        card.block.registerTagGeneration(BlockTags.NEEDS_STONE_TOOL)
+        card.mineableTags.forEach {
+            card.block.registerTagGeneration(it)
+        }
     }
 
     registerShapedRecipeGeneration(BlockMaterialCard.MIRANAGITE_BLOCK.item) {
