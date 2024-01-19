11fc5b4f50c128911dfdc30ebb1fe19ab28411ad
MirageFairy <idolm@ster>
2024-01-19 23:50:03 +0900

    CREATIVE: add: 8 fairies

--- src/main/kotlin/miragefairy2024/mod/fairy/Motif.kt
+++ src/main/kotlin/miragefairy2024/mod/fairy/Motif.kt
@@ -38,6 +38,30 @@ enum class MotifCard(
     AIR(
         "air", 0, "Airia", "空気精アイリャ", 0xFFBE80, 0xDEFFFF, 0xDEFFFF, 0xB0FFFF,
     ),
+    LIGHT(
+        "light", 3, "Lightia", "光精リグチャ", 0xFFFFD8, 0xFFFFD8, 0xFFFFC5, 0xFFFF00,
+    ),
+    SUN(
+        "sun", 10, "Sunia", "太陽精スーニャ", 0xff2f00, 0xff972b, 0xff7500, 0xffe7b2,
+    ),
+    FIRE(
+        "fire", 2, "Firia", "火精フィーリャ", 0xFF6C01, 0xF9DFA4, 0xFF7324, 0xFF4000,
+    ),
+    WATER(
+        "water", 1, "Wateria", "水精ワテーリャ", 0x5469F2, 0x5985FF, 0x172AD3, 0x2D40F4,
+    ),
+    DIRT(
+        "dirt", 1, "Dirtia", "土精ディルチャ", 0xB87440, 0xB9855C, 0x593D29, 0x914A18,
+    ),
+    ENDERMAN(
+        "enderman", 6, "Endermania", "終界人精エンデルマーニャ", 0x000000, 0x161616, 0x161616, 0xEF84FA,
+    ),
+    CARROT(
+        "carrot", 4, "Carrotia", "人参精ツァッローチャ", 0xF98D10, 0xFD7F11, 0xE3710F, 0x248420,
+    ),
+    CAKE(
+        "cake", 4, "Cakia", "蛋麭精ツァーキャ", 0xCC850C, 0xF5F0DC, 0xD3D0BF, 0xDE3334,
+    ),
     ;
 
     val identifier = Identifier(MirageFairy2024.modId, path)
