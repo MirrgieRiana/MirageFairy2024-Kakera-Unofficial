562f2916bd6e0e2625fdaa8cd208a72dee5f29c0
MirageFairy <idolm@ster>
2024-01-20 02:39:59 +0900

    refactor: Motif

--- src/main/kotlin/miragefairy2024/mod/fairy/Motif.kt
+++ src/main/kotlin/miragefairy2024/mod/fairy/Motif.kt
@@ -41,35 +41,35 @@ enum class MotifCard(
 ) : Motif {
     AIR(
         "air", 0, "Airia", "空気精アイリャ", 0xFFBE80, 0xDEFFFF, 0xDEFFFF, 0xB0FFFF,
-        Recipes().common(),
+        Recipes().always,
     ),
     LIGHT(
         "light", 3, "Lightia", "光精リグチャ", 0xFFFFD8, 0xFFFFD8, 0xFFFFC5, 0xFFFF00,
-        Recipes().common(ConventionalBiomeTags.IN_OVERWORLD),
+        Recipes().overworld,
     ),
     SUN(
         "sun", 10, "Sunia", "太陽精スーニャ", 0xff2f00, 0xff972b, 0xff7500, 0xffe7b2,
-        Recipes().common(ConventionalBiomeTags.IN_OVERWORLD),
+        Recipes().overworld,
     ),
     FIRE(
         "fire", 2, "Firia", "火精フィーリャ", 0xFF6C01, 0xF9DFA4, 0xFF7324, 0xFF4000,
-        Recipes().common(ConventionalBiomeTags.IN_NETHER),
+        Recipes().nether,
     ),
     WATER(
         "water", 1, "Wateria", "水精ワテーリャ", 0x5469F2, 0x5985FF, 0x172AD3, 0x2D40F4,
-        Recipes().common(ConventionalBiomeTags.IN_OVERWORLD),
+        Recipes().overworld,
     ),
     DIRT(
         "dirt", 1, "Dirtia", "土精ディルチャ", 0xB87440, 0xB9855C, 0x593D29, 0x914A18,
-        Recipes().common(ConventionalBiomeTags.IN_OVERWORLD),
+        Recipes().overworld,
     ),
     ENDERMAN(
         "enderman", 6, "Endermania", "終界人精エンデルマーニャ", 0x000000, 0x161616, 0x161616, 0xEF84FA,
-        Recipes().common(ConventionalBiomeTags.IN_OVERWORLD).common(ConventionalBiomeTags.IN_NETHER).common(ConventionalBiomeTags.IN_THE_END),
+        Recipes().overworld.nether.end,
     ),
     CARROT(
         "carrot", 4, "Carrotia", "人参精ツァッローチャ", 0xF98D10, 0xFD7F11, 0xE3710F, 0x248420,
-        Recipes().common(ConventionalBiomeTags.IN_OVERWORLD),
+        Recipes().overworld,
     ),
     CAKE(
         "cake", 4, "Cakia", "蛋麭精ツァーキャ", 0xCC850C, 0xF5F0DC, 0xD3D0BF, 0xDE3334,
@@ -94,6 +94,11 @@ enum class MotifCard(
 private fun MotifCard.Recipes.common(biome: TagKey<Biome>? = null) = this.onInit { card ->
     COMMON_MOTIF_RECIPES += CommonMotifRecipe(card, biome)
 }
+
+private val MotifCard.Recipes.always get() = this.common()
+private val MotifCard.Recipes.overworld get() = this.common(ConventionalBiomeTags.IN_OVERWORLD)
+private val MotifCard.Recipes.nether get() = this.common(ConventionalBiomeTags.IN_NETHER)
+private val MotifCard.Recipes.end get() = this.common(ConventionalBiomeTags.IN_THE_END)
 
 fun initMotif() {
     MotifCard.entries.forEach { card ->
