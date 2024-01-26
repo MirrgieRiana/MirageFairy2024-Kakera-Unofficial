0bccadd03cd4fa99985f72df1049e6c832e55780
MirageFairy <idolm@ster>
2024-01-26 22:56:23 +0900

    internal: add: Channel

---
+++ src/client/kotlin/miragefairy2024/client/util/Channel.kt
@@ -0,0 +1,20 @@
+package miragefairy2024.client.util
+
+import miragefairy2024.util.Channel
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking
+import net.fabricmc.fabric.api.networking.v1.PacketByteBufs
+
+fun <P> Channel<P>.sendToServer(packet: P) {
+    val buf = PacketByteBufs.create()
+    this.writeToBuf(buf, packet)
+    ClientPlayNetworking.send(packetId, buf)
+}
+
+fun <P> Channel<P>.registerClientPacketReceiver(handler: (P) -> Unit) {
+    ClientPlayNetworking.registerGlobalReceiver(this.packetId) { client, _, buf, _ ->
+        val data = this.readFromBuf(buf)
+        client.execute {
+            handler(data)
+        }
+    }
+}
---
+++ src/main/kotlin/miragefairy2024/util/Channel.kt
@@ -0,0 +1,27 @@
+package miragefairy2024.util
+
+import net.fabricmc.fabric.api.networking.v1.PacketByteBufs
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking
+import net.minecraft.network.PacketByteBuf
+import net.minecraft.server.network.ServerPlayerEntity
+import net.minecraft.util.Identifier
+
+abstract class Channel<P>(val packetId: Identifier) {
+    abstract fun writeToBuf(buf: PacketByteBuf, packet: P)
+    abstract fun readFromBuf(buf: PacketByteBuf): P
+}
+
+fun <P> Channel<P>.sendToClient(player: ServerPlayerEntity, packet: P) {
+    val buf = PacketByteBufs.create()
+    this.writeToBuf(buf, packet)
+    ServerPlayNetworking.send(player, this.packetId, buf)
+}
+
+fun <P> Channel<P>.registerServerPacketReceiver(handler: (ServerPlayerEntity, P) -> Unit) {
+    ServerPlayNetworking.registerGlobalReceiver(this.packetId) { server, player, _, buf, _ ->
+        val data = this.readFromBuf(buf)
+        server.execute {
+            handler(player, data)
+        }
+    }
+}
