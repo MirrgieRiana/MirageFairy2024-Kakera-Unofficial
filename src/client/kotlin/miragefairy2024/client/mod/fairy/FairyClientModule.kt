ca8956861f8e78e2e5956354c3c68b3b98b399fa
MirageFairy <idolm@ster>
2024-01-27 00:45:06 +0900

    internal: add: GainFairyDreamChannel, toast

--- src/client/kotlin/miragefairy2024/client/mod/fairy/FairyClientModule.kt
+++ src/client/kotlin/miragefairy2024/client/mod/fairy/FairyClientModule.kt
@@ -1,8 +1,43 @@
 package miragefairy2024.client.mod.fairy
 
+import io.wispforest.owo.ui.component.Components
+import io.wispforest.owo.ui.container.Containers
+import io.wispforest.owo.ui.core.Insets
+import io.wispforest.owo.ui.core.Sizing
+import io.wispforest.owo.ui.core.Surface
+import io.wispforest.owo.ui.core.VerticalAlignment
+import miragefairy2024.MirageFairy2024
+import miragefairy2024.client.util.createOwoToast
+import miragefairy2024.client.util.horizontalSpace
+import miragefairy2024.client.util.registerClientPacketReceiver
+import miragefairy2024.mod.fairy.FairyCard
+import miragefairy2024.mod.fairy.GAIN_FAIRY_DREAM_TRANSLATION
+import miragefairy2024.mod.fairy.GainFairyDreamChannel
 import miragefairy2024.mod.fairy.motifTableScreenHandlerType
+import miragefairy2024.mod.fairy.setFairyMotif
+import miragefairy2024.util.black
+import miragefairy2024.util.createItemStack
+import miragefairy2024.util.invoke
+import net.minecraft.client.MinecraftClient
 import net.minecraft.client.gui.screen.ingame.HandledScreens
+import net.minecraft.util.Identifier
 
 fun initFairyClientModule() {
+
+    // GUI登録
     HandledScreens.register(motifTableScreenHandlerType) { gui, inventory, title -> MotifTableScreen(gui, inventory, title) }
+
+    // パケットハンドラ登録
+    GainFairyDreamChannel.registerClientPacketReceiver { motif ->
+        val component = Containers.horizontalFlow(Sizing.fixed(160), Sizing.fixed(32)).apply {
+            surface(Surface.tiled(Identifier(MirageFairy2024.modId, "textures/gui/fairy_dream_toast.png"), 160, 32))
+            padding(Insets.of(8))
+            verticalAlignment(VerticalAlignment.CENTER)
+            child(Components.item(FairyCard.item.createItemStack().also { it.setFairyMotif(motif) }))
+            child(horizontalSpace(6))
+            child(Components.label(GAIN_FAIRY_DREAM_TRANSLATION().black))
+        }
+        MinecraftClient.getInstance().toastManager.add(createOwoToast(component))
+    }
+
 }
--- src/main/kotlin/miragefairy2024/mod/fairy/FairyDream.kt
+++ src/main/kotlin/miragefairy2024/mod/fairy/FairyDream.kt
@@ -2,11 +2,17 @@ package miragefairy2024.mod.fairy
 
 import miragefairy2024.MirageFairy2024
 import miragefairy2024.mod.extraPlayerDataCategoryRegistry
+import miragefairy2024.util.Translation
+import miragefairy2024.util.enJa
 import miragefairy2024.util.register
 import net.minecraft.util.Identifier
+
+val GAIN_FAIRY_DREAM_TRANSLATION = Translation({ "gui.miragefairy2024.fairy_dream.gain" }, "Dreamed of a new fairy!", "新たな妖精の夢を見た！")
 
 fun initFairyDream() {
 
     FairyDreamContainerExtraPlayerDataCategory.register(extraPlayerDataCategoryRegistry, Identifier(MirageFairy2024.modId, "fairy_dream"))
+
+    GAIN_FAIRY_DREAM_TRANSLATION.enJa()
 
 }
