c3dde4d86a1bc17098c0a2a5fc03a46c3bd7fdae
MirageFairy <idolm@ster>
2024-01-27 02:11:14 +0900

    CREATIVE: add: debug_clear_fairy_dream, debug_gain_fairy_dream

--- src/main/kotlin/miragefairy2024/mod/fairy/FairyDream.kt
+++ src/main/kotlin/miragefairy2024/mod/fairy/FairyDream.kt
@@ -4,7 +4,13 @@ import miragefairy2024.MirageFairy2024
 import miragefairy2024.mod.extraPlayerDataCategoryRegistry
 import miragefairy2024.util.Translation
 import miragefairy2024.util.enJa
+import miragefairy2024.util.opposite
 import miragefairy2024.util.register
+import miragefairy2024.util.registerDebugItem
+import miragefairy2024.util.sendToClient
+import miragefairy2024.util.text
+import net.minecraft.item.Items
+import net.minecraft.server.network.ServerPlayerEntity
 import net.minecraft.util.Identifier
 
 val GAIN_FAIRY_DREAM_TRANSLATION = Translation({ "gui.miragefairy2024.fairy_dream.gain" }, "Dreamed of a new fairy!", "新たな妖精の夢を見た！")
@@ -14,5 +20,27 @@ fun initFairyDream() {
     FairyDreamContainerExtraPlayerDataCategory.register(extraPlayerDataCategoryRegistry, Identifier(MirageFairy2024.modId, "fairy_dream"))
 
     GAIN_FAIRY_DREAM_TRANSLATION.enJa()
+
+    registerDebugItem("debug_clear_fairy_dream", Items.STRING, 0x0000DD) { world, player, _, _ ->
+        if (world.isClient) return@registerDebugItem
+        player.fairyDreamContainer.clear()
+        player.sendMessage(text { "Cleared fairy dream"() }, true)
+    }
+
+    registerDebugItem("debug_gain_fairy_dream", Items.STRING, 0x0000FF) { world, player, hand, _ ->
+        if (world.isClient) return@registerDebugItem
+        if (player !is ServerPlayerEntity) return@registerDebugItem
+
+        val fairyItemStack = player.getStackInHand(hand.opposite)
+        if (!fairyItemStack.isOf(FairyCard.item)) return@registerDebugItem
+        val motif = fairyItemStack.getFairyMotif() ?: return@registerDebugItem
+
+        if (!player.isSneaking) {
+            player.sendMessage(text { "${player.fairyDreamContainer[motif]}"() })
+        } else {
+            player.fairyDreamContainer[motif] = true
+            GainFairyDreamChannel.sendToClient(player, motif)
+        }
+    }
 
 }
