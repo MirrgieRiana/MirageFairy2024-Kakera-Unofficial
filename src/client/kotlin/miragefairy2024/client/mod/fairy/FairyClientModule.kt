5a584a6d42e1ff695b6009ab44e4912633b2b4e0
MirageFairy <idolm@ster>
2024-01-27 22:54:06 +0900

    VIEW: FairyClientModule

--- src/client/kotlin/miragefairy2024/client/mod/fairy/FairyClientModule.kt
+++ src/client/kotlin/miragefairy2024/client/mod/fairy/FairyClientModule.kt
@@ -7,9 +7,11 @@ import io.wispforest.owo.ui.core.Sizing
 import io.wispforest.owo.ui.core.Surface
 import io.wispforest.owo.ui.core.VerticalAlignment
 import miragefairy2024.MirageFairy2024
+import miragefairy2024.client.util.LimitedLabelComponent
 import miragefairy2024.client.util.createOwoToast
 import miragefairy2024.client.util.horizontalSpace
 import miragefairy2024.client.util.registerClientPacketReceiver
+import miragefairy2024.client.util.verticalSpace
 import miragefairy2024.mod.fairy.FairyCard
 import miragefairy2024.mod.fairy.GAIN_FAIRY_DREAM_TRANSLATION
 import miragefairy2024.mod.fairy.GainFairyDreamChannel
@@ -17,6 +19,7 @@ import miragefairy2024.mod.fairy.motifTableScreenHandlerType
 import miragefairy2024.mod.fairy.setFairyMotif
 import miragefairy2024.util.black
 import miragefairy2024.util.createItemStack
+import miragefairy2024.util.darkBlue
 import miragefairy2024.util.invoke
 import net.minecraft.client.MinecraftClient
 import net.minecraft.client.gui.screen.ingame.HandledScreens
@@ -29,13 +32,18 @@ fun initFairyClientModule() {
 
     // パケットハンドラ登録
     GainFairyDreamChannel.registerClientPacketReceiver { motif ->
+        val itemStack = FairyCard.item.createItemStack().also { it.setFairyMotif(motif) }
         val component = Containers.horizontalFlow(Sizing.fixed(160), Sizing.fixed(32)).apply {
             surface(Surface.tiled(Identifier(MirageFairy2024.modId, "textures/gui/fairy_dream_toast.png"), 160, 32))
-            padding(Insets.of(8))
+            padding(Insets.of(0, 0, 8, 8))
             verticalAlignment(VerticalAlignment.CENTER)
-            child(Components.item(FairyCard.item.createItemStack().also { it.setFairyMotif(motif) }))
+            child(Components.item(itemStack))
             child(horizontalSpace(6))
-            child(Components.label(GAIN_FAIRY_DREAM_TRANSLATION().black))
+            child(Containers.verticalFlow(Sizing.content(), Sizing.content()).apply {
+                child(Components.label(GAIN_FAIRY_DREAM_TRANSLATION().black))
+                child(verticalSpace(2))
+                child(LimitedLabelComponent(itemStack.name.darkBlue).horizontalSizing(Sizing.fixed(160 - 8 - 16 - 6 - 10 - 8)).margins(Insets.of(0, 0, 4, 0)))
+            })
         }
         MinecraftClient.getInstance().toastManager.add(createOwoToast(component))
     }
