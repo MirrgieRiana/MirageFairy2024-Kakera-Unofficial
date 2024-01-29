0c9841bdd22bb9df16c3bac8fc7d7f214d1b1628
MirageFairy <idolm@ster>
2024-01-30 02:41:06 +0900

    refactor: ItemRendererMixin

--- src/client/java/miragefairy2024/mixin/client/ItemRendererMixin.java
+++ src/client/java/miragefairy2024/mixin/client/ItemRendererMixin.java
@@ -15,7 +15,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ItemRenderer.class)
 public abstract class ItemRendererMixin {
     @Inject(method = "renderItem(Lnet/minecraft/item/ItemStack;Lnet/minecraft/client/render/model/json/ModelTransformationMode;ZLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;IILnet/minecraft/client/render/model/BakedModel;)V", at = @At(value = "HEAD"))
-    public void renderItem(ItemStack stack, ModelTransformationMode renderMode, boolean leftHanded, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay, BakedModel model, CallbackInfo ci) {
+    private void renderItem(ItemStack stack, ModelTransformationMode renderMode, boolean leftHanded, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay, BakedModel model, CallbackInfo ci) {
         RenderItemHandler.Companion.getINSTANCE().renderItem(stack, renderMode, leftHanded, matrices, vertexConsumers, light, overlay, model);
     }
 }
