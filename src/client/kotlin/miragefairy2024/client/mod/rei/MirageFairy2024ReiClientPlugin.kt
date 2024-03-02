173ee030d5a5f9469711f2c9f05c6368af3deb44
MirageFairy <idolm@ster>
2024-03-02 17:25:39 +0900

    refactor: move: ClientReiCategoryCard

--- src/client/kotlin/miragefairy2024/client/mod/rei/MirageFairy2024ReiClientPlugin.kt
+++ src/client/kotlin/miragefairy2024/client/mod/rei/MirageFairy2024ReiClientPlugin.kt
@@ -2,24 +2,7 @@ package miragefairy2024.client.mod.rei
 
 import me.shedaniel.rei.api.client.plugins.REIClientPlugin
 import me.shedaniel.rei.api.client.registry.category.CategoryRegistry
-import me.shedaniel.rei.api.client.registry.display.DisplayCategory
 import me.shedaniel.rei.api.client.registry.display.DisplayRegistry
-import me.shedaniel.rei.api.common.display.basic.BasicDisplay
-import miragefairy2024.mod.rei.ReiCategoryCard
-
-abstract class ClientReiCategoryCard<D : BasicDisplay>(val parent: ReiCategoryCard<D>) {
-    companion object {
-        val entries = listOf(
-            WorldGenTraitClientReiCategoryCard,
-            MagicPlantCropClientReiCategoryCard,
-            FairyQuestRecipeClientReiCategoryCard,
-            CommonMotifRecipeClientReiCategoryCard,
-        )
-    }
-
-    abstract fun registerDisplays(registry: DisplayRegistry)
-    abstract fun createCategory(): DisplayCategory<D>
-}
 
 @Suppress("unused")
 class MirageFairy2024ReiClientPlugin : REIClientPlugin {
