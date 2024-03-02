be4a155c8e83f3f969b43e9fb068df832de2375d
MirageFairy <idolm@ster>
2024-03-02 15:02:54 +0900

    internal: PassiveSkillEffectCard: add: MANA

--- src/main/kotlin/miragefairy2024/mod/passiveskill/PassiveSkillEffects.kt
+++ src/main/kotlin/miragefairy2024/mod/passiveskill/PassiveSkillEffects.kt
@@ -3,8 +3,14 @@ package miragefairy2024.mod.passiveskill
 import miragefairy2024.MirageFairy2024
 import miragefairy2024.util.Translation
 import miragefairy2024.util.enJa
+import miragefairy2024.util.invoke
 import miragefairy2024.util.register
+import miragefairy2024.util.text
+import mirrg.kotlin.hydrogen.formatAs
+import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.util.Identifier
+import net.minecraft.util.math.BlockPos
+import net.minecraft.world.World
 
 fun initPassiveSkillEffects() {
     PassiveSkillEffectCard.entries.forEach { card ->
@@ -19,8 +25,21 @@ abstract class PassiveSkillEffectCard<T>(path: String) : PassiveSkillEffect<T> {
     companion object {
         val entries = mutableListOf<PassiveSkillEffectCard<*>>()
         private operator fun <T> PassiveSkillEffectCard<T>.unaryPlus() = this.also { entries += it }
+
+        val MANA = +ManaPassiveSkillEffect("mana")
     }
 
     val identifier = Identifier(MirageFairy2024.modId, path)
     abstract val translations: List<Translation>
+}
+
+class ManaPassiveSkillEffect(path: String) : PassiveSkillEffectCard<Double>(path) {
+    override val isPreprocessor = true
+    override fun getText(value: Double) = text { translation() + " ${value formatAs "%+.0f"}"() }
+    override val unit = 0.0
+    override fun castOrThrow(value: Any?) = value as Double
+    override fun combine(a: Double, b: Double) = a + b
+    override fun update(world: World, blockPos: BlockPos, player: PlayerEntity, oldValue: Double, newValue: Double) = Unit
+    val translation = Translation({ "miragefairy2024.passive_skill_type.${identifier.toTranslationKey()}" }, "Mana", "魔力")
+    override val translations = listOf(translation)
 }
