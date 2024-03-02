8c45029282dc97955c6fe00edbda4b94c33b3b0b
MirageFairy <idolm@ster>
2024-03-02 17:24:45 +0900

    refactor: move: ReiCategoryCard

--- src/main/kotlin/miragefairy2024/mod/rei/MirageFairy2024ReiServerPlugin.kt
+++ src/main/kotlin/miragefairy2024/mod/rei/MirageFairy2024ReiServerPlugin.kt
@@ -1,30 +1,8 @@
 package miragefairy2024.mod.rei
 
-import me.shedaniel.rei.api.common.category.CategoryIdentifier
 import me.shedaniel.rei.api.common.display.DisplaySerializerRegistry
 import me.shedaniel.rei.api.common.display.basic.BasicDisplay
 import me.shedaniel.rei.api.common.plugins.REIServerPlugin
-import miragefairy2024.MirageFairy2024
-import miragefairy2024.util.Translation
-
-abstract class ReiCategoryCard<D : BasicDisplay>(
-    val path: String,
-    enName: String,
-    jaName: String,
-) {
-    companion object {
-        val entries = listOf(
-            WorldGenTraitReiCategoryCard,
-            MagicPlantCropReiCategoryCard,
-            FairyQuestRecipeReiCategoryCard,
-            CommonMotifRecipeReiCategoryCard,
-        )
-    }
-
-    val translation = Translation({ "category.rei.${MirageFairy2024.modId}.$path" }, enName, jaName)
-    val identifier: CategoryIdentifier<D> by lazy { CategoryIdentifier.of(MirageFairy2024.modId, "plugins/$path") }
-    abstract val serializer: BasicDisplay.Serializer<D>
-}
 
 @Suppress("unused")
 class MirageFairy2024ReiServerPlugin : REIServerPlugin {
