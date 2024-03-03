5e5b6d31f27cbcf9a549261bb6cfdceef7baa4ac
MirageFairy <idolm@ster>
2024-03-04 02:55:39 +0900

    internal: FairyDreamTable: add: getDisplayMap, getDisplayTagMap

--- src/main/kotlin/miragefairy2024/mod/fairy/FairyDreamRecipe.kt
+++ src/main/kotlin/miragefairy2024/mod/fairy/FairyDreamRecipe.kt
@@ -16,16 +16,23 @@ object FairyDreamRecipes {
 
 class FairyDreamTable<T>(val registry: Registry<T>) {
     private val map = mutableMapOf<T, MutableSet<Motif>>()
+    private val displayMap = mutableListOf<Pair<T, Motif>>()
+    private val displayTagMap = mutableListOf<Pair<TagKey<T>, Motif>>()
 
     fun register(key: T, motif: Motif) {
         map.getOrPut(key) { mutableSetOf() } += motif
+        displayMap += Pair(key, motif)
     }
 
     fun registerFromTag(tag: TagKey<T>, motif: Motif) {
         registry.getEntryList(tag).getOrElse { return }.map { it.value() }.forEach {
-            register(it, motif)
+            map.getOrPut(it) { mutableSetOf() } += motif
         }
+        displayTagMap += Pair(tag, motif)
     }
 
     fun test(key: T): Set<Motif> = map.getOrElse(key) { setOf() }
+
+    fun getDisplayMap(): List<Pair<T, Motif>> = displayMap
+    fun getDisplayTagMap(): List<Pair<TagKey<T>, Motif>> = displayTagMap
 }
