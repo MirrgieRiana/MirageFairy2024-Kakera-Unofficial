755d71c8ac40a4f185af8fcff212e427de2820c9
MirageFairy <idolm@ster>
2024-03-06 18:19:10 +0900

    internal: add: buildText

--- src/main/kotlin/miragefairy2024/util/TextScope.kt
+++ src/main/kotlin/miragefairy2024/util/TextScope.kt
@@ -2,12 +2,24 @@ package miragefairy2024.util
 
 import net.minecraft.text.Text
 
-inline fun text(block: TextScope.() -> Text) = block(TextScope)
+inline fun text(block: TextScope.() -> Text) = block(TextScope())
 
-object TextScope {
+open class TextScope {
     fun empty(): Text = Text.empty()
     operator fun String.invoke(): Text = Text.of(this)
     fun translate(key: String): Text = Text.translatable(key)
     fun translate(key: String, vararg args: Any?): Text = Text.translatable(key, *args)
     operator fun Text.plus(text: Text): Text = Text.empty().append(this).append(text)
+}
+
+fun buildText(block: BuildTextScope.() -> Unit) = BuildTextScope().also { block(it) }.build()
+
+class BuildTextScope : TextScope() {
+    private val texts = mutableListOf<Text>()
+
+    operator fun Text.not() {
+        texts += this
+    }
+
+    fun build() = texts.join()
 }
