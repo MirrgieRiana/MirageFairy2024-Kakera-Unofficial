1f2239362b3bfd1a72bc624778018b5c5597b93d
MirageFairy <idolm@ster>
2024-03-17 21:26:35 +0900

    internal: add: TagKey.registerTagGeneration

--- src/main/kotlin/miragefairy2024/util/Tag.kt
+++ src/main/kotlin/miragefairy2024/util/Tag.kt
@@ -9,6 +9,16 @@ fun Block.registerTagGeneration(tagProvider: () -> TagKey<Block>) = MirageFairy2
     it(tagProvider()).add(this)
 }
 
+@JvmName("registerTagGenerationOfBlockTag")
+fun TagKey<Block>.registerTagGeneration(tagProvider: () -> TagKey<Block>) = MirageFairy2024DataGenerator.blockTagGenerators {
+    it(tagProvider()).addOptionalTag(this)
+}
+
 fun Item.registerTagGeneration(tagProvider: () -> TagKey<Item>) = MirageFairy2024DataGenerator.itemTagGenerators {
     it(tagProvider()).add(this)
+}
+
+@JvmName("registerTagGenerationOfItemTag")
+fun TagKey<Item>.registerTagGeneration(tagProvider: () -> TagKey<Item>) = MirageFairy2024DataGenerator.itemTagGenerators {
+    it(tagProvider()).addOptionalTag(this)
 }
