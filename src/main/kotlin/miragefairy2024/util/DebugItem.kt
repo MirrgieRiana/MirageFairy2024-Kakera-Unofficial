68cf84019e6d1753ea3858e58b2fcc41e67d4883
MirageFairy <idolm@ster>
2024-07-10 14:03:11 +0900

    internal: add: registerClientDebugItem, registerServerDebugItem

--- src/main/kotlin/miragefairy2024/util/DebugItem.kt
+++ src/main/kotlin/miragefairy2024/util/DebugItem.kt
@@ -8,7 +8,10 @@ import net.minecraft.data.client.TextureMap
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
+import net.minecraft.item.Items
 import net.minecraft.registry.Registries
+import net.minecraft.server.network.ServerPlayerEntity
+import net.minecraft.server.world.ServerWorld
 import net.minecraft.util.Hand
 import net.minecraft.util.Identifier
 import net.minecraft.util.TypedActionResult
@@ -28,6 +31,20 @@ fun registerDebugItem(path: String, icon: Item = Items.BOOK, color: Int = 0x8888
     item.registerItemGroup(mirageFairy2024ItemGroupCard.itemGroupKey)
     item.registerItemModelGeneration(Models.GENERATED with TextureMap.layer0(icon))
     item.registerColorProvider { _, _ -> color }
+}
+
+fun registerClientDebugItem(path: String, icon: Item = Items.BOOK, color: Int = 0x888888, action: (World, PlayerEntity, Hand, ItemStack) -> Unit) {
+    registerDebugItem(path, icon, color) { world, player, hand, itemStack ->
+        if (!world.isClient) return@registerDebugItem
+        action(world, player, hand, itemStack)
+    }
+}
+
+fun registerServerDebugItem(path: String, icon: Item = Items.BOOK, color: Int = 0x888888, action: (ServerWorld, ServerPlayerEntity, Hand, ItemStack) -> Unit) {
+    registerDebugItem(path, icon, color) { world, player, hand, itemStack ->
+        if (world.isClient) return@registerDebugItem
+        action(world as ServerWorld, player as ServerPlayerEntity, hand, itemStack)
+    }
 }
 
 fun writeAction(player: PlayerEntity, fileName: String, text: String) {
