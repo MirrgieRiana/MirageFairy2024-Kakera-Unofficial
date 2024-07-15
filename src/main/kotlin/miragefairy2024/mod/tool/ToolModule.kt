94400cab4948b0f8b0149c19a3e1cd82ceb96d29
MirageFairy <idolm@ster>
2024-07-15 22:37:13 +0900

    ADD: Fairy Crystal Pickaxe

--- src/main/kotlin/miragefairy2024/mod/tool/ToolModule.kt
+++ src/main/kotlin/miragefairy2024/mod/tool/ToolModule.kt
@@ -1,17 +1,22 @@
 package miragefairy2024.mod.tool
 
 import miragefairy2024.MirageFairy2024
+import miragefairy2024.mod.MaterialCard
 import miragefairy2024.mod.PoemList
+import miragefairy2024.mod.ToolMaterialCard
 import miragefairy2024.mod.mirageFairy2024ItemGroupCard
 import miragefairy2024.mod.poem
 import miragefairy2024.mod.registerPoem
 import miragefairy2024.mod.registerPoemGeneration
 import miragefairy2024.util.enJa
+import miragefairy2024.util.on
 import miragefairy2024.util.register
 import miragefairy2024.util.registerItemGroup
 import miragefairy2024.util.registerItemModelGeneration
+import miragefairy2024.util.registerShapedRecipeGeneration
 import net.minecraft.data.client.Models
 import net.minecraft.item.Item
+import net.minecraft.item.Items
 import net.minecraft.registry.Registries
 import net.minecraft.util.Identifier
 
@@ -61,5 +66,19 @@ class ToolCard<I : Item>(
     companion object {
         val entries = mutableListOf<ToolCard<*>>()
         private fun <I : Item> ToolCard<I>.register() = this.also { entries.add(this) }
+
+        val FAIRY_CRYSTAL_PICKAXE = ToolCard(
+            "fairy_crystal_pickaxe", "Fairy Crystal Pickaxe", "フェアリークリスタルのつるはし",
+            "A transparent brain", "闇を打ち砕く透明な心。",
+            2, PickaxeType(ToolMaterialCard.FAIRY_CRYSTAL), // TODO 特殊能力
+        ) {
+            registerShapedRecipeGeneration(item) {
+                pattern("###")
+                pattern(" R ")
+                pattern(" R ")
+                input('#', MaterialCard.FAIRY_CRYSTAL.item)
+                input('R', Items.STICK)
+            } on MaterialCard.FAIRY_CRYSTAL.item
+        }.register()
     }
 }
