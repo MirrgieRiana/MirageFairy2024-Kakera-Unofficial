d7fdfb0c286333cb2a817364f0c417774195bf03
MirageFairy <idolm@ster>
2024-07-16 16:34:25 +0900

    internal: add: registerBiomeTagGeneration

--- src/main/kotlin/miragefairy2024/util/Tag.kt
+++ src/main/kotlin/miragefairy2024/util/Tag.kt
@@ -4,6 +4,8 @@ import miragefairy2024.MirageFairy2024DataGenerator
 import net.minecraft.block.Block
 import net.minecraft.item.Item
 import net.minecraft.registry.tag.TagKey
+import net.minecraft.util.Identifier
+import net.minecraft.world.biome.Biome
 
 fun Block.registerBlockTagGeneration(tagProvider: () -> TagKey<Block>) = MirageFairy2024DataGenerator.blockTagGenerators {
     it(tagProvider()).add(this)
@@ -18,5 +20,13 @@ fun Item.registerItemTagGeneration(tagProvider: () -> TagKey<Item>) = MirageFair
 }
 
 fun TagKey<Item>.registerItemTagGeneration(tagProvider: () -> TagKey<Item>) = MirageFairy2024DataGenerator.itemTagGenerators {
+    it(tagProvider()).addOptionalTag(this)
+}
+
+fun Identifier.registerBiomeTagGeneration(tagProvider: () -> TagKey<Biome>) = MirageFairy2024DataGenerator.biomeTagGenerators {
+    it(tagProvider()).add(this)
+}
+
+fun TagKey<Biome>.registerBiomeTagGeneration(tagProvider: () -> TagKey<Biome>) = MirageFairy2024DataGenerator.biomeTagGenerators {
     it(tagProvider()).addOptionalTag(this)
 }
