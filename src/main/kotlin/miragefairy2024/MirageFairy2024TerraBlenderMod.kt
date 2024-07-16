f0726c7118603ed967e9834317ac5199aca0a94e
MirageFairy <idolm@ster>
2024-07-16 16:34:57 +0900

    internal: add: MirageFairy2024TerraBlenderMod

---
+++ src/main/kotlin/miragefairy2024/MirageFairy2024TerraBlenderMod.kt
@@ -0,0 +1,14 @@
+package miragefairy2024
+
+import terrablender.api.TerraBlenderApi
+
+object TerraBlenderEvents {
+    val onTerraBlenderInitialized = InitializationEventRegistry<() -> Unit>()
+}
+
+class MirageFairy2024TerraBlenderMod : TerraBlenderApi {
+    override fun onTerraBlenderInitialized() {
+        Modules.init()
+        TerraBlenderEvents.onTerraBlenderInitialized.fire { it() }
+    }
+}
