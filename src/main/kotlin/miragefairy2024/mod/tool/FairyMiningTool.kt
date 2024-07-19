26677ebbc138c0f4f18d7c0a4b7108470513c875
MirageFairy <idolm@ster>
2024-07-19 16:03:02 +0900

    internal: FairyMiningToolItem: add: areaMining, silkTouch, naturalRecovery

--- src/main/kotlin/miragefairy2024/mod/tool/FairyMiningTool.kt
+++ src/main/kotlin/miragefairy2024/mod/tool/FairyMiningTool.kt
@@ -1,6 +1,7 @@
 package miragefairy2024.mod.tool
 
 import miragefairy2024.MirageFairy2024
+import miragefairy2024.mixin.api.OverrideEnchantmentLevelCallback
 import miragefairy2024.mod.PoemList
 import miragefairy2024.mod.PoemType
 import miragefairy2024.mod.ToolMaterialCard
@@ -9,13 +10,20 @@ import miragefairy2024.util.NeighborType
 import miragefairy2024.util.Translation
 import miragefairy2024.util.blockVisitor
 import miragefairy2024.util.breakBlockByMagic
+import miragefairy2024.util.randomInt
 import miragefairy2024.util.registerItemTagGeneration
+import miragefairy2024.util.repair
+import mirrg.kotlin.hydrogen.atLeast
 import net.fabricmc.fabric.api.tag.convention.v1.ConventionalBlockTags
 import net.fabricmc.yarn.constants.MiningLevels
 import net.minecraft.block.Block
 import net.minecraft.block.BlockState
+import net.minecraft.enchantment.Enchantment
+import net.minecraft.enchantment.Enchantments
+import net.minecraft.entity.Entity
 import net.minecraft.entity.EquipmentSlot
 import net.minecraft.entity.LivingEntity
+import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
 import net.minecraft.item.MiningToolItem
@@ -34,8 +42,11 @@ class FairyMiningToolType(
     var attackDamage = 0F
     var attackSpeed = 0F
     val effectiveBlockTags = mutableListOf<TagKey<Block>>()
+    var areaMining = false
     var mineAll = false
     var cutAll = false
+    var silkTouch = false
+    var naturalRecovery = false
     val descriptions = mutableListOf<Translation>()
 
     override fun createItem() = FairyMiningToolItem(this, Item.Settings())
@@ -78,6 +89,11 @@ fun FairyMiningToolType.axe(attackDamage: Float, attackSpeed: Float) = this.also
 // @param attackDamage wood: 0.0, stone: -1.0, gold: 0.0, iron: -2.0, diamond: -3.0, netherite: -4.0
 // @param attackSpeed wood: -3.0, stone: -2.0, gold: -3.0, iron: -1.0, diamond: 0.0, netherite: 0.0
 
+fun FairyMiningToolType.areaMining() = this.also {
+    it.areaMining = true
+    it.descriptions += FairyMiningToolItem.AREA_MINING_TRANSLATION
+}
+
 fun FairyMiningToolType.mineAll() = this.also {
     it.mineAll = true
     it.descriptions += FairyMiningToolItem.MINE_ALL_TRANSLATION
@@ -88,11 +104,24 @@ fun FairyMiningToolType.cutAll() = this.also {
     it.descriptions += FairyMiningToolItem.CUT_ALL_TRANSLATION
 }
 
+fun FairyMiningToolType.silkTouch() = this.also {
+    it.silkTouch = true
+    it.descriptions += FairyMiningToolItem.SILK_TOUCH_TRANSLATION
+}
 
-class FairyMiningToolItem(private val type: FairyMiningToolType, settings: Settings) : MiningToolItem(type.attackDamage, type.attackSpeed, type.toolMaterialCard.toolMaterial, BlockTags.PICKAXE_MINEABLE/* dummy */, settings) {
+fun FairyMiningToolType.naturalRecovery() = this.also {
+    it.naturalRecovery = true
+    it.descriptions += FairyMiningToolItem.NATURAL_RECOVERY_TRANSLATION
+}
+
+
+class FairyMiningToolItem(private val type: FairyMiningToolType, settings: Settings) : MiningToolItem(type.attackDamage, type.attackSpeed, type.toolMaterialCard.toolMaterial, BlockTags.PICKAXE_MINEABLE/* dummy */, settings), OverrideEnchantmentLevelCallback {
     companion object {
+        val AREA_MINING_TRANSLATION = Translation({ "item.${MirageFairy2024.modId}.fairy_mining_tool.area_mining" }, "Area mining", "範囲採掘")
         val MINE_ALL_TRANSLATION = Translation({ "item.${MirageFairy2024.modId}.fairy_mining_tool.mine_all" }, "Mine the entire ore", "鉱石全体を採掘")
         val CUT_ALL_TRANSLATION = Translation({ "item.${MirageFairy2024.modId}.fairy_mining_tool.cut_all" }, "Cut down the entire tree", "木全体を伐採")
+        val SILK_TOUCH_TRANSLATION = Translation({ "item.${MirageFairy2024.modId}.fairy_mining_tool.silk_touch" }, "Silk Touch", "シルクタッチ")
+        val NATURAL_RECOVERY_TRANSLATION = Translation({ "item.${MirageFairy2024.modId}.fairy_mining_tool.natural_recovery" }, "Natural recovery", "自然回復")
     }
 
     override fun getMiningSpeedMultiplier(stack: ItemStack, state: BlockState) = if (type.effectiveBlockTags.any { state.isIn(it) }) miningSpeed else 1.0F
@@ -108,6 +137,36 @@ class FairyMiningToolItem(private val type: FairyMiningToolType, settings: Setti
 
     override fun postMine(stack: ItemStack, world: World, state: BlockState, pos: BlockPos, miner: LivingEntity): Boolean {
         super.postMine(stack, world, state, pos, miner)
+        if (type.areaMining) run fail@{
+            if (world.isClient) return@fail
+
+            if (miner.isSneaking) return@fail // 使用者がスニーク中
+            if (miner !is ServerPlayerEntity) return@fail // 使用者がプレイヤーでない
+            if (!isSuitableFor(state)) return@fail // 掘ったブロックに対して特効でない
+            if (!state.isIn(ConventionalBlockTags.ORES)) return@fail // 掘ったブロックが鉱石ではない
+
+            // 発動
+
+            (-1..1).forEach { x ->
+                (-1..1).forEach { y ->
+                    (-1..1).forEach { z ->
+                        if (x != 0 && y != 0 && z != 0) {
+                            val targetBlockPos = pos.add(x, y, z)
+                            val targetBlockState = world.getBlockState(targetBlockPos)
+                            if (targetBlockState === state) {
+                                if (stack.isEmpty) return@fail // ツールの耐久値が枯渇した
+                                if (stack.maxDamage - stack.damage <= 1) return@fail // ツールの耐久値が残り1
+                                if (breakBlockByMagic(stack, world, targetBlockPos, miner)) {
+                                    stack.damage(1, miner) {
+                                        it.sendEquipmentBreakStatus(EquipmentSlot.MAINHAND)
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
         if (type.mineAll) run fail@{
             if (world.isClient) return@fail
 
@@ -168,5 +227,20 @@ class FairyMiningToolItem(private val type: FairyMiningToolType, settings: Setti
             }
         }
         return true
+    }
+
+    override fun inventoryTick(stack: ItemStack, world: World, entity: Entity, slot: Int, selected: Boolean) {
+        if (type.naturalRecovery) {
+            if (entity !is PlayerEntity) return // プレイヤーじゃない
+            if (stack !== entity.mainHandStack) return // メインハンドに持っていない
+            stack.repair(world.random.randomInt(0.1))
+        }
+    }
+
+    override fun overrideEnchantmentLevel(enchantment: Enchantment, itemStack: ItemStack, oldLevel: Int): Int {
+        if (type.silkTouch) {
+            if (enchantment == Enchantments.SILK_TOUCH) return oldLevel atLeast 1
+        }
+        return oldLevel
     }
 }
