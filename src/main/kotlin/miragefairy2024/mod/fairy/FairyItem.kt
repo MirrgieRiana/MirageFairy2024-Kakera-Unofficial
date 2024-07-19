f7338ecf9bc5326ed264ac573d74f4ec460ece20
MirageFairy <idolm@ster>
2024-07-19 18:52:43 +0900

    refactor: PassiveSkill

--- src/main/kotlin/miragefairy2024/mod/fairy/FairyItem.kt
+++ src/main/kotlin/miragefairy2024/mod/fairy/FairyItem.kt
@@ -239,11 +239,11 @@ class FairyItem(settings: Settings) : Item(settings), PassiveSkillProvider {
 
     override fun getPassiveSkill(itemStack: ItemStack): PassiveSkill? {
         val motif = itemStack.getFairyMotif() ?: return null
-        val level = motif.rare.toDouble() + log(itemStack.getFairyCondensation().toDouble() * itemStack.count, 3.0)
         return PassiveSkill(
             "fairy/" concat motif.getIdentifier()!!,
             motif,
-            level,
+            motif.rare.toDouble(),
+            itemStack.getFairyCondensation().toDouble() * itemStack.count.toDouble(),
             motif.passiveSkillSpecifications,
         )
     }
