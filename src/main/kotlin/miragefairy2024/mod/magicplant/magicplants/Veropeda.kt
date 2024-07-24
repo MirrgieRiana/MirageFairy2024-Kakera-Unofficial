8a71e15b6007e16a2271e628401ae9f7a79af022
MirageFairy <idolm@ster>
2024-07-24 13:54:34 +0900

    cleanup: Veropeda

--- src/main/kotlin/miragefairy2024/mod/magicplant/magicplants/Veropeda.kt
+++ src/main/kotlin/miragefairy2024/mod/magicplant/magicplants/Veropeda.kt
@@ -72,83 +72,79 @@ fun initVeropeda() {
         }
     }
 
+    // 地形生成
     ModEvents.onInitialize {
-
-        // 地形生成
-        run {
-            // 小さな塊
-            val veropedaClusterConfiguredFeatureKey = registerDynamicGeneration(RegistryKeys.CONFIGURED_FEATURE, Identifier(MirageFairy2024.modId, "veropeda_cluster")) {
-                val blockStateProvider = BlockStateProvider.of(card.block.withAge(card.block.maxAge))
-                Feature.FLOWER with RandomPatchFeatureConfig(6, 6, 2, PlacedFeatures.createEntry(Feature.SIMPLE_BLOCK, SimpleBlockFeatureConfig(blockStateProvider)))
-            }
-
-            // 大きな塊
-            val largeVeropedaClusterConfiguredFeatureKey = registerDynamicGeneration(RegistryKeys.CONFIGURED_FEATURE, Identifier(MirageFairy2024.modId, "large_veropeda_cluster")) {
-                val blockStateProvider = BlockStateProvider.of(card.block.withAge(card.block.maxAge))
-                Feature.FLOWER with RandomPatchFeatureConfig(40, 8, 3, PlacedFeatures.createEntry(Feature.SIMPLE_BLOCK, SimpleBlockFeatureConfig(blockStateProvider)))
-            }
-
-            // 地上
-            registerDynamicGeneration(RegistryKeys.PLACED_FEATURE, Identifier(MirageFairy2024.modId, "veropeda_cluster")) {
-                val placementModifiers = listOf(
-                    RarityFilterPlacementModifier.of(16),
-                    SquarePlacementModifier.of(),
-                    PlacedFeatures.MOTION_BLOCKING_HEIGHTMAP,
-                    BiomePlacementModifier.of(),
-                )
-                it.getRegistryLookup(RegistryKeys.CONFIGURED_FEATURE).getOrThrow(veropedaClusterConfiguredFeatureKey) with placementModifiers
-            }.also {
-                BiomeModifications.addFeature(BiomeSelectors.tag(ConventionalBiomeTags.CLIMATE_DRY), GenerationStep.Feature.VEGETAL_DECORATION, it)
-            }
-
-            // ネザー
-            registerDynamicGeneration(RegistryKeys.PLACED_FEATURE, Identifier(MirageFairy2024.modId, "nether_veropeda_cluster")) {
-                val placementModifiers = listOf(
-                    RarityFilterPlacementModifier.of(8),
-                    CountMultilayerPlacementModifier.of(1),
-                    BiomePlacementModifier.of(),
-                )
-                it.getRegistryLookup(RegistryKeys.CONFIGURED_FEATURE).getOrThrow(largeVeropedaClusterConfiguredFeatureKey) with placementModifiers
-            }.also {
-                BiomeModifications.addFeature(BiomeSelectors.foundInTheNether(), GenerationStep.Feature.VEGETAL_DECORATION, it)
-            }
+        // 小さな塊
+        val veropedaClusterConfiguredFeatureKey = registerDynamicGeneration(RegistryKeys.CONFIGURED_FEATURE, Identifier(MirageFairy2024.modId, "veropeda_cluster")) {
+            val blockStateProvider = BlockStateProvider.of(card.block.withAge(card.block.maxAge))
+            Feature.FLOWER with RandomPatchFeatureConfig(6, 6, 2, PlacedFeatures.createEntry(Feature.SIMPLE_BLOCK, SimpleBlockFeatureConfig(blockStateProvider)))
         }
 
-        // 特性
-        WorldGenTraitRecipeInitScope(card.block).run {
-
-            // 標準特性
-            registerWorldGenTraitRecipe("A.RS", TraitCard.PAVEMENT_FLOWERS) // アスファルトに咲く花
-            registerWorldGenTraitRecipe("A.RS", TraitCard.OSMOTIC_ABSORPTION) // 浸透吸収
-            registerWorldGenTraitRecipe("A.RS", TraitCard.ARID_ADAPTATION) // 乾燥適応
-            registerWorldGenTraitRecipe("..CR", TraitCard.SEEDS_PRODUCTION) // 種子生成
-            registerWorldGenTraitRecipe("C.CR", TraitCard.FRUITS_PRODUCTION) // 果実生成
-            registerWorldGenTraitRecipe("C.CR", TraitCard.LEAVES_PRODUCTION) // 葉面生成
-            registerWorldGenTraitRecipe("..CR", TraitCard.GOLDEN_APPLE) // 金のリンゴ
-
-            // N特性
-            registerWorldGenTraitRecipe("NRS.", TraitCard.PHAEOSYNTHESIS) // 闇合成
-            registerWorldGenTraitRecipe("NRS.", TraitCard.CARNIVOROUS_PLANT) // 食虫植物
-            registerWorldGenTraitRecipe("NRS.", TraitCard.PROSPERITY_OF_SPECIES) // 種の繁栄
-            registerWorldGenTraitRecipe("NRS.", TraitCard.FOUR_LEAFED) // 四つ葉
-            registerWorldGenTraitRecipe("NRS.", TraitCard.NATURAL_ABSCISSION) // 自然落果
-            registerWorldGenTraitRecipe("..NR", TraitCard.PHOTOSYNTHESIS) // 光合成
-            registerWorldGenTraitRecipe("..NR", TraitCard.EXPERIENCE_PRODUCTION) // 経験値生成
-            registerWorldGenTraitRecipe("..NR", TraitCard.FRUIT_OF_KNOWLEDGE) // 知識の果実
-            registerWorldGenTraitRecipe("..NR", TraitCard.SPINY_LEAVES) // 棘状の葉
-            registerWorldGenTraitRecipe("..NR", TraitCard.DESERT_GEM) // 砂漠の宝石
-            registerWorldGenTraitRecipe("..NR", TraitCard.ADVERSITY_FLOWER) // 高嶺の花
-            registerWorldGenTraitRecipe("..NR", TraitCard.FLESHY_LEAVES) // 肉厚の葉
-
-            // SR特性
-            registerWorldGenTraitRecipe(".S..", TraitCard.COLD_ADAPTATION) // 寒冷適応
-            registerWorldGenTraitRecipe(".S..", TraitCard.WARM_ADAPTATION) // 温暖適応
-            registerWorldGenTraitRecipe(".S..", TraitCard.HOT_ADAPTATION) // 熱帯適応
-            registerWorldGenTraitRecipe(".S..", TraitCard.ARID_ADAPTATION) // 乾燥適応
-            registerWorldGenTraitRecipe(".S..", TraitCard.MESIC_ADAPTATION) // 中湿適応
-            registerWorldGenTraitRecipe(".S..", TraitCard.HUMID_ADAPTATION) // 湿潤適応
-
+        // 大きな塊
+        val largeVeropedaClusterConfiguredFeatureKey = registerDynamicGeneration(RegistryKeys.CONFIGURED_FEATURE, Identifier(MirageFairy2024.modId, "large_veropeda_cluster")) {
+            val blockStateProvider = BlockStateProvider.of(card.block.withAge(card.block.maxAge))
+            Feature.FLOWER with RandomPatchFeatureConfig(40, 8, 3, PlacedFeatures.createEntry(Feature.SIMPLE_BLOCK, SimpleBlockFeatureConfig(blockStateProvider)))
         }
+
+        // 地上
+        registerDynamicGeneration(RegistryKeys.PLACED_FEATURE, Identifier(MirageFairy2024.modId, "veropeda_cluster")) {
+            val placementModifiers = listOf(
+                RarityFilterPlacementModifier.of(16),
+                SquarePlacementModifier.of(),
+                PlacedFeatures.MOTION_BLOCKING_HEIGHTMAP,
+                BiomePlacementModifier.of(),
+            )
+            it.getRegistryLookup(RegistryKeys.CONFIGURED_FEATURE).getOrThrow(veropedaClusterConfiguredFeatureKey) with placementModifiers
+        }.also {
+            BiomeModifications.addFeature(BiomeSelectors.tag(ConventionalBiomeTags.CLIMATE_DRY), GenerationStep.Feature.VEGETAL_DECORATION, it)
+        }
+
+        // ネザー
+        registerDynamicGeneration(RegistryKeys.PLACED_FEATURE, Identifier(MirageFairy2024.modId, "nether_veropeda_cluster")) {
+            val placementModifiers = listOf(
+                RarityFilterPlacementModifier.of(8),
+                CountMultilayerPlacementModifier.of(1),
+                BiomePlacementModifier.of(),
+            )
+            it.getRegistryLookup(RegistryKeys.CONFIGURED_FEATURE).getOrThrow(largeVeropedaClusterConfiguredFeatureKey) with placementModifiers
+        }.also {
+            BiomeModifications.addFeature(BiomeSelectors.foundInTheNether(), GenerationStep.Feature.VEGETAL_DECORATION, it)
+        }
+    }
+
+    // 特性
+    WorldGenTraitRecipeInitScope(card.block).run {
+
+        // 標準特性
+        registerWorldGenTraitRecipe("A.RS", TraitCard.PAVEMENT_FLOWERS) // アスファルトに咲く花
+        registerWorldGenTraitRecipe("A.RS", TraitCard.OSMOTIC_ABSORPTION) // 浸透吸収
+        registerWorldGenTraitRecipe("A.RS", TraitCard.ARID_ADAPTATION) // 乾燥適応
+        registerWorldGenTraitRecipe("..CR", TraitCard.SEEDS_PRODUCTION) // 種子生成
+        registerWorldGenTraitRecipe("C.CR", TraitCard.FRUITS_PRODUCTION) // 果実生成
+        registerWorldGenTraitRecipe("C.CR", TraitCard.LEAVES_PRODUCTION) // 葉面生成
+        registerWorldGenTraitRecipe("..CR", TraitCard.GOLDEN_APPLE) // 金のリンゴ
+
+        // N特性
+        registerWorldGenTraitRecipe("NRS.", TraitCard.PHAEOSYNTHESIS) // 闇合成
+        registerWorldGenTraitRecipe("NRS.", TraitCard.CARNIVOROUS_PLANT) // 食虫植物
+        registerWorldGenTraitRecipe("NRS.", TraitCard.PROSPERITY_OF_SPECIES) // 種の繁栄
+        registerWorldGenTraitRecipe("NRS.", TraitCard.FOUR_LEAFED) // 四つ葉
+        registerWorldGenTraitRecipe("NRS.", TraitCard.NATURAL_ABSCISSION) // 自然落果
+        registerWorldGenTraitRecipe("..NR", TraitCard.PHOTOSYNTHESIS) // 光合成
+        registerWorldGenTraitRecipe("..NR", TraitCard.EXPERIENCE_PRODUCTION) // 経験値生成
+        registerWorldGenTraitRecipe("..NR", TraitCard.FRUIT_OF_KNOWLEDGE) // 知識の果実
+        registerWorldGenTraitRecipe("..NR", TraitCard.SPINY_LEAVES) // 棘状の葉
+        registerWorldGenTraitRecipe("..NR", TraitCard.DESERT_GEM) // 砂漠の宝石
+        registerWorldGenTraitRecipe("..NR", TraitCard.ADVERSITY_FLOWER) // 高嶺の花
+        registerWorldGenTraitRecipe("..NR", TraitCard.FLESHY_LEAVES) // 肉厚の葉
+
+        // SR特性
+        registerWorldGenTraitRecipe(".S..", TraitCard.COLD_ADAPTATION) // 寒冷適応
+        registerWorldGenTraitRecipe(".S..", TraitCard.WARM_ADAPTATION) // 温暖適応
+        registerWorldGenTraitRecipe(".S..", TraitCard.HOT_ADAPTATION) // 熱帯適応
+        registerWorldGenTraitRecipe(".S..", TraitCard.ARID_ADAPTATION) // 乾燥適応
+        registerWorldGenTraitRecipe(".S..", TraitCard.MESIC_ADAPTATION) // 中湿適応
+        registerWorldGenTraitRecipe(".S..", TraitCard.HUMID_ADAPTATION) // 湿潤適応
 
     }
 
