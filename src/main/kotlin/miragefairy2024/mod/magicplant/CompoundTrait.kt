ece6976c62930ba6189a795fd9bef2b77ff3979a
MirageFairy <idolm@ster>
2024-08-06 20:45:04 +0900

    refactor: extract: TraitFactor

--- src/main/kotlin/miragefairy2024/mod/magicplant/CompoundTrait.kt
+++ src/main/kotlin/miragefairy2024/mod/magicplant/CompoundTrait.kt
@@ -16,12 +16,3 @@ class CompoundTrait(sortKey: String, private val factor: TraitFactor, private va
         }
     }
 }
-
-fun interface TraitFactor {
-    fun getFactor(world: World, blockPos: BlockPos): Double
-}
-
-fun interface TraitCondition : TraitFactor {
-    override fun getFactor(world: World, blockPos: BlockPos) = if (isValid(world, blockPos)) 1.0 else 0.0
-    fun isValid(world: World, blockPos: BlockPos): Boolean
-}
