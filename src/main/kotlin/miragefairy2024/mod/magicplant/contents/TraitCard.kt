e8ac0cfd50602788739b040c37a7eeffedf4ce85
MirageFairy <idolm@ster>
2024-08-06 20:45:58 +0900

    refactor: move: CompoundTrait

--- src/main/kotlin/miragefairy2024/mod/magicplant/contents/TraitCard.kt
+++ src/main/kotlin/miragefairy2024/mod/magicplant/contents/TraitCard.kt
@@ -1,9 +1,12 @@
 package miragefairy2024.mod.magicplant.contents
 
 import miragefairy2024.MirageFairy2024
-import miragefairy2024.mod.magicplant.CompoundTrait
+import miragefairy2024.mod.magicplant.MutableTraitEffects
+import miragefairy2024.mod.magicplant.Trait
 import miragefairy2024.mod.magicplant.TraitFactor
 import net.minecraft.util.Identifier
+import net.minecraft.util.math.BlockPos
+import net.minecraft.world.World
 
 enum class TraitCard(
     path: String,
@@ -54,4 +57,17 @@ enum class TraitCard(
 
     val identifier = Identifier(MirageFairy2024.modId, path)
     val trait = CompoundTrait(sortKey, factor, traitEffectKeyCard)
+}
+
+class CompoundTrait(sortKey: String, private val factor: TraitFactor, private val traitEffectKeyCard: TraitEffectKeyCard) : Trait(traitEffectKeyCard.color, sortKey) {
+    override fun getTraitEffects(world: World, blockPos: BlockPos, level: Int): MutableTraitEffects? {
+        val factor = factor.getFactor(world, blockPos)
+        return if (factor != 0.0) {
+            val traitEffects = MutableTraitEffects()
+            traitEffects[traitEffectKeyCard.traitEffectKey] = traitEffectKeyCard.traitEffectKey.getValue(level) * factor
+            traitEffects
+        } else {
+            null
+        }
+    }
 }
