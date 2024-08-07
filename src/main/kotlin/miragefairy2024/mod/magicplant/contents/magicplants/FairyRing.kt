32656e1664ffcc3c9704312f4909a1cf3b4aafef
MirageFairy <idolm@ster>
2024-08-07 17:31:33 +0900

    refactor: extract: FairyRing

---
+++ src/main/kotlin/miragefairy2024/mod/magicplant/contents/magicplants/FairyRing.kt
@@ -0,0 +1,62 @@
+package miragefairy2024.mod.magicplant.contents.magicplants
+
+import com.mojang.serialization.Codec
+import com.mojang.serialization.codecs.RecordCodecBuilder
+import net.minecraft.registry.entry.RegistryEntry
+import net.minecraft.util.math.BlockPos
+import net.minecraft.util.math.MathHelper
+import net.minecraft.world.gen.feature.Feature
+import net.minecraft.world.gen.feature.FeatureConfig
+import net.minecraft.world.gen.feature.PlacedFeature
+import net.minecraft.world.gen.feature.util.FeatureContext
+
+class FairyRingFeatureConfig(val tries: Int, val minRadius: Float, val maxRadius: Float, val ySpread: Int, val feature: RegistryEntry<PlacedFeature>) : FeatureConfig {
+    companion object {
+        val CODEC: Codec<FairyRingFeatureConfig> = RecordCodecBuilder.create { instance ->
+            instance.group(
+                Codec.INT.fieldOf("tries").forGetter(FairyRingFeatureConfig::tries),
+                Codec.FLOAT.fieldOf("min_radius").forGetter(FairyRingFeatureConfig::minRadius),
+                Codec.FLOAT.fieldOf("max_radius").forGetter(FairyRingFeatureConfig::maxRadius),
+                Codec.INT.fieldOf("y_spread").forGetter(FairyRingFeatureConfig::ySpread),
+                PlacedFeature.REGISTRY_CODEC.fieldOf("feature").forGetter(FairyRingFeatureConfig::feature),
+            ).apply(instance, ::FairyRingFeatureConfig)
+        }
+    }
+
+    init {
+        require(tries >= 0)
+        require(minRadius >= 0F)
+        require(maxRadius >= 0F)
+        require(maxRadius >= minRadius)
+        require(ySpread >= 0)
+    }
+}
+
+class FairyRingFeature(codec: Codec<FairyRingFeatureConfig>) : Feature<FairyRingFeatureConfig>(codec) {
+    override fun generate(context: FeatureContext<FairyRingFeatureConfig>): Boolean {
+        val config = context.config
+        val random = context.random
+        val originBlockPos = context.origin
+        val world = context.world
+
+        var count = 0
+        val minRadius = config.minRadius
+        val radiusRange = config.maxRadius - minRadius
+        val y1 = config.ySpread + 1
+        val mutableBlockPos = BlockPos.Mutable()
+        for (l in 0 until config.tries) {
+            val r = random.nextFloat() * radiusRange + minRadius
+            val theta = random.nextFloat() * MathHelper.TAU
+            val x = MathHelper.floor(MathHelper.cos(theta) * r)
+            val y = random.nextInt(y1) - random.nextInt(y1)
+            val z = MathHelper.floor(MathHelper.sin(theta) * r)
+
+            mutableBlockPos.set(originBlockPos, x, y, z)
+            if (config.feature.value().generateUnregistered(world, context.generator, random, mutableBlockPos)) {
+                count++
+            }
+        }
+
+        return count > 0
+    }
+}
