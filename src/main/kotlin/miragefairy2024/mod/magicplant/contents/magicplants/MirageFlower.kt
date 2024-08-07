d64557694ef83187db32495ec4b69ede22315b31
MirageFairy <idolm@ster>
2024-08-07 19:45:29 +0900

    refactor: MirageFlower: getMirageFlour

--- src/main/kotlin/miragefairy2024/mod/magicplant/contents/magicplants/MirageFlower.kt
+++ src/main/kotlin/miragefairy2024/mod/magicplant/contents/magicplants/MirageFlower.kt
@@ -237,24 +237,24 @@ class MirageFlowerBlock(settings: Settings) : SimpleMagicPlantBlock({ MirageFlow
 
     override val ageProperty: IntProperty get() = Properties.AGE_3
     override fun getOutlineShape(state: BlockState, world: BlockView, pos: BlockPos, context: ShapeContext) = AGE_TO_SHAPE[getAge(state)]
-
-    override fun getFruitDrops(count: Int, random: Random): List<ItemStack> {
-        var count2 = count.toDouble()
-        if (count2 < 3) return listOf(MaterialCard.MIRAGE_FLOUR.item.createItemStack(random.randomInt(count2)))
-        count2 /= 9.0
-        if (count2 < 3) return listOf(MaterialCard.MIRAGE_FLOUR_OF_NATURE.item.createItemStack(random.randomInt(count2)))
-        count2 /= 9.0
-        if (count2 < 3) return listOf(MaterialCard.MIRAGE_FLOUR_OF_EARTH.item.createItemStack(random.randomInt(count2)))
-        count2 /= 9.0
-        if (count2 < 3) return listOf(MaterialCard.MIRAGE_FLOUR_OF_UNDERWORLD.item.createItemStack(random.randomInt(count2)))
-        count2 /= 9.0
-        if (count2 < 3) return listOf(MaterialCard.MIRAGE_FLOUR_OF_SKY.item.createItemStack(random.randomInt(count2)))
-        count2 /= 9.0
-        if (count2 < 3) return listOf(MaterialCard.MIRAGE_FLOUR_OF_UNIVERSE.item.createItemStack(random.randomInt(count2)))
-        count2 /= 9.0
-        return listOf(MaterialCard.MIRAGE_FLOUR_OF_TIME.item.createItemStack(random.randomInt(count2)))
-    }
-
+    override fun getFruitDrops(count: Int, random: Random): List<ItemStack> = getMirageFlour(count, random)
     override fun getLeafDrops(count: Int, random: Random): List<ItemStack> = listOf(MaterialCard.MIRAGE_LEAVES.item.createItemStack(count))
     override fun getRareDrops(count: Int, random: Random): List<ItemStack> = listOf(MaterialCard.FAIRY_CRYSTAL.item.createItemStack(count))
+}
+
+private fun getMirageFlour(count: Int, random: Random): List<ItemStack> {
+    var count2 = count.toDouble()
+    if (count2 < 3) return listOf(MaterialCard.MIRAGE_FLOUR.item.createItemStack(random.randomInt(count2)))
+    count2 /= 9.0
+    if (count2 < 3) return listOf(MaterialCard.MIRAGE_FLOUR_OF_NATURE.item.createItemStack(random.randomInt(count2)))
+    count2 /= 9.0
+    if (count2 < 3) return listOf(MaterialCard.MIRAGE_FLOUR_OF_EARTH.item.createItemStack(random.randomInt(count2)))
+    count2 /= 9.0
+    if (count2 < 3) return listOf(MaterialCard.MIRAGE_FLOUR_OF_UNDERWORLD.item.createItemStack(random.randomInt(count2)))
+    count2 /= 9.0
+    if (count2 < 3) return listOf(MaterialCard.MIRAGE_FLOUR_OF_SKY.item.createItemStack(random.randomInt(count2)))
+    count2 /= 9.0
+    if (count2 < 3) return listOf(MaterialCard.MIRAGE_FLOUR_OF_UNIVERSE.item.createItemStack(random.randomInt(count2)))
+    count2 /= 9.0
+    return listOf(MaterialCard.MIRAGE_FLOUR_OF_TIME.item.createItemStack(random.randomInt(count2)))
 }
