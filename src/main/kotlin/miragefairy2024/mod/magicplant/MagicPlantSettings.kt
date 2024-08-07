0e38134945eeed3405ceeff1ec80f791e9e4507b
MirageFairy <idolm@ster>
2024-08-07 18:23:25 +0900

    refactor: MirageFlowerSettings

--- src/main/kotlin/miragefairy2024/mod/magicplant/MagicPlantSettings.kt
+++ src/main/kotlin/miragefairy2024/mod/magicplant/MagicPlantSettings.kt
@@ -1,3 +1,15 @@
 package miragefairy2024.mod.magicplant
 
-abstract class MagicPlantSettings
+abstract class MagicPlantSettings {
+    abstract val blockPath: String
+    abstract val blockEnName: String
+    abstract val blockJaName: String
+    abstract val itemPath: String
+    abstract val itemEnName: String
+    abstract val itemJaName: String
+    abstract val tier: Int
+    abstract val enPoem: String
+    abstract val jaPoem: String
+    abstract val enClassification: String
+    abstract val jaClassification: String
+}
