3c7532a4fae8a7fb6006bb1b43a126f9a7a24662
MirageFairy <idolm@ster>
2024-08-07 17:06:57 +0900

    internal: add: BiomeSelectorScope

--- src/main/kotlin/miragefairy2024/util/WorldGen.kt
+++ src/main/kotlin/miragefairy2024/util/WorldGen.kt
@@ -6,11 +6,14 @@ import miragefairy2024.ModContext
 import miragefairy2024.ModEvents
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectionContext
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors
 import net.minecraft.registry.Registerable
 import net.minecraft.registry.Registry
 import net.minecraft.registry.RegistryKey
 import net.minecraft.registry.entry.RegistryEntry
+import net.minecraft.registry.tag.TagKey
 import net.minecraft.util.Identifier
+import net.minecraft.world.biome.Biome
 import net.minecraft.world.gen.GenerationStep
 import net.minecraft.world.gen.feature.ConfiguredFeature
 import net.minecraft.world.gen.feature.Feature
@@ -45,6 +48,22 @@ fun <T> registerDynamicGeneration(registryKey: RegistryKey<out Registry<T>>, key
 }
 
 context(ModContext)
-fun RegistryKey<PlacedFeature>.registerFeature(step: GenerationStep.Feature, biomeSelectorCreator: () -> Predicate<BiomeSelectionContext>) = ModEvents.onInitialize {
-    BiomeModifications.addFeature(biomeSelectorCreator(), step, this)
+fun RegistryKey<PlacedFeature>.registerFeature(step: GenerationStep.Feature, biomeSelectorCreator: BiomeSelectorScope.() -> Predicate<BiomeSelectionContext>) = ModEvents.onInitialize {
+    BiomeModifications.addFeature(biomeSelectorCreator(BiomeSelectorScope), step, this)
 }
+
+
+// BiomeSelectorScope
+
+object BiomeSelectorScope
+
+context(BiomeSelectorScope) val all: Predicate<BiomeSelectionContext> get() = BiomeSelectors.all()
+context(BiomeSelectorScope) val overworld: Predicate<BiomeSelectionContext> get() = BiomeSelectors.foundInOverworld()
+context(BiomeSelectorScope) val nether: Predicate<BiomeSelectionContext> get() = BiomeSelectors.foundInTheNether()
+context(BiomeSelectorScope) val end: Predicate<BiomeSelectionContext> get() = BiomeSelectors.foundInTheEnd()
+context(BiomeSelectorScope) fun biome(vararg biomes: RegistryKey<Biome>): Predicate<BiomeSelectionContext> = BiomeSelectors.includeByKey(*biomes)
+context(BiomeSelectorScope) fun biome(biomes: Collection<RegistryKey<Biome>>): Predicate<BiomeSelectionContext> = BiomeSelectors.includeByKey(biomes)
+context(BiomeSelectorScope) fun tag(tag: TagKey<Biome>): Predicate<BiomeSelectionContext> = BiomeSelectors.tag(tag)
+context(BiomeSelectorScope) operator fun Predicate<BiomeSelectionContext>.not(): Predicate<BiomeSelectionContext> = this.negate()
+context(BiomeSelectorScope) operator fun Predicate<BiomeSelectionContext>.times(other: Predicate<BiomeSelectionContext>): Predicate<BiomeSelectionContext> = this.and(other)
+context(BiomeSelectorScope) operator fun Predicate<BiomeSelectionContext>.plus(other: Predicate<BiomeSelectionContext>): Predicate<BiomeSelectionContext> = this.or(other)
