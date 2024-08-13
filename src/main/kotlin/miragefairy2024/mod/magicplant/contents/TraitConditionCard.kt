45f0545ed7a304a0dd0809654e719c1a1cb6f280
MirageFairy <idolm@ster>
2024-08-13 20:15:35 +0900

    internal: TraitCondition: add: emoji

--- src/main/kotlin/miragefairy2024/mod/magicplant/contents/TraitConditionCard.kt
+++ src/main/kotlin/miragefairy2024/mod/magicplant/contents/TraitConditionCard.kt
@@ -1,5 +1,7 @@
 package miragefairy2024.mod.magicplant.contents
 
+import miragefairy2024.mod.Emoji
+import miragefairy2024.mod.invoke
 import miragefairy2024.mod.magicplant.TraitCondition
 import miragefairy2024.mod.magicplant.getMagicPlantBlockEntity
 import miragefairy2024.util.HumidityCategory
@@ -16,21 +18,28 @@ import net.minecraft.world.Heightmap
 import net.minecraft.world.World
 
 enum class TraitConditionCard(
-    val traitCondition: TraitCondition,
+    emoji: Emoji,
+    function: (world: World, blockPos: BlockPos) -> Double,
 ) {
-    FLOOR_MOISTURE(TraitCondition { world, blockPos -> world.getMoisture(blockPos.down()) }),
-    FLOOR_CRYSTAL_ERG(TraitCondition { world, blockPos -> world.getCrystalErg(blockPos.down()) }),
-    FLOOR_HARDNESS(TraitCondition { world, blockPos -> getFloorHardness(world, blockPos) }),
-    LIGHT(TraitCondition { world, blockPos -> (world.getLightLevel(blockPos) - 8 atLeast 0) / 7.0 }),
-    DARKNESS(TraitCondition { world, blockPos -> ((15 - world.getLightLevel(blockPos)) - 8 atLeast 0) / 7.0 }),
-    LOW_TEMPERATURE(TraitCondition { world, blockPos -> if (world.getBiome(blockPos).temperatureCategory == TemperatureCategory.LOW) 1.0 else 0.0 }),
-    MEDIUM_TEMPERATURE(TraitCondition { world, blockPos -> if (world.getBiome(blockPos).temperatureCategory == TemperatureCategory.MEDIUM) 1.0 else 0.0 }),
-    HIGH_TEMPERATURE(TraitCondition { world, blockPos -> if (world.getBiome(blockPos).temperatureCategory == TemperatureCategory.HIGH) 1.0 else 0.0 }),
-    LOW_HUMIDITY(TraitCondition { world, blockPos -> if (world.getBiome(blockPos).humidityCategory == HumidityCategory.LOW) 1.0 else 0.0 }),
-    MEDIUM_HUMIDITY(TraitCondition { world, blockPos -> if (world.getBiome(blockPos).humidityCategory == HumidityCategory.MEDIUM) 1.0 else 0.0 }),
-    HIGH_HUMIDITY(TraitCondition { world, blockPos -> if (world.getBiome(blockPos).humidityCategory == HumidityCategory.HIGH) 1.0 else 0.0 }),
-    OUTDOOR(TraitCondition { world, blockPos -> if (blockPos.y >= world.getTopPosition(Heightmap.Type.MOTION_BLOCKING, blockPos).y) 1.0 else 0.0 }),
-    NATURAL(TraitCondition { world, blockPos -> if (world.getMagicPlantBlockEntity(blockPos)?.isNatural() == true) 1.0 else 0.0 }),
+    FLOOR_MOISTURE(Emoji.FLOOR_MOISTURE, { world, blockPos -> world.getMoisture(blockPos.down()) }),
+    FLOOR_CRYSTAL_ERG(Emoji.FLOOR_CRYSTAL_ERG, { world, blockPos -> world.getCrystalErg(blockPos.down()) }),
+    FLOOR_HARDNESS(Emoji.FLOOR_HARDNESS, { world, blockPos -> getFloorHardness(world, blockPos) }),
+    LIGHT(Emoji.LIGHT, { world, blockPos -> (world.getLightLevel(blockPos) - 8 atLeast 0) / 7.0 }),
+    DARKNESS(Emoji.DARKNESS, { world, blockPos -> ((15 - world.getLightLevel(blockPos)) - 8 atLeast 0) / 7.0 }),
+    LOW_TEMPERATURE(Emoji.LOW_TEMPERATURE, { world, blockPos -> if (world.getBiome(blockPos).temperatureCategory == TemperatureCategory.LOW) 1.0 else 0.0 }),
+    MEDIUM_TEMPERATURE(Emoji.MEDIUM_TEMPERATURE, { world, blockPos -> if (world.getBiome(blockPos).temperatureCategory == TemperatureCategory.MEDIUM) 1.0 else 0.0 }),
+    HIGH_TEMPERATURE(Emoji.HIGH_TEMPERATURE, { world, blockPos -> if (world.getBiome(blockPos).temperatureCategory == TemperatureCategory.HIGH) 1.0 else 0.0 }),
+    LOW_HUMIDITY(Emoji.LOW_HUMIDITY, { world, blockPos -> if (world.getBiome(blockPos).humidityCategory == HumidityCategory.LOW) 1.0 else 0.0 }),
+    MEDIUM_HUMIDITY(Emoji.MEDIUM_HUMIDITY, { world, blockPos -> if (world.getBiome(blockPos).humidityCategory == HumidityCategory.MEDIUM) 1.0 else 0.0 }),
+    HIGH_HUMIDITY(Emoji.HIGH_HUMIDITY, { world, blockPos -> if (world.getBiome(blockPos).humidityCategory == HumidityCategory.HIGH) 1.0 else 0.0 }),
+    OUTDOOR(Emoji.OUTDOOR, { world, blockPos -> if (blockPos.y >= world.getTopPosition(Heightmap.Type.MOTION_BLOCKING, blockPos).y) 1.0 else 0.0 }),
+    NATURAL(Emoji.NATURAL, { world, blockPos -> if (world.getMagicPlantBlockEntity(blockPos)?.isNatural() == true) 1.0 else 0.0 }),
+    ;
+
+    val traitCondition = object : TraitCondition {
+        override val emoji = emoji()
+        override fun getFactor(world: World, blockPos: BlockPos) = function(world, blockPos)
+    }
 }
 
 private fun getFloorHardness(world: World, blockPos: BlockPos): Double {
