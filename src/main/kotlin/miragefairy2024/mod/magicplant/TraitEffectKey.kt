c124a701613ec9ca43a4ec1dcc030e10072e6414
MirageFairy <idolm@ster>
2024-08-13 20:32:31 +0900

    internal: TraitEffectKey: add: name

--- src/main/kotlin/miragefairy2024/mod/magicplant/TraitEffectKey.kt
+++ src/main/kotlin/miragefairy2024/mod/magicplant/TraitEffectKey.kt
@@ -1,10 +1,6 @@
 package miragefairy2024.mod.magicplant
 
 import miragefairy2024.MirageFairy2024
-import miragefairy2024.ModContext
-import miragefairy2024.util.en
-import miragefairy2024.util.ja
-import miragefairy2024.util.text
 import net.fabricmc.fabric.api.event.registry.FabricRegistryBuilder
 import net.fabricmc.fabric.api.event.registry.RegistryAttribute
 import net.minecraft.registry.Registry
@@ -12,7 +8,6 @@ import net.minecraft.registry.RegistryKey
 import net.minecraft.text.Style
 import net.minecraft.text.Text
 import net.minecraft.util.Identifier
-import net.minecraft.util.Util
 
 // api
 
@@ -21,6 +16,7 @@ val traitEffectKeyRegistry: Registry<TraitEffectKey<*>> = FabricRegistryBuilder.
 
 abstract class TraitEffectKey<T : Any> {
     abstract val emoji: Text
+    abstract val name: Text
     abstract val sortValue: Double
     abstract val style: Style
     abstract fun getValue(level: Double): T
@@ -30,19 +26,7 @@ abstract class TraitEffectKey<T : Any> {
 }
 
 
-// init
-
-context(ModContext)
-fun TraitEffectKey<*>.enJa(enName: String, jaName: String) {
-    en { this.getTranslationKey() to enName }
-    ja { this.getTranslationKey() to jaName }
-}
-
-
 // util
 
 fun TraitEffectKey<*>.getIdentifier() = traitEffectKeyRegistry.getId(this)!!
 fun Identifier.toTraitEffectKey() = traitEffectKeyRegistry.get(this)
-
-fun TraitEffectKey<*>.getTranslationKey(): String = Util.createTranslationKey("mirageFairy2024.traitEffect", this.getIdentifier())
-fun TraitEffectKey<*>.getName() = text { translate(this@getName.getTranslationKey()) }
