5b92c9cddf052e013d6dcb91b9de0e016c051b7e
MirageFairy <idolm@ster>
2024-08-13 18:51:37 +0900

    format: TraitEffectKeyCard

--- src/main/kotlin/miragefairy2024/mod/magicplant/contents/TraitEffectKeyCard.kt
+++ src/main/kotlin/miragefairy2024/mod/magicplant/contents/TraitEffectKeyCard.kt
@@ -22,13 +22,16 @@ enum class TraitEffectKeyCard(
 ) {
     NUTRITION("nutrition", "NTR", "栄養値", Formatting.AQUA, false),
     ENVIRONMENT("environment", "ENV", "環境値", Formatting.GREEN, false),
-    GROWTH_BOOST("growth_boost", "GRW", "成長速度", Formatting.DARK_BLUE, false),
+
     SEEDS_PRODUCTION("seeds_production", "SEED", "種子生成", Formatting.RED, false),
     FRUITS_PRODUCTION("fruits_production", "FRUIT", "果実生成", Formatting.LIGHT_PURPLE, false),
     LEAVES_PRODUCTION("leaves_production", "LEAF", "葉面生成", Formatting.DARK_GREEN, false),
     RARE_PRODUCTION("rare_production", "RARE", "希少品生成", Formatting.GOLD, false),
-    PRODUCTION_BOOST("production_boost", "PRD", "生産能力", Formatting.DARK_RED, false),
     EXPERIENCE_PRODUCTION("experience_production", "XP", "経験値", Formatting.YELLOW, false),
+
+    GROWTH_BOOST("growth_boost", "GRW", "成長速度", Formatting.DARK_BLUE, false),
+    PRODUCTION_BOOST("production_boost", "PRD", "生産能力", Formatting.DARK_RED, false),
+
     FORTUNE_FACTOR("fortune_factor", "FTN", "幸運係数", Formatting.DARK_PURPLE, false),
     NATURAL_ABSCISSION("natural_abscission", "NA", "自然落果", Formatting.BLUE, true),
     ;
