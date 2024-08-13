bd86252f7bc9fc52aba650dbb267178a9bf72a5c
MirageFairy <idolm@ster>
2024-08-13 17:56:23 +0900

    refactor: add: sortedEntrySet

--- src/main/kotlin/miragefairy2024/util/Registry.kt
+++ src/main/kotlin/miragefairy2024/util/Registry.kt
@@ -3,6 +3,7 @@ package miragefairy2024.util
 import miragefairy2024.ModContext
 import miragefairy2024.ModEvents
 import net.minecraft.registry.Registry
+import net.minecraft.registry.RegistryKey
 import net.minecraft.util.Identifier
 
 context(ModContext)
@@ -11,3 +12,5 @@ fun <T> T.register(registry: Registry<T>, identifier: Identifier) {
         Registry.register(registry, identifier, this@register)
     }
 }
+
+val <T> Registry<T>.sortedEntrySet: List<Map.Entry<RegistryKey<T>, T>> get() = this.entrySet.sortedBy { it.key.value }
