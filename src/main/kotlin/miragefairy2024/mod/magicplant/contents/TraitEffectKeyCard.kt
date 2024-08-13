4ddc93f1e5215784c36edd9c424ace44efec6021
MirageFairy <idolm@ster>
2024-08-13 20:36:36 +0900

    VIEW: change: trait effect English name

--- src/main/kotlin/miragefairy2024/mod/magicplant/contents/TraitEffectKeyCard.kt
+++ src/main/kotlin/miragefairy2024/mod/magicplant/contents/TraitEffectKeyCard.kt
@@ -25,20 +25,20 @@ enum class TraitEffectKeyCard(
     style: Style,
     isLogScale: Boolean,
 ) {
-    NUTRITION("nutrition", Emoji.NUTRITION, "NTR", "栄養値", 1100.0, Style.EMPTY.withColor(0xCECECE), false),
-    ENVIRONMENT("environment", Emoji.MEDIUM_TEMPERATURE, "ENV", "環境値", 1200.0, Style.EMPTY.withColor(0xA0CC8E), false),
+    NUTRITION("nutrition", Emoji.NUTRITION, "Nutrition", "栄養値", 1100.0, Style.EMPTY.withColor(0xCECECE), false),
+    ENVIRONMENT("environment", Emoji.MEDIUM_TEMPERATURE, "Environment", "環境値", 1200.0, Style.EMPTY.withColor(0xA0CC8E), false),
 
-    SEEDS_PRODUCTION("seeds_production", Emoji.SEEDS_PRODUCTION, "SEED", "種子生成", 2100.0, Style.EMPTY.withColor(0xFFC587), false),
-    FRUITS_PRODUCTION("fruits_production", Emoji.FRUITS_PRODUCTION, "FRUIT", "果実生成", 2200.0, Style.EMPTY.withColor(0xFF87BF), false),
-    LEAVES_PRODUCTION("leaves_production", Emoji.LEAVES_PRODUCTION, "LEAF", "葉面生成", 2300.0, Style.EMPTY.withColor(0x32C900), false),
-    RARE_PRODUCTION("rare_production", Emoji.RARE_PRODUCTION, "RARE", "希少品生成", 2400.0, Style.EMPTY.withColor(0x00E2E2), false),
-    EXPERIENCE_PRODUCTION("experience_production", Emoji.LEVEL, "XP", "経験値", 2500.0, Style.EMPTY.withColor(0xEFEF00), false),
+    SEEDS_PRODUCTION("seeds_production", Emoji.SEEDS_PRODUCTION, "Seeds Production", "種子生成", 2100.0, Style.EMPTY.withColor(0xFFC587), false),
+    FRUITS_PRODUCTION("fruits_production", Emoji.FRUITS_PRODUCTION, "Fruits Production", "果実生成", 2200.0, Style.EMPTY.withColor(0xFF87BF), false),
+    LEAVES_PRODUCTION("leaves_production", Emoji.LEAVES_PRODUCTION, "Leaves Production", "葉面生成", 2300.0, Style.EMPTY.withColor(0x32C900), false),
+    RARE_PRODUCTION("rare_production", Emoji.RARE_PRODUCTION, "Rare Production", "希少品生成", 2400.0, Style.EMPTY.withColor(0x00E2E2), false),
+    EXPERIENCE_PRODUCTION("experience_production", Emoji.LEVEL, "Experience Production", "経験値", 2500.0, Style.EMPTY.withColor(0xEFEF00), false),
 
-    GROWTH_BOOST("growth_boost", Emoji.GROWTH_BOOST, "GRW", "成長速度", 3100.0, Style.EMPTY.withColor(0x00C600), false),
-    PRODUCTION_BOOST("production_boost", Emoji.PRODUCTION_BOOST, "PRD", "生産能力", 3200.0, Style.EMPTY.withColor(0xFF4242), false),
+    GROWTH_BOOST("growth_boost", Emoji.GROWTH_BOOST, "Growth Boost", "成長速度", 3100.0, Style.EMPTY.withColor(0x00C600), false),
+    PRODUCTION_BOOST("production_boost", Emoji.PRODUCTION_BOOST, "Production Boost", "生産能力", 3200.0, Style.EMPTY.withColor(0xFF4242), false),
 
-    FORTUNE_FACTOR("fortune_factor", Emoji.MANA, "FTN", "幸運係数", 4100.0, Style.EMPTY.withColor(0xFF4FFF), false),
-    NATURAL_ABSCISSION("natural_abscission", Emoji.NATURAL_ABSCISSION, "NA", "自然落果", 4200.0, Style.EMPTY.withColor(0x5959FF), true),
+    FORTUNE_FACTOR("fortune_factor", Emoji.MANA, "Fortune Factor", "幸運係数", 4100.0, Style.EMPTY.withColor(0xFF4FFF), false),
+    NATURAL_ABSCISSION("natural_abscission", Emoji.NATURAL_ABSCISSION, "Natural Abscission", "自然落果", 4200.0, Style.EMPTY.withColor(0x5959FF), true),
     CROSSBREEDING("crossbreeding", Emoji.CROSSBREEDING, "Crossbreeding", "交雑", 4300.0, Style.EMPTY.withColor(0xFFA011), true),
     ;
 
