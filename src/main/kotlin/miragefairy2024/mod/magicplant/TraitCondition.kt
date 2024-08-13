6f5cc18a34555e8f4365143404e1e600888a0ac4
MirageFairy <idolm@ster>
2024-08-13 20:15:35 +0900

    internal: TraitCondition: add: emoji

--- src/main/kotlin/miragefairy2024/mod/magicplant/TraitCondition.kt
+++ src/main/kotlin/miragefairy2024/mod/magicplant/TraitCondition.kt
@@ -1,10 +1,12 @@
 package miragefairy2024.mod.magicplant
 
+import net.minecraft.text.Text
 import net.minecraft.util.math.BlockPos
 import net.minecraft.world.World
 
 // api
 
-fun interface TraitCondition {
+interface TraitCondition {
+    val emoji: Text
     fun getFactor(world: World, blockPos: BlockPos): Double
 }
