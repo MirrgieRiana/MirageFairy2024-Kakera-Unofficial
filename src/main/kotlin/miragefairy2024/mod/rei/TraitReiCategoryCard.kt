6e24a4b8edfd3243e1755564db18d08691ab0ce0
MirageFairy <idolm@ster>
2024-08-14 21:15:36 +0900

    internal: TraitReiCategoryCard

--- src/main/kotlin/miragefairy2024/mod/rei/TraitReiCategoryCard.kt
+++ src/main/kotlin/miragefairy2024/mod/rei/TraitReiCategoryCard.kt
@@ -1,18 +1,27 @@
 package miragefairy2024.mod.rei
 
 import me.shedaniel.rei.api.common.display.basic.BasicDisplay
+import me.shedaniel.rei.api.common.entry.EntryIngredient
+import miragefairy2024.mod.magicplant.Trait
+import miragefairy2024.mod.magicplant.getIdentifier
+import miragefairy2024.mod.magicplant.toTrait
+import miragefairy2024.util.string
+import miragefairy2024.util.toIdentifier
 import mirrg.kotlin.hydrogen.Single
 
 object TraitReiCategoryCard : ReiCategoryCard<TraitReiCategoryCard.Display>("trait", "Trait", "特性") {
     override val serializer: Single<BasicDisplay.Serializer<Display>> by lazy {
-        Single(BasicDisplay.Serializer.ofRecipeLess({ _, _, tag ->
-            Display()
+        Single(BasicDisplay.Serializer.ofRecipeLess({ inputs, _, tag ->
+            Display(
+                inputs,
+                tag.getString("Trait").toIdentifier().toTrait()!!,
+            )
         }, { display, tag ->
-
+            tag.putString("Trait", display.trait.getIdentifier().string)
         }))
     }
 
-    class Display : BasicDisplay(listOf(), listOf()) {
+    class Display(inputs: List<EntryIngredient>, val trait: Trait) : BasicDisplay(inputs, listOf()) {
         override fun getCategoryIdentifier() = identifier.first
     }
 }
