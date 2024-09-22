1b44f9a003471006ee05a94d54b3573d88eb03ba
MirageFairy <idolm@ster>
2024-09-23 00:26:30 +0900

    refactor: extract: DamageTypeModule

---
+++ src/main/kotlin/miragefairy2024/mod/tool/DamageTypeModule.kt
@@ -0,0 +1,35 @@
+package miragefairy2024.mod.tool
+
+import miragefairy2024.MirageFairy2024
+import miragefairy2024.ModContext
+import miragefairy2024.util.en
+import miragefairy2024.util.ja
+import miragefairy2024.util.registerDamageTypeTagGeneration
+import miragefairy2024.util.registerDynamicGeneration
+import miragefairy2024.util.with
+import net.minecraft.entity.damage.DamageType
+import net.minecraft.registry.RegistryKeys
+import net.minecraft.registry.tag.DamageTypeTags
+
+object MagicDamageTypeCard {
+    val identifier = MirageFairy2024.identifier("magic")
+    val registryKey = RegistryKeys.DAMAGE_TYPE with identifier
+    val damageType = DamageType(identifier.toTranslationKey(), 0.1F)
+}
+
+context(ModContext)
+fun initDamageTypeModule() {
+    MagicDamageTypeCard.let { card ->
+        registerDynamicGeneration(card.registryKey) {
+            card.damageType
+        }
+        en { card.identifier.toTranslationKey("death.attack") to "%1\$s was killed by magic" }
+        ja { card.identifier.toTranslationKey("death.attack") to "%1\$sは魔法で殺された" }
+        en { card.identifier.toTranslationKey("death.attack", "player") to "%1\$s was killed by magic whilst trying to escape %2\$s" }
+        ja { card.identifier.toTranslationKey("death.attack", "player") to "%1\$sは%2\$sとの戦闘中に魔法で殺された" }
+        card.identifier.registerDamageTypeTagGeneration { DamageTypeTags.IS_PROJECTILE }
+        card.identifier.registerDamageTypeTagGeneration { DamageTypeTags.BYPASSES_ARMOR }
+        card.identifier.registerDamageTypeTagGeneration { DamageTypeTags.WITCH_RESISTANT_TO }
+        card.identifier.registerDamageTypeTagGeneration { DamageTypeTags.AVOIDS_GUARDIAN_THORNS }
+    }
+}
--- src/main/kotlin/miragefairy2024/mod/tool/ToolModule.kt
+++ src/main/kotlin/miragefairy2024/mod/tool/ToolModule.kt
@@ -18,31 +18,17 @@ import miragefairy2024.mod.tool.contents.createScythe
 import miragefairy2024.mod.tool.contents.createShootingStaff
 import miragefairy2024.mod.tool.contents.createShovel
 import miragefairy2024.mod.tool.contents.createSword
-import miragefairy2024.util.en
 import miragefairy2024.util.enJa
-import miragefairy2024.util.ja
 import miragefairy2024.util.on
 import miragefairy2024.util.register
-import miragefairy2024.util.registerDamageTypeTagGeneration
-import miragefairy2024.util.registerDynamicGeneration
 import miragefairy2024.util.registerItemGroup
 import miragefairy2024.util.registerModelGeneration
 import miragefairy2024.util.registerShapedRecipeGeneration
-import miragefairy2024.util.with
 import net.minecraft.data.client.Models
-import net.minecraft.entity.damage.DamageType
 import net.minecraft.item.Item
 import net.minecraft.item.Items
 import net.minecraft.registry.Registries
-import net.minecraft.registry.RegistryKeys
 import net.minecraft.registry.tag.BlockTags
-import net.minecraft.registry.tag.DamageTypeTags
-
-object MagicDamageTypeCard {
-    val identifier = MirageFairy2024.identifier("magic")
-    val registryKey = RegistryKeys.DAMAGE_TYPE with identifier
-    val damageType = DamageType(identifier.toTranslationKey(), 0.1F)
-}
 
 context(ModContext)
 fun initToolModule() {
@@ -62,20 +48,7 @@ fun initToolModule() {
     ShootingStaffItem.NOT_ENOUGH_EXPERIENCE_TRANSLATION.enJa()
     ShootingStaffItem.DESCRIPTION_TRANSLATION.enJa()
 
-    MagicDamageTypeCard.let { card ->
-        registerDynamicGeneration(card.registryKey) {
-            card.damageType
-        }
-        en { card.identifier.toTranslationKey("death.attack") to "%1\$s was killed by magic" }
-        ja { card.identifier.toTranslationKey("death.attack") to "%1\$sは魔法で殺された" }
-        en { card.identifier.toTranslationKey("death.attack", "player") to "%1\$s was killed by magic whilst trying to escape %2\$s" }
-        ja { card.identifier.toTranslationKey("death.attack", "player") to "%1\$sは%2\$sとの戦闘中に魔法で殺された" }
-        card.identifier.registerDamageTypeTagGeneration { DamageTypeTags.IS_PROJECTILE }
-        card.identifier.registerDamageTypeTagGeneration { DamageTypeTags.BYPASSES_ARMOR }
-        card.identifier.registerDamageTypeTagGeneration { DamageTypeTags.WITCH_RESISTANT_TO }
-        card.identifier.registerDamageTypeTagGeneration { DamageTypeTags.AVOIDS_GUARDIAN_THORNS }
-    }
-
+    initDamageTypeModule()
     initToolMaterialModule()
 }
 
