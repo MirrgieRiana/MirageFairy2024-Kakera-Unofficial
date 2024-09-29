b1444faee660468d17da4c9b6b68f74896200b21
MirageFairy <idolm@ster>
2024-09-30 00:22:18 +0900

    refactor: FairyStatueClientModule

--- src/client/kotlin/miragefairy2024/client/mod/FairyStatueClientModule.kt
+++ src/client/kotlin/miragefairy2024/client/mod/FairyStatueClientModule.kt
@@ -2,7 +2,7 @@ package miragefairy2024.client.mod
 
 import miragefairy2024.api.client.RenderItemHandler
 import miragefairy2024.client.util.stack
-import miragefairy2024.mod.fairy.FairyStatueCard
+import miragefairy2024.mod.fairy.FairyStatueBlockItem
 import miragefairy2024.mod.fairy.createFairyItemStack
 import miragefairy2024.mod.fairy.getFairyStatueMotif
 import net.minecraft.client.MinecraftClient
@@ -11,7 +11,7 @@ import net.minecraft.client.render.model.json.ModelTransformationMode
 
 fun initFairyStatueClientModule() {
     RenderItemHandler.listeners += RenderItemHandler { stack, renderMode, leftHanded, matrices, vertexConsumers, light, overlay, model ->
-        if (!stack.isOf(FairyStatueCard.item)) return@RenderItemHandler
+        if (stack.item !is FairyStatueBlockItem) return@RenderItemHandler
         val motif = stack.getFairyStatueMotif()
         val itemStack = motif?.createFairyItemStack() ?: return@RenderItemHandler
         matrices.stack {
