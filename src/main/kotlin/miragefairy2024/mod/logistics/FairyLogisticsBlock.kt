5296a6b12e53b0e9d092715a3976a1439a4c751c
MirageFairy <idolm@ster>
2024-10-10 05:32:56 +0900

    !!

--- src/main/kotlin/miragefairy2024/mod/logistics/FairyLogisticsBlock.kt
+++ src/main/kotlin/miragefairy2024/mod/logistics/FairyLogisticsBlock.kt
@@ -25,8 +25,6 @@ import miragefairy2024.util.with
 import net.fabricmc.fabric.api.`object`.builder.v1.block.FabricBlockSettings
 import net.minecraft.block.Block
 import net.minecraft.block.BlockState
-import net.minecraft.block.FacingBlock
-import net.minecraft.block.HorizontalFacingBlock
 import net.minecraft.block.ShapeContext
 import net.minecraft.block.piston.PistonBehavior
 import net.minecraft.item.BlockItem
@@ -35,8 +33,12 @@ import net.minecraft.item.ItemPlacementContext
 import net.minecraft.registry.Registries
 import net.minecraft.registry.tag.BlockTags
 import net.minecraft.state.StateManager
+import net.minecraft.state.property.DirectionProperty
+import net.minecraft.state.property.EnumProperty
+import net.minecraft.state.property.Properties
 import net.minecraft.util.BlockMirror
 import net.minecraft.util.BlockRotation
+import net.minecraft.util.StringIdentifiable
 import net.minecraft.util.math.BlockPos
 import net.minecraft.util.math.Direction
 import net.minecraft.util.shape.VoxelShape
@@ -58,16 +60,25 @@ abstract class FairyLogisticsBlockConfiguration {
         card.item.registerItemGroup(mirageFairy2024ItemGroupCard.itemGroupKey)
 
         card.block.registerVariantsBlockStateGeneration {
-            fun f(direction: Direction, suffix: String, y: BlockStateVariantRotation?): BlockStateVariantEntry {
-                return propertiesOf(HorizontalFacingBlock.FACING with direction) with BlockStateVariant(model = "block/" * card.block.getIdentifier() * suffix).with(y = y)
+            fun f(verticalFacing: FairyLogisticsBlock.VerticalFacing, direction: Direction, suffix: String, y: BlockStateVariantRotation): BlockStateVariantEntry {
+                return propertiesOf(
+                    FairyLogisticsBlock.VERTICAL_FACING with verticalFacing,
+                    FairyLogisticsBlock.FACING with direction,
+                ) with BlockStateVariant(model = "block/" * card.block.getIdentifier() * suffix).with(y = y)
             }
             listOf(
-                f(Direction.UP, "_up", null),
-                f(Direction.DOWN, "_down", null),
-                f(Direction.NORTH, "", BlockStateVariantRotation.R180),
-                f(Direction.EAST, "", BlockStateVariantRotation.R270),
-                f(Direction.SOUTH, "", BlockStateVariantRotation.R0),
-                f(Direction.WEST, "", BlockStateVariantRotation.R90),
+                f(FairyLogisticsBlock.VerticalFacing.UP, Direction.NORTH, "_up", BlockStateVariantRotation.R180),
+                f(FairyLogisticsBlock.VerticalFacing.UP, Direction.EAST, "_up", BlockStateVariantRotation.R270),
+                f(FairyLogisticsBlock.VerticalFacing.UP, Direction.SOUTH, "_up", BlockStateVariantRotation.R0),
+                f(FairyLogisticsBlock.VerticalFacing.UP, Direction.WEST, "_up", BlockStateVariantRotation.R90),
+                f(FairyLogisticsBlock.VerticalFacing.SIDE, Direction.NORTH, "", BlockStateVariantRotation.R180),
+                f(FairyLogisticsBlock.VerticalFacing.SIDE, Direction.EAST, "", BlockStateVariantRotation.R270),
+                f(FairyLogisticsBlock.VerticalFacing.SIDE, Direction.SOUTH, "", BlockStateVariantRotation.R0),
+                f(FairyLogisticsBlock.VerticalFacing.SIDE, Direction.WEST, "", BlockStateVariantRotation.R90),
+                f(FairyLogisticsBlock.VerticalFacing.DOWN, Direction.NORTH, "_down", BlockStateVariantRotation.R180),
+                f(FairyLogisticsBlock.VerticalFacing.DOWN, Direction.EAST, "_down", BlockStateVariantRotation.R270),
+                f(FairyLogisticsBlock.VerticalFacing.DOWN, Direction.SOUTH, "_down", BlockStateVariantRotation.R0),
+                f(FairyLogisticsBlock.VerticalFacing.DOWN, Direction.WEST, "_down", BlockStateVariantRotation.R90),
             )
         }
         card.block.registerCutoutRenderLayer()
@@ -92,23 +103,46 @@ open class FairyLogisticsBlockCard(val configuration: FairyLogisticsBlockConfigu
     val poemList = PoemList(configuration.tier).poem(configuration.poem)
 }
 
-open class FairyLogisticsBlock(settings: Settings) : FacingBlock(settings) {
+open class FairyLogisticsBlock(settings: Settings) : Block(settings) {
     companion object {
+        val VERTICAL_FACING: EnumProperty<VerticalFacing> = EnumProperty.of("vertical_facing", VerticalFacing::class.java)
+        val FACING: DirectionProperty = Properties.HORIZONTAL_FACING
         private val SHAPES: Array<VoxelShape> = arrayOf(
-            createCuboidShape(2.0, 2.0, 0.0, 14.0, 14.0, 8.0), // DOWN
-            createCuboidShape(2.0, 2.0, 0.0, 14.0, 14.0, 8.0), // UP
-            createCuboidShape(2.0, 2.0, 0.0, 14.0, 14.0, 8.0), // NORTH
+            // UP
+            createCuboidShape(2.0, 4.0, 8.0, 14.0, 16.0, 16.0), // SOUTH
+            createCuboidShape(0.0, 4.0, 2.0, 8.0, 16.0, 14.0), // WEST
+            createCuboidShape(2.0, 4.0, 0.0, 14.0, 16.0, 8.0), // NORTH
+            createCuboidShape(8.0, 4.0, 2.0, 16.0, 16.0, 14.0), // EAST
+
+            // SIDE
             createCuboidShape(2.0, 2.0, 8.0, 14.0, 14.0, 16.0), // SOUTH
             createCuboidShape(0.0, 2.0, 2.0, 8.0, 14.0, 14.0), // WEST
+            createCuboidShape(2.0, 2.0, 0.0, 14.0, 14.0, 8.0), // NORTH
             createCuboidShape(8.0, 2.0, 2.0, 16.0, 14.0, 14.0), // EAST
+
+            // DOWN
+            createCuboidShape(2.0, 0.0, 8.0, 14.0, 12.0, 16.0), // SOUTH
+            createCuboidShape(0.0, 0.0, 2.0, 8.0, 12.0, 14.0), // WEST
+            createCuboidShape(2.0, 0.0, 0.0, 14.0, 12.0, 8.0), // NORTH
+            createCuboidShape(8.0, 0.0, 2.0, 16.0, 12.0, 14.0), // EAST
         )
     }
 
+    enum class VerticalFacing(val string: String, val id: Int) : StringIdentifiable {
+        UP("up", 0),
+        SIDE("side", 1),
+        DOWN("down", 2),
+        ;
+
+        override fun asString() = string
+    }
+
     init {
-        defaultState = defaultState.with(FACING, Direction.NORTH)
+        defaultState = defaultState.with(VERTICAL_FACING, VerticalFacing.SIDE).with(FACING, Direction.NORTH)
     }
 
     override fun appendProperties(builder: StateManager.Builder<Block, BlockState>) {
+        builder.add(VERTICAL_FACING)
         builder.add(FACING)
     }
 
@@ -118,9 +152,16 @@ open class FairyLogisticsBlock(settings: Settings) : FacingBlock(settings) {
     @Suppress("OVERRIDE_DEPRECATION")
     override fun mirror(state: BlockState, mirror: BlockMirror): BlockState = state.rotate(mirror.getRotation(state[FACING]))
 
-    override fun getPlacementState(ctx: ItemPlacementContext): BlockState = defaultState.with(FACING, ctx.side.opposite)
+    override fun getPlacementState(ctx: ItemPlacementContext): BlockState {
+        val verticalFacing = when (ctx.side) {
+            Direction.UP -> VerticalFacing.DOWN
+            Direction.DOWN -> VerticalFacing.UP
+            else -> VerticalFacing.SIDE
+        }
+        return defaultState.with(VERTICAL_FACING, verticalFacing).with(FACING, ctx.horizontalPlayerFacing)
+    }
 
     @Suppress("OVERRIDE_DEPRECATION")
-    override fun getOutlineShape(state: BlockState, world: BlockView, pos: BlockPos, context: ShapeContext) = SHAPES[state[FACING].id]
+    override fun getOutlineShape(state: BlockState, world: BlockView, pos: BlockPos, context: ShapeContext) = SHAPES[4 * state[VERTICAL_FACING].id + state[FACING].horizontal]
 
 }
