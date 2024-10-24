d6ef108d465f310f45d1511c8f62739cd73ffd58
MirageFairy <idolm@ster>
2024-10-24 14:31:34 +0900

    !!

---
+++ src/main/kotlin/miragefairy2024/lib/RichMachineBlock.kt
@@ -0,0 +1,106 @@
+package miragefairy2024.lib
+
+import miragefairy2024.util.checkType
+import net.fabricmc.fabric.api.screenhandler.v1.ExtendedScreenHandlerFactory
+import net.minecraft.block.Block
+import net.minecraft.block.BlockEntityProvider
+import net.minecraft.block.BlockState
+import net.minecraft.block.entity.BlockEntity
+import net.minecraft.block.entity.BlockEntityTicker
+import net.minecraft.block.entity.BlockEntityType
+import net.minecraft.entity.LivingEntity
+import net.minecraft.entity.player.PlayerEntity
+import net.minecraft.entity.player.PlayerInventory
+import net.minecraft.item.ItemStack
+import net.minecraft.network.PacketByteBuf
+import net.minecraft.screen.NamedScreenHandlerFactory
+import net.minecraft.server.network.ServerPlayerEntity
+import net.minecraft.stat.Stats
+import net.minecraft.util.ActionResult
+import net.minecraft.util.Hand
+import net.minecraft.util.hit.BlockHitResult
+import net.minecraft.util.math.BlockPos
+import net.minecraft.world.World
+
+abstract class RichMachineBlock(private val arguments: Arguments) : Block(arguments.settings), BlockEntityProvider {
+
+    interface Arguments {
+        val settings: Settings
+        val blockEntityType: BlockEntityType<out RichMachineBlockEntity>
+    }
+
+
+    // Block Entity
+
+    abstract override fun createBlockEntity(pos: BlockPos, state: BlockState): RichMachineBlockEntity
+
+    @Suppress("DEPRECATION", "OVERRIDE_DEPRECATION")
+    override fun onSyncedBlockEvent(state: BlockState, world: World, pos: BlockPos, type: Int, data: Int): Boolean {
+        super.onSyncedBlockEvent(state, world, pos, type, data)
+        val blockEntity = world.getBlockEntity(pos) ?: return false
+        return blockEntity.onSyncedBlockEvent(type, data)
+    }
+
+    override fun onPlaced(world: World, pos: BlockPos, state: BlockState, placer: LivingEntity?, itemStack: ItemStack) {
+        if (itemStack.hasCustomName()) {
+            val blockEntity = world.getBlockEntity(pos) as? RichMachineBlockEntity ?: return
+            blockEntity.customName = itemStack.name
+        }
+    }
+
+    @Suppress("DEPRECATION", "OVERRIDE_DEPRECATION")
+    override fun onStateReplaced(state: BlockState, world: World, pos: BlockPos, newState: BlockState, moved: Boolean) {
+        if (!state.isOf(newState.block)) {
+            val blockEntity = world.getBlockEntity(pos) as? RichMachineBlockEntity
+            blockEntity?.dropItems()
+            super.onStateReplaced(state, world, pos, newState, moved)
+        }
+    }
+
+
+    // Move
+
+    override fun <T : BlockEntity> getTicker(world: World, state: BlockState, type: BlockEntityType<T>): BlockEntityTicker<T>? {
+        return if (world.isClient) {
+            checkType(type, arguments.blockEntityType) { world2, pos, state2, blockEntity ->
+                blockEntity.clientTick(world2, pos, state2)
+            }
+        } else {
+            checkType(type, arguments.blockEntityType) { world2, pos, state2, blockEntity ->
+                blockEntity.serverTick(world2, pos, state2)
+            }
+        }
+    }
+
+
+    // Gui
+
+    @Suppress("OVERRIDE_DEPRECATION")
+    override fun createScreenHandlerFactory(state: BlockState, world: World, pos: BlockPos) = world.getBlockEntity(pos) as? NamedScreenHandlerFactory
+
+    @Suppress("OVERRIDE_DEPRECATION")
+    override fun onUse(state: BlockState, world: World, pos: BlockPos, player: PlayerEntity, hand: Hand, hit: BlockHitResult): ActionResult {
+        if (world.isClient) return ActionResult.SUCCESS
+        val blockEntity = world.getBlockEntity(pos) as? RichMachineBlockEntity ?: return ActionResult.CONSUME
+        player.openHandledScreen(object : ExtendedScreenHandlerFactory {
+            override fun createMenu(syncId: Int, playerInventory: PlayerInventory, player: PlayerEntity) = blockEntity.createMenu(syncId, playerInventory, player)
+            override fun getDisplayName() = blockEntity.displayName
+            override fun writeScreenOpeningData(player: ServerPlayerEntity, buf: PacketByteBuf) = Unit
+        })
+        player.incrementStat(Stats.USED.getOrCreateStat(this.asItem()))
+        return ActionResult.CONSUME
+    }
+
+
+    // Status
+
+    @Suppress("OVERRIDE_DEPRECATION")
+    override fun hasComparatorOutput(state: BlockState) = true
+
+    @Suppress("OVERRIDE_DEPRECATION")
+    override fun getComparatorOutput(state: BlockState, world: World, pos: BlockPos): Int {
+        val blockEntity = world.getBlockEntity(pos) as? RichMachineBlockEntity ?: return 0
+        return blockEntity.getComparatorOutput()
+    }
+
+}
