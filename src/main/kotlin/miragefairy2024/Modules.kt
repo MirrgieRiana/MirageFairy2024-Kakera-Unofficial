439c4f592fb4d485baff601f6b3344c4428b587c
_ <tacticsrealize2@gmail.com>
2025-01-07 10:12:50 +0900

    ADD: debris

--- src/main/kotlin/miragefairy2024/Modules.kt
+++ src/main/kotlin/miragefairy2024/Modules.kt
@@ -7,6 +7,7 @@ import miragefairy2024.mod.haimeviska.initHaimeviskaModule
 import miragefairy2024.mod.initBiomeModule
 import miragefairy2024.mod.initBlockMaterialsModule
 import miragefairy2024.mod.initCommonModule
+import miragefairy2024.mod.initDebrisModule
 import miragefairy2024.mod.initEnchantmentModule
 import miragefairy2024.mod.initEntityModule
 import miragefairy2024.mod.initExtraPlayerDataModule
@@ -59,4 +60,5 @@ fun initModules() {
     initEnchantmentModule()
     initTelescopeModule()
     initFairyFountainModule()
+    initDebrisModule()
 }
