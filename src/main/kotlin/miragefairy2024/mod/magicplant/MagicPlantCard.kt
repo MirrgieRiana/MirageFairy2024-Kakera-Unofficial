0de04f526bfb31a3962292b9c8548f61db4f073b
_ <tacticsrealize2@gmail.com>
2025-01-07 17:48:02 +0900

    refactor: MagicPlantCard: settings -> configuration

--- src/main/kotlin/miragefairy2024/mod/magicplant/MagicPlantCard.kt
+++ src/main/kotlin/miragefairy2024/mod/magicplant/MagicPlantCard.kt
@@ -7,15 +7,15 @@ import net.minecraft.block.entity.BlockEntityType
 import net.minecraft.item.Item
 import net.minecraft.util.math.BlockPos
 
-open class MagicPlantCard<B : MagicPlantBlock>(private val settings: MagicPlantConfiguration<*, B>) {
-    val blockIdentifier = MirageFairy2024.identifier(settings.blockPath)
-    val itemIdentifier = MirageFairy2024.identifier(settings.itemPath)
-    val block = settings.createBlock()
-    private fun createBlockEntity(blockPos: BlockPos, blockState: BlockState) = MagicPlantBlockEntity(settings, blockPos, blockState)
+open class MagicPlantCard<B : MagicPlantBlock>(private val configuration: MagicPlantConfiguration<*, B>) {
+    val blockIdentifier = MirageFairy2024.identifier(configuration.blockPath)
+    val itemIdentifier = MirageFairy2024.identifier(configuration.itemPath)
+    val block = configuration.createBlock()
+    private fun createBlockEntity(blockPos: BlockPos, blockState: BlockState) = MagicPlantBlockEntity(configuration, blockPos, blockState)
     val blockEntityType: BlockEntityType<MagicPlantBlockEntity> = BlockEntityType(::createBlockEntity, setOf(block), null)
     val item = MagicPlantSeedItem(block, Item.Settings())
-    val possibleTraits = settings.possibleTraits
+    val possibleTraits = configuration.possibleTraits
 
     context(ModContext)
-    fun init() = settings.init()
+    fun init() = configuration.init()
 }
