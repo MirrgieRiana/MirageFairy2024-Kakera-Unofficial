761e5fa6503cdc423c2b09106272d088378729cc
_ <tacticsrealize2@gmail.com>
2025-01-14 15:52:24 +0900

    refactor: HorizontalFacingMachineBlock

--- src/main/kotlin/miragefairy2024/lib/HorizontalFacingMachineBlock.kt
+++ src/main/kotlin/miragefairy2024/lib/HorizontalFacingMachineBlock.kt
@@ -1,5 +1,3 @@
 package miragefairy2024.lib
 
-import net.fabricmc.fabric.api.`object`.builder.v1.block.FabricBlockSettings
-
-open class HorizontalFacingMachineBlock(private val card: MachineCard<*, *, *>, settings: FabricBlockSettings) : SimpleHorizontalFacingBlock(settings)
+open class HorizontalFacingMachineBlock(private val card: MachineCard<*, *, *>) : SimpleHorizontalFacingBlock(card.createBlockSettings())
