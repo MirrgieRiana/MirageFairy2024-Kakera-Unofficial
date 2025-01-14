9da8f9f8599882f50df9dcde53f4d862cad21bb4
_ <tacticsrealize2@gmail.com>
2025-01-14 16:05:44 +0900

    refactor: MachineCard: block

--- src/main/kotlin/miragefairy2024/lib/MachineCard.kt
+++ src/main/kotlin/miragefairy2024/lib/MachineCard.kt
@@ -3,8 +3,13 @@ package miragefairy2024.lib
 import net.fabricmc.fabric.api.`object`.builder.v1.block.FabricBlockSettings
 import net.minecraft.block.Block
 
+@Suppress("LeakingThis") // ブートストラップ問題のため解決不可能なので妥協する
 abstract class MachineCard<B : Block, E : MachineBlockEntity<E>, H : MachineScreenHandler> {
 
+    // Block
+
     abstract fun createBlockSettings(): FabricBlockSettings
+    abstract fun createBlock(): B
+    val block = createBlock()
 
 }
