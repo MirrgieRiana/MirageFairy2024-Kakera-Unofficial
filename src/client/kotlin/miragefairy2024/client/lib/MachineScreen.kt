960ff2e766bb724d285d1fa964e54f632357a0b7
_ <tacticsrealize2@gmail.com>
2025-01-14 15:41:45 +0900

    refactor: card

--- src/client/kotlin/miragefairy2024/client/lib/MachineScreen.kt
+++ src/client/kotlin/miragefairy2024/client/lib/MachineScreen.kt
@@ -1,8 +1,9 @@
 package miragefairy2024.client.lib
 
+import miragefairy2024.lib.MachineCard
 import miragefairy2024.lib.MachineScreenHandler
 import net.minecraft.client.gui.screen.ingame.HandledScreen
 import net.minecraft.entity.player.PlayerInventory
 import net.minecraft.text.Text
 
-abstract class MachineScreen<H : MachineScreenHandler>(handler: H, playerInventory: PlayerInventory, title: Text) : HandledScreen<H>(handler, playerInventory, title)
+abstract class MachineScreen<H : MachineScreenHandler>(private val card: MachineCard<*, *, *>, handler: H, playerInventory: PlayerInventory, title: Text) : HandledScreen<H>(handler, playerInventory, title)
