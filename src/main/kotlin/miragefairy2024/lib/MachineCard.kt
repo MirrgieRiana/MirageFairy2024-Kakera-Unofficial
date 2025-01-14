73f86c7315d02374f9dc6cf047c0d505f389d068
_ <tacticsrealize2@gmail.com>
2025-01-14 16:47:27 +0900

    refactor: MachineCard: BlockEntity

--- src/main/kotlin/miragefairy2024/lib/MachineCard.kt
+++ src/main/kotlin/miragefairy2024/lib/MachineCard.kt
@@ -2,10 +2,23 @@ package miragefairy2024.lib
 
 import net.fabricmc.fabric.api.`object`.builder.v1.block.FabricBlockSettings
 import net.minecraft.block.Block
+import net.minecraft.block.BlockState
+import net.minecraft.block.entity.BlockEntity
+import net.minecraft.block.entity.BlockEntityType
 import net.minecraft.util.Identifier
+import net.minecraft.util.math.BlockPos
 
 @Suppress("LeakingThis") // ブートストラップ問題のため解決不可能なので妥協する
 abstract class MachineCard<B : Block, E : MachineBlockEntity<E>, H : MachineScreenHandler> {
+    companion object {
+        context(C)
+        inline fun <C, reified E> BlockEntityAccessor(crossinline creator: (card: C, blockPos: BlockPos, blockState: BlockState) -> E) = object : BlockEntityAccessor<E> {
+            override fun create(blockPos: BlockPos, blockState: BlockState) = creator(this@C, blockPos, blockState)
+            override fun castOrThrow(blockEntity: BlockEntity?) = blockEntity as E
+            override fun castOrNull(blockEntity: BlockEntity?) = blockEntity as? E
+        }
+    }
+
 
     // Specification
 
@@ -18,5 +31,18 @@ abstract class MachineCard<B : Block, E : MachineBlockEntity<E>, H : MachineScre
     abstract fun createBlockSettings(): FabricBlockSettings
     abstract fun createBlock(): B
     val block = createBlock()
+
+
+    // BlockEntity
+
+    interface BlockEntityAccessor<E> {
+        fun create(blockPos: BlockPos, blockState: BlockState): E
+        fun castOrThrow(blockEntity: BlockEntity?): E
+        fun castOrNull(blockEntity: BlockEntity?): E?
+    }
+
+    abstract fun createBlockEntityAccessor(): BlockEntityAccessor<E>
+    val blockEntityAccessor = createBlockEntityAccessor()
+    val blockEntityType = BlockEntityType(blockEntityAccessor::create, setOf(block), null)
 
 }
