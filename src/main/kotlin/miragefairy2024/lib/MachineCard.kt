50a55be9508ca1acde07dea0fbc697c4eb908628
_ <tacticsrealize2@gmail.com>
2025-01-14 16:14:41 +0900

    refactor: MachineCard: Specification

--- src/main/kotlin/miragefairy2024/lib/MachineCard.kt
+++ src/main/kotlin/miragefairy2024/lib/MachineCard.kt
@@ -2,9 +2,16 @@ package miragefairy2024.lib
 
 import net.fabricmc.fabric.api.`object`.builder.v1.block.FabricBlockSettings
 import net.minecraft.block.Block
+import net.minecraft.util.Identifier
 
 @Suppress("LeakingThis") // ブートストラップ問題のため解決不可能なので妥協する
 abstract class MachineCard<B : Block, E : MachineBlockEntity<E>, H : MachineScreenHandler> {
+
+    // Specification
+
+    abstract fun createIdentifier(): Identifier
+    val identifier = createIdentifier()
+
 
     // Block
 
