cdcddc3d3cafab102f7beabfcecc8856dfbc4cb1
_ <tacticsrealize2@gmail.com>
2025-01-15 13:38:03 +0900

    refactor: MachineBlockEntity: gui

--- src/main/kotlin/miragefairy2024/lib/MachineBlockEntity.kt
+++ src/main/kotlin/miragefairy2024/lib/MachineBlockEntity.kt
@@ -4,22 +4,30 @@ import miragefairy2024.util.EMPTY_ITEM_STACK
 import miragefairy2024.util.readFromNbt
 import miragefairy2024.util.reset
 import miragefairy2024.util.writeToNbt
+import mirrg.kotlin.hydrogen.unit
 import net.minecraft.block.Block
 import net.minecraft.block.BlockState
 import net.minecraft.block.entity.LockableContainerBlockEntity
+import net.minecraft.entity.player.PlayerEntity
+import net.minecraft.entity.player.PlayerInventory
 import net.minecraft.inventory.Inventories
+import net.minecraft.inventory.Inventory
 import net.minecraft.inventory.SidedInventory
 import net.minecraft.item.ItemStack
 import net.minecraft.nbt.NbtCompound
 import net.minecraft.network.listener.ClientPlayPacketListener
 import net.minecraft.network.packet.Packet
 import net.minecraft.network.packet.s2c.play.BlockEntityUpdateS2CPacket
+import net.minecraft.screen.PropertyDelegate
+import net.minecraft.screen.ScreenHandler
+import net.minecraft.screen.ScreenHandlerContext
+import net.minecraft.text.Text
 import net.minecraft.util.ItemScatterer
 import net.minecraft.util.math.BlockPos
 import net.minecraft.util.math.Direction
 import net.minecraft.world.World
 
-abstract class MachineBlockEntity<E : MachineBlockEntity<E>>(private val card: MachineCard<*, *, *>, pos: BlockPos, state: BlockState) : LockableContainerBlockEntity(card.blockEntityType, pos, state), SidedInventory {
+abstract class MachineBlockEntity<E : MachineBlockEntity<E>>(private val card: MachineCard<*, E, *>, pos: BlockPos, state: BlockState) : LockableContainerBlockEntity(card.blockEntityType, pos, state), SidedInventory {
 
     interface InventorySlotConfiguration {
         fun isValid(itemStack: ItemStack): Boolean
@@ -28,6 +36,8 @@ abstract class MachineBlockEntity<E : MachineBlockEntity<E>>(private val card: M
         val isObservable: Boolean
         val dropItem: Boolean
     }
+
+    abstract fun getThis(): E
 
 
     // Data
@@ -108,5 +118,29 @@ abstract class MachineBlockEntity<E : MachineBlockEntity<E>>(private val card: M
     abstract fun serverTick(world: World, pos: BlockPos, state: BlockState)
 
     abstract fun clientTick(world: World, pos: BlockPos, state: BlockState)
+
+
+    // Gui
+
+    private val propertyDelegate = object : PropertyDelegate {
+        override fun size() = card.propertyConfigurations.size
+        override fun get(index: Int) = card.propertyConfigurations.getOrNull(index)?.let { it.encode(it.get(getThis())).toInt() } ?: 0
+        override fun set(index: Int, value: Int) = unit { card.propertyConfigurations.getOrNull(index)?.let { it.set(getThis(), it.decode(value.toShort())) } }
+    }
+
+    override fun canPlayerUse(player: PlayerEntity) = Inventory.canPlayerUse(this, player)
+
+    override fun getContainerName(): Text = card.block.name
+
+    override fun createScreenHandler(syncId: Int, playerInventory: PlayerInventory): ScreenHandler {
+        val arguments = MachineScreenHandler.Arguments(
+            syncId,
+            playerInventory,
+            this,
+            propertyDelegate,
+            ScreenHandlerContext.create(world, pos),
+        )
+        return card.createScreenHandler(arguments)
+    }
 
 }
