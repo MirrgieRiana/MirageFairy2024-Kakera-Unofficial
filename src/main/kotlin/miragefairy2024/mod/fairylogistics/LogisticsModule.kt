8f6e258eeebc6da9884d3fd26204e7129dd2a48e
_ <tacticsrealize2@gmail.com>
2025-01-16 12:32:10 +0900

    !!

--- src/main/kotlin/miragefairy2024/mod/fairylogistics/LogisticsModule.kt
+++ src/main/kotlin/miragefairy2024/mod/fairylogistics/LogisticsModule.kt
@@ -2,7 +2,14 @@ package miragefairy2024.mod.fairylogistics
 
 import miragefairy2024.ModContext
 
+val fairyLogisticsNodeCards: List<FairyLogisticsCard<*, *, *>> = listOf(
+    FairyMailboxCard,
+    FairyDistributionCenterCard,
+)
+
 context(ModContext)
 fun initFairyLogisticsModule() {
-
+    fairyLogisticsNodeCards.forEach {
+        it.init()
+    }
 }
