4932c5347c239a38343c45c9babe394b38495bbc
_ <tacticsrealize2@gmail.com>
2025-01-17 21:18:12 +0900

    !!

--- src/main/kotlin/miragefairy2024/mod/fairylogistics/FairyActiveConsumer.kt
+++ src/main/kotlin/miragefairy2024/mod/fairylogistics/FairyActiveConsumer.kt
@@ -1,12 +1,17 @@
 package miragefairy2024.mod.fairylogistics
 
 import miragefairy2024.lib.MachineScreenHandler
+import miragefairy2024.util.EnJa
 import net.minecraft.block.BlockState
 import net.minecraft.util.math.BlockPos
 import net.minecraft.util.math.Direction
 
 object FairyActiveConsumerCard : FairyLogisticsCard<FairyActiveConsumerBlock, FairyActiveConsumerBlockEntity, FairyActiveConsumerScreenHandler>() {
     override fun getPath() = "fairy_active_consumer"
+    override val tier = 3
+    override val name = EnJa("Fairy Active Consumer", "妖精の郵便受け")
+    override val poem = EnJa()
+    override val description = EnJa("The ordered items are delivered", "注文したアイテムが搬入される")
     override fun createBlock() = FairyActiveConsumerBlock(this)
     override fun createBlockEntityAccessor() = BlockEntityAccessor(::FairyActiveConsumerBlockEntity)
     override fun createScreenHandler(arguments: MachineScreenHandler.Arguments) = FairyActiveConsumerScreenHandler(this, arguments)
