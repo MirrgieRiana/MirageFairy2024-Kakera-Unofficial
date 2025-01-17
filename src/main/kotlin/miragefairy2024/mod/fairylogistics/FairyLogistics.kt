dded40ef63c75f116aa4bd13bc060173ad2903bd
_ <tacticsrealize2@gmail.com>
2025-01-17 13:04:47 +0900

    internal: add: FairyLogisticsCard, FairyActiveConsumerCard, FairyPassiveSupplierCard

---
+++ src/main/kotlin/miragefairy2024/mod/fairylogistics/FairyLogistics.kt
@@ -0,0 +1,10 @@
+package miragefairy2024.mod.fairylogistics
+
+import miragefairy2024.ModContext
+
+open class FairyLogisticsCard {
+    context(ModContext)
+    fun init() {
+
+    }
+}
--- src/main/kotlin/miragefairy2024/mod/fairylogistics/FairyLogisticsModule.kt
+++ src/main/kotlin/miragefairy2024/mod/fairylogistics/FairyLogisticsModule.kt
@@ -2,7 +2,14 @@ package miragefairy2024.mod.fairylogistics
 
 import miragefairy2024.ModContext
 
+val fairyLogisticsCards: List<FairyLogisticsCard> = listOf(
+    FairyPassiveSupplierCard,
+    FairyActiveConsumerCard,
+)
+
 context(ModContext)
 fun initFairyLogisticsModule() {
-
+    fairyLogisticsCards.forEach {
+        it.init()
+    }
 }
