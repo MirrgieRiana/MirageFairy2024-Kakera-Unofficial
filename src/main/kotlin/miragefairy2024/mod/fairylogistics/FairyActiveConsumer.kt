58addf4c39c06018d2ae1106c458dcdff8ee2292
_ <tacticsrealize2@gmail.com>
2025-01-19 17:09:02 +0900

    stub: FairyLogistics: block outline

--- src/main/kotlin/miragefairy2024/mod/fairylogistics/FairyActiveConsumer.kt
+++ src/main/kotlin/miragefairy2024/mod/fairylogistics/FairyActiveConsumer.kt
@@ -8,9 +8,12 @@ import miragefairy2024.util.on
 import miragefairy2024.util.registerShapedRecipeGeneration
 import net.minecraft.block.BlockState
 import net.minecraft.block.MapColor
+import net.minecraft.block.ShapeContext
 import net.minecraft.item.Items
 import net.minecraft.sound.BlockSoundGroup
 import net.minecraft.util.math.BlockPos
+import net.minecraft.util.shape.VoxelShape
+import net.minecraft.world.BlockView
 
 object FairyActiveConsumerCard : FairyLogisticsCard<FairyActiveConsumerBlock, FairyActiveConsumerBlockEntity, FairyActiveConsumerScreenHandler>() {
     override fun getPath() = "fairy_active_consumer"
@@ -39,7 +42,32 @@ object FairyActiveConsumerCard : FairyLogisticsCard<FairyActiveConsumerBlock, Fa
     }
 }
 
-class FairyActiveConsumerBlock(card: FairyActiveConsumerCard) : FairyLogisticsBlock(card)
+class FairyActiveConsumerBlock(card: FairyActiveConsumerCard) : FairyLogisticsBlock(card) {
+    companion object {
+        private val SHAPES: Array<VoxelShape> = arrayOf(
+            // UP
+            createCuboidShape(2.0, 4.0, 8.0, 14.0, 16.0, 16.0), // SOUTH
+            createCuboidShape(0.0, 4.0, 2.0, 8.0, 16.0, 14.0), // WEST
+            createCuboidShape(2.0, 4.0, 0.0, 14.0, 16.0, 8.0), // NORTH
+            createCuboidShape(8.0, 4.0, 2.0, 16.0, 16.0, 14.0), // EAST
+
+            // SIDE
+            createCuboidShape(2.0, 2.0, 8.0, 14.0, 14.0, 16.0), // SOUTH
+            createCuboidShape(0.0, 2.0, 2.0, 8.0, 14.0, 14.0), // WEST
+            createCuboidShape(2.0, 2.0, 0.0, 14.0, 14.0, 8.0), // NORTH
+            createCuboidShape(8.0, 2.0, 2.0, 16.0, 14.0, 14.0), // EAST
+
+            // DOWN
+            createCuboidShape(2.0, 0.0, 8.0, 14.0, 12.0, 16.0), // SOUTH
+            createCuboidShape(0.0, 0.0, 2.0, 8.0, 12.0, 14.0), // WEST
+            createCuboidShape(2.0, 0.0, 0.0, 14.0, 12.0, 8.0), // NORTH
+            createCuboidShape(8.0, 0.0, 2.0, 16.0, 12.0, 14.0), // EAST
+        )
+    }
+
+    @Suppress("OVERRIDE_DEPRECATION")
+    override fun getOutlineShape(state: BlockState, world: BlockView, pos: BlockPos, context: ShapeContext) = SHAPES[4 * state[VERTICAL_FACING].id + state[FACING].horizontal]
+}
 
 class FairyActiveConsumerBlockEntity(card: FairyActiveConsumerCard, pos: BlockPos, state: BlockState) : FairyLogisticsBlockEntity<FairyActiveConsumerBlockEntity>(card, pos, state) {
     override fun getThis() = this
--- src/main/kotlin/miragefairy2024/mod/fairylogistics/FairyPassiveSupplier.kt
+++ src/main/kotlin/miragefairy2024/mod/fairylogistics/FairyPassiveSupplier.kt
@@ -9,11 +9,14 @@ import miragefairy2024.util.registerBlockTagGeneration
 import miragefairy2024.util.registerShapedRecipeGeneration
 import net.minecraft.block.BlockState
 import net.minecraft.block.MapColor
+import net.minecraft.block.ShapeContext
 import net.minecraft.item.Items
 import net.minecraft.registry.tag.BlockTags
 import net.minecraft.registry.tag.ItemTags
 import net.minecraft.sound.BlockSoundGroup
 import net.minecraft.util.math.BlockPos
+import net.minecraft.util.shape.VoxelShape
+import net.minecraft.world.BlockView
 
 object FairyPassiveSupplierCard : FairyLogisticsCard<FairyPassiveSupplierBlock, FairyPassiveSupplierBlockEntity, FairyPassiveSupplierScreenHandler>() {
     override fun getPath() = "fairy_passive_supplier"
@@ -45,7 +48,32 @@ object FairyPassiveSupplierCard : FairyLogisticsCard<FairyPassiveSupplierBlock, 
     }
 }
 
-class FairyPassiveSupplierBlock(card: FairyPassiveSupplierCard) : FairyLogisticsBlock(card)
+class FairyPassiveSupplierBlock(card: FairyPassiveSupplierCard) : FairyLogisticsBlock(card) {
+    companion object {
+        private val SHAPES: Array<VoxelShape> = arrayOf(
+            // UP
+            createCuboidShape(2.0, 4.0, 8.0, 14.0, 16.0, 16.0), // SOUTH
+            createCuboidShape(0.0, 4.0, 2.0, 8.0, 16.0, 14.0), // WEST
+            createCuboidShape(2.0, 4.0, 0.0, 14.0, 16.0, 8.0), // NORTH
+            createCuboidShape(8.0, 4.0, 2.0, 16.0, 16.0, 14.0), // EAST
+
+            // SIDE
+            createCuboidShape(2.0, 2.0, 8.0, 14.0, 14.0, 16.0), // SOUTH
+            createCuboidShape(0.0, 2.0, 2.0, 8.0, 14.0, 14.0), // WEST
+            createCuboidShape(2.0, 2.0, 0.0, 14.0, 14.0, 8.0), // NORTH
+            createCuboidShape(8.0, 2.0, 2.0, 16.0, 14.0, 14.0), // EAST
+
+            // DOWN
+            createCuboidShape(2.0, 0.0, 8.0, 14.0, 12.0, 16.0), // SOUTH
+            createCuboidShape(0.0, 0.0, 2.0, 8.0, 12.0, 14.0), // WEST
+            createCuboidShape(2.0, 0.0, 0.0, 14.0, 12.0, 8.0), // NORTH
+            createCuboidShape(8.0, 0.0, 2.0, 16.0, 12.0, 14.0), // EAST
+        )
+    }
+
+    @Suppress("OVERRIDE_DEPRECATION")
+    override fun getOutlineShape(state: BlockState, world: BlockView, pos: BlockPos, context: ShapeContext) = SHAPES[4 * state[VERTICAL_FACING].id + state[FACING].horizontal]
+}
 
 class FairyPassiveSupplierBlockEntity(card: FairyPassiveSupplierCard, pos: BlockPos, state: BlockState) : FairyLogisticsBlockEntity<FairyPassiveSupplierBlockEntity>(card, pos, state) {
     override fun getThis() = this
