3ab8769fea1d5c771c8e00448fab1d2b57984148
_ <tacticsrealize2@gmail.com>
2025-01-31 14:01:17 +0900

    internal: obtain

--- src/main/kotlin/miragefairy2024/util/Player.kt
+++ src/main/kotlin/miragefairy2024/util/Player.kt
@@ -1,12 +1,11 @@
 package miragefairy2024.util
 
 import net.minecraft.entity.Entity
-import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.ItemStack
 import net.minecraft.util.Hand
 
 /** @param itemStack 内部でコピーされるため、破壊されません。 */
-fun PlayerEntity.obtain(itemStack: ItemStack) {
+fun Entity.obtain(itemStack: ItemStack) {
     val itemEntity = this.dropStack(itemStack.copy(), 0.5F)
     if (itemEntity != null) {
         itemEntity.resetPickupDelay()
