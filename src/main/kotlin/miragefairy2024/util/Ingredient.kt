c475f7de129934960b57d634134db107a7409000
_ <tacticsrealize2@gmail.com>
2025-01-31 14:33:31 +0900

    refactor: add: CustomIngredientSerializer.register

--- src/main/kotlin/miragefairy2024/util/Ingredient.kt
+++ src/main/kotlin/miragefairy2024/util/Ingredient.kt
@@ -1,5 +1,8 @@
 package miragefairy2024.util
 
+import miragefairy2024.ModContext
+import miragefairy2024.ModEvents
+import net.fabricmc.fabric.api.recipe.v1.ingredient.CustomIngredientSerializer
 import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
 import net.minecraft.recipe.Ingredient
@@ -8,3 +11,10 @@ import net.minecraft.registry.tag.TagKey
 fun Item.toIngredient(): Ingredient = Ingredient.ofItems(this)
 fun ItemStack.toIngredient(): Ingredient = Ingredient.ofStacks(this)
 fun TagKey<Item>.toIngredient(): Ingredient = Ingredient.fromTag(this)
+
+context(ModContext)
+fun CustomIngredientSerializer<*>.register() {
+    ModEvents.onInitialize {
+        CustomIngredientSerializer.register(this)
+    }
+}
