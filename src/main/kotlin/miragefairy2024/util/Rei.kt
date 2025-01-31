cc0f64446d401ece8ef5f49e49f83792bab7cb8e
_ <tacticsrealize2@gmail.com>
2025-01-31 14:34:29 +0900

    internal: add: Point.minus

--- src/main/kotlin/miragefairy2024/util/Rei.kt
+++ src/main/kotlin/miragefairy2024/util/Rei.kt
@@ -14,3 +14,4 @@ fun Iterable<EntryStack<*>>.toEntryIngredient(): EntryIngredient = EntryIngredie
 
 
 operator fun Point.plus(other: Point) = Point(this.x + other.x, this.y + other.y)
+operator fun Point.minus(other: Point) = Point(this.x - other.x, this.y - other.y)
