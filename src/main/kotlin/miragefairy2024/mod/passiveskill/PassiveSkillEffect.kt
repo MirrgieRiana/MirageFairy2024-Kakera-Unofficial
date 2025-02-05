76ffe1f1859062a99daed7e616a70cd332d2f1e3
_ <tacticsrealize2@gmail.com>
2025-02-06 07:42:35 +0900

    internal: add: PassiveSkillResult sync

--- src/main/kotlin/miragefairy2024/mod/passiveskill/PassiveSkillEffect.kt
+++ src/main/kotlin/miragefairy2024/mod/passiveskill/PassiveSkillEffect.kt
@@ -14,9 +14,14 @@ import miragefairy2024.mod.passiveskill.effects.MendingPassiveSkillEffect
 import miragefairy2024.mod.passiveskill.effects.MiningSpeedPassiveSkillEffect
 import miragefairy2024.mod.passiveskill.effects.RegenerationPassiveSkillEffect
 import miragefairy2024.mod.passiveskill.effects.StatusEffectPassiveSkillEffect
+import miragefairy2024.util.boolean
+import miragefairy2024.util.double
+import miragefairy2024.util.get
 import miragefairy2024.util.register
+import miragefairy2024.util.wrapper
 import mirrg.kotlin.hydrogen.formatAs
 import net.minecraft.entity.attribute.EntityAttributes
+import net.minecraft.nbt.NbtCompound
 
 context(ModContext)
 fun initPassiveSkillEffect() {
@@ -56,10 +61,14 @@ abstract class DoublePassiveSkillEffectCard(path: String) : PassiveSkillEffectCa
     override val unit = 0.0
     override fun castOrThrow(value: Any?) = value as Double
     override fun combine(a: Double, b: Double) = a + b
+    override fun fromNbt(nbt: NbtCompound) = nbt.wrapper["value"].double.get()!!
+    override fun toNbt(value: Double) = NbtCompound().also { it.wrapper["value"].double.set(value) }
 }
 
 abstract class BooleanPassiveSkillEffectCard(path: String) : PassiveSkillEffectCard<Boolean>(path) {
     override val unit = false
     override fun castOrThrow(value: Any?) = value as Boolean
     override fun combine(a: Boolean, b: Boolean) = a || b
+    override fun fromNbt(nbt: NbtCompound) = nbt.wrapper["value"].boolean.get()!!
+    override fun toNbt(value: Boolean) = NbtCompound().also { it.wrapper["value"].boolean.set(value) }
 }
