c80e7ed807839c476fdf6f5eb2db67cf9904b739
_ <tacticsrealize2@gmail.com>
2025-02-10 15:21:04 +0900

    refactor: passiveskill

--- src/main/kotlin/miragefairy2024/mod/passiveskill/PassiveSkill.kt
+++ src/main/kotlin/miragefairy2024/mod/passiveskill/PassiveSkill.kt
@@ -1,50 +1,26 @@
 package miragefairy2024.mod.passiveskill
 
-import miragefairy2024.MirageFairy2024
-import miragefairy2024.ModContext
 import miragefairy2024.mod.fairy.Motif
-import net.fabricmc.fabric.api.event.registry.FabricRegistryBuilder
-import net.fabricmc.fabric.api.event.registry.RegistryAttribute
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.ItemStack
 import net.minecraft.nbt.NbtCompound
-import net.minecraft.registry.Registry
-import net.minecraft.registry.RegistryKey
 import net.minecraft.text.Text
 import net.minecraft.util.Identifier
 import net.minecraft.util.math.BlockPos
 import net.minecraft.world.World
 
-context(ModContext)
-fun initPassiveSkillModule() {
-    initPassiveSkillConditions()
-    initPassiveSkillEffect()
-    initPassiveSkillExecution()
-}
-
-
 interface PassiveSkillProvider {
     fun getPassiveSkill(itemStack: ItemStack): PassiveSkill?
 }
 
-
 class PassiveSkill(val providerId: Identifier, val motif: Motif?, val rare: Double, val count: Double, val specifications: List<PassiveSkillSpecification<*>>)
 
-
 class PassiveSkillSpecification<T>(val conditions: List<PassiveSkillCondition>, val effect: PassiveSkillEffect<T>, val valueProvider: (mana: Double) -> T)
-
-
-class PassiveSkillContext(val world: World, val blockPos: BlockPos, val player: PlayerEntity)
-
 
 interface PassiveSkillCondition {
     val text: Text
     fun test(context: PassiveSkillContext, level: Double, mana: Double): Boolean
 }
-
-
-val passiveSkillEffectRegistryKey: RegistryKey<Registry<PassiveSkillEffect<*>>> = RegistryKey.ofRegistry(MirageFairy2024.identifier("passive_skill_effect"))
-val passiveSkillEffectRegistry: Registry<PassiveSkillEffect<*>> = FabricRegistryBuilder.createSimple(passiveSkillEffectRegistryKey).attribute(RegistryAttribute.SYNCED).buildAndRegister()
 
 interface PassiveSkillEffect<T> {
     val isPreprocessor: Boolean
@@ -56,3 +32,5 @@ interface PassiveSkillEffect<T> {
     fun toNbt(value: T): NbtCompound = NbtCompound()
     fun update(context: PassiveSkillContext, oldValue: T, newValue: T)
 }
+
+class PassiveSkillContext(val world: World, val blockPos: BlockPos, val player: PlayerEntity)
--- src/main/kotlin/miragefairy2024/mod/passiveskill/PassiveSkillModule.kt
+++ src/main/kotlin/miragefairy2024/mod/passiveskill/PassiveSkillModule.kt
@@ -3,6 +3,8 @@ package miragefairy2024.mod.passiveskill
 import miragefairy2024.MirageFairy2024
 import miragefairy2024.ModContext
 import miragefairy2024.ModEvents
+import miragefairy2024.mod.passiveskill.conditions.ItemFoodIngredientPassiveSkillCondition
+import miragefairy2024.mod.passiveskill.conditions.SimplePassiveSkillConditionCard
 import miragefairy2024.mod.passiveskill.effects.CollectionPassiveSkillEffect
 import miragefairy2024.mod.passiveskill.effects.ElementPassiveSkillEffect
 import miragefairy2024.mod.passiveskill.effects.EntityAttributePassiveSkillEffect
@@ -16,15 +18,34 @@ import miragefairy2024.mod.passiveskill.effects.RegenerationPassiveSkillEffect
 import miragefairy2024.mod.passiveskill.effects.StatusEffectPassiveSkillEffect
 import miragefairy2024.util.boolean
 import miragefairy2024.util.double
+import miragefairy2024.util.enJa
 import miragefairy2024.util.get
 import miragefairy2024.util.register
 import miragefairy2024.util.wrapper
 import mirrg.kotlin.hydrogen.formatAs
+import net.fabricmc.fabric.api.event.registry.FabricRegistryBuilder
+import net.fabricmc.fabric.api.event.registry.RegistryAttribute
 import net.minecraft.entity.attribute.EntityAttributes
 import net.minecraft.nbt.NbtCompound
+import net.minecraft.registry.Registry
+import net.minecraft.registry.RegistryKey
+
+val passiveSkillEffectRegistryKey: RegistryKey<Registry<PassiveSkillEffect<*>>> = RegistryKey.ofRegistry(MirageFairy2024.identifier("passive_skill_effect"))
+val passiveSkillEffectRegistry: Registry<PassiveSkillEffect<*>> = FabricRegistryBuilder.createSimple(passiveSkillEffectRegistryKey).attribute(RegistryAttribute.SYNCED).buildAndRegister()
 
 context(ModContext)
-fun initPassiveSkillEffect() {
+fun initPassiveSkillModule() {
+
+    // Condition
+
+    SimplePassiveSkillConditionCard.entries.forEach { card ->
+        card.init()
+    }
+    ItemFoodIngredientPassiveSkillCondition.translation.enJa()
+
+
+    // Effect
+
     listOf(
         ManaBoostPassiveSkillEffect,
         EntityAttributePassiveSkillEffect,
@@ -44,6 +65,12 @@ fun initPassiveSkillEffect() {
     ModEvents.onInitialize {
         EntityAttributePassiveSkillEffect.FORMATTERS[EntityAttributes.GENERIC_MOVEMENT_SPEED] = { (it / 0.1) * 100 formatAs "%+.0f%%" }
     }
+
+
+    // Execution
+
+    initPassiveSkillExecution()
+
 }
 
 abstract class PassiveSkillEffectCard<T>(path: String) : PassiveSkillEffect<T> {
