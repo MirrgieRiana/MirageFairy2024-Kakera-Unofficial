85af975def31d87e8fd117f2e881d0cbcd4bb40d
_ <tacticsrealize2@gmail.com>
2025-02-18 16:53:54 +0900

    refactor: add: IntToolEffectType

--- src/main/kotlin/miragefairy2024/mod/tool/effects/ToolEffectType.kt
+++ src/main/kotlin/miragefairy2024/mod/tool/effects/ToolEffectType.kt
@@ -1,8 +1,14 @@
 package miragefairy2024.mod.tool.effects
 
 import miragefairy2024.mod.tool.ToolEffectType
+import mirrg.kotlin.hydrogen.max
 
 abstract class BooleanToolEffectType : ToolEffectType<Boolean> {
     override fun castOrThrow(value: Any?) = value as Boolean
     override fun merge(a: Boolean, b: Boolean) = a || b
+}
+
+abstract class IntToolEffectType : ToolEffectType<Int> {
+    override fun castOrThrow(value: Any?) = value as Int
+    override fun merge(a: Int, b: Int) = a max b
 }
