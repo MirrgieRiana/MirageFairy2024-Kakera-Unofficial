54ede3fda5e8fae90e127f9bd100e46cab9b2678
_ <tacticsrealize2@gmail.com>
2025-02-18 22:36:55 +0900

    internal: add: toWavAsOgg

--- src/main/kotlin/miragefairy2024/wave/Ogg.kt
+++ src/main/kotlin/miragefairy2024/wave/Ogg.kt
@@ -30,3 +30,28 @@ fun ByteArray.toOggAsWav(): ByteArray {
         result.await()
     }
 }
+
+fun ByteArray.toWavAsOgg(): ByteArray {
+    val processBuilder = ProcessBuilder("bash", "-c", "ffmpeg -i - -f wav -")
+    val process = processBuilder.start()
+    return runBlocking {
+        launch(Dispatchers.IO) {
+            process.outputStream.use { output ->
+                output.write(this@toWavAsOgg)
+            }
+        }
+        val err = async(Dispatchers.IO) {
+            process.errorStream.use { input ->
+                input.readBytes()
+            }
+        }
+        val result = async(Dispatchers.IO) {
+            process.inputStream.use { input ->
+                input.readBytes()
+            }
+        }
+        val returnCode = process.waitFor()
+        if (returnCode != 0) throw IOException("Process exit: $returnCode\n${err.await().toString(Charsets.UTF_8).replace("""\n+\Z""".toRegex(), "")}")
+        result.await()
+    }
+}
