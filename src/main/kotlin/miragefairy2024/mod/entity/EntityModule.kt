41d43990e292366a65cec080db1cacdd17144b58
_ <tacticsrealize2@gmail.com>
2025-02-24 00:08:51 +0900

    refactor: initEntityModule

--- src/main/kotlin/miragefairy2024/mod/entity/EntityModule.kt
+++ src/main/kotlin/miragefairy2024/mod/entity/EntityModule.kt
@@ -1,13 +1,8 @@
 package miragefairy2024.mod.entity
 
 import miragefairy2024.ModContext
-import miragefairy2024.util.register
-import net.minecraft.registry.Registries
 
 context(ModContext)
 fun initEntityModule() {
-    AntimatterBoltCard.let { card ->
-        card.entityType.register(Registries.ENTITY_TYPE, card.identifier)
-        card.init()
-    }
+    AntimatterBoltCard.init()
 }
