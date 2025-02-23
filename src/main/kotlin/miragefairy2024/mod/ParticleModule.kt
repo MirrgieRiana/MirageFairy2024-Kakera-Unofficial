f76e7a5cded228b6225a992785bf0d1ec5d394a2
_ <tacticsrealize2@gmail.com>
2025-02-23 23:24:32 +0900

    VIEW: add: haimeviska particle

--- src/main/kotlin/miragefairy2024/mod/ParticleModule.kt
+++ src/main/kotlin/miragefairy2024/mod/ParticleModule.kt
@@ -23,6 +23,7 @@ enum class ParticleTypeCard(
     MIRAGE_FLOUR("mirage_flour", "mirage_flour", false),
     ATTRACTING_MAGIC("attracting_magic", "mission", false),
     AURA("aura", "mission", false),
+    HAIMEVISKA_BLOSSOM("haimeviska_blossom", "haimeviska_blossom", false)
     ;
 
     val identifier = MirageFairy2024.identifier(path)
