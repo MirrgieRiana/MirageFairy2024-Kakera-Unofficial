fb6f2dc0d521172e10a4543194e5f90b614c5b82
_ <tacticsrealize2@gmail.com>
2025-02-24 00:08:13 +0900

    refactor: entity

---
+++ src/main/kotlin/miragefairy2024/mod/entity/AntimatterBolt.kt
@@ -0,0 +1,172 @@
+package miragefairy2024.mod.entity
+
+import miragefairy2024.MirageFairy2024
+import miragefairy2024.ModContext
+import miragefairy2024.mod.ParticleTypeCard
+import miragefairy2024.mod.SoundEventCard
+import miragefairy2024.mod.tool.MagicDamageTypeCard
+import miragefairy2024.util.getValue
+import miragefairy2024.util.isServer
+import miragefairy2024.util.registerEntityTypeTagGeneration
+import miragefairy2024.util.setValue
+import net.fabricmc.fabric.api.`object`.builder.v1.entity.FabricEntityTypeBuilder
+import net.minecraft.entity.Entity
+import net.minecraft.entity.EntityDimensions
+import net.minecraft.entity.EntityStatuses
+import net.minecraft.entity.EntityType
+import net.minecraft.entity.LivingEntity
+import net.minecraft.entity.SpawnGroup
+import net.minecraft.entity.data.DataTracker
+import net.minecraft.entity.data.TrackedData
+import net.minecraft.entity.data.TrackedDataHandlerRegistry
+import net.minecraft.entity.projectile.ProjectileEntity
+import net.minecraft.entity.projectile.ProjectileUtil
+import net.minecraft.nbt.NbtCompound
+import net.minecraft.registry.tag.EntityTypeTags
+import net.minecraft.sound.SoundCategory
+import net.minecraft.util.hit.EntityHitResult
+import net.minecraft.util.hit.HitResult
+import net.minecraft.util.math.Vec3d
+import net.minecraft.world.World
+
+object AntimatterBoltCard {
+    val spawnGroup = SpawnGroup.MISC
+    val width = 0.5F
+    val height = 0.5F
+    fun createEntity(entityType: EntityType<AntimatterBoltEntity>, world: World) = AntimatterBoltEntity(entityType, world)
+    val identifier = MirageFairy2024.identifier("antimatter_bolt")
+    val entityType: EntityType<AntimatterBoltEntity> = FabricEntityTypeBuilder.create(spawnGroup) { entityType, world -> createEntity(entityType, world) }
+        .dimensions(EntityDimensions.fixed(width, height))
+        .build()
+
+    context(ModContext)
+    fun init() {
+        entityType.registerEntityTypeTagGeneration { EntityTypeTags.IMPACT_PROJECTILES }
+    }
+}
+
+class AntimatterBoltEntity(entityType: EntityType<out AntimatterBoltEntity>, world: World) : ProjectileEntity(entityType, world) {
+    companion object {
+        val DAMAGE: TrackedData<Float> = DataTracker.registerData(AntimatterBoltEntity::class.java, TrackedDataHandlerRegistry.FLOAT)
+        val MAX_DISTANCE: TrackedData<Float> = DataTracker.registerData(AntimatterBoltEntity::class.java, TrackedDataHandlerRegistry.FLOAT)
+    }
+
+
+    var damage by DAMAGE
+    var maxDistance by MAX_DISTANCE
+
+    override fun initDataTracker() {
+        dataTracker.startTracking(DAMAGE, 0F)
+        dataTracker.startTracking(MAX_DISTANCE, 0F)
+    }
+
+    override fun writeCustomDataToNbt(nbt: NbtCompound) {
+        nbt.putFloat("Damage", damage)
+        nbt.putFloat("MaxDistance", maxDistance)
+    }
+
+    override fun readCustomDataFromNbt(nbt: NbtCompound) {
+        damage = nbt.getFloat("Damage")
+        maxDistance = nbt.getFloat("MaxDistance")
+    }
+
+
+    private var prevPos: Vec3d? = null
+    override fun tick() {
+        super.tick()
+
+        // 距離判定
+        val movingDistance = velocity.length().toFloat()
+        val stopped = if (movingDistance >= maxDistance) { // 10 >= 1 → velocity *= 0.1( = 1 / 10)
+            if (movingDistance >= 0.001F) velocity = velocity.multiply((maxDistance / movingDistance).toDouble())
+            true
+        } else {
+            false
+        }
+        maxDistance -= movingDistance
+
+        // 衝突判定
+        val hitResult = ProjectileUtil.getCollision(this) { canHit(it) }
+        if (hitResult.type != HitResult.Type.MISS) onCollision(hitResult)
+        if (isRemoved) return
+
+        checkBlockCollision()
+        if (isRemoved) return
+
+        val vec3d = velocity
+        val nextX = x + vec3d.x
+        val nextY = y + vec3d.y
+        val nextZ = z + vec3d.z
+
+        updateRotation()
+
+        // 水に触れると消滅
+        if (isInsideWaterOrBubbleColumn) {
+            discard()
+            return
+        }
+
+        // パーティクル
+        prevPos?.let { prevPos ->
+            repeat(2) { i ->
+                world.addParticle(
+                    ParticleTypeCard.AURA.particleType,
+                    prevPos.x + (x - prevPos.x) * (i / 3.0),
+                    prevPos.y + (y - prevPos.y) * (i / 3.0),
+                    prevPos.z + (z - prevPos.z) * (i / 3.0),
+                    (random.nextDouble() - 0.5) * 0.02,
+                    (random.nextDouble() - 0.5) * 0.02,
+                    (random.nextDouble() - 0.5) * 0.02,
+                )
+            }
+        }
+
+        // 距離判定
+        if (stopped) {
+            discard()
+            return
+        }
+
+        // 位置更新
+        prevPos = pos
+        setPosition(nextX, nextY, nextZ)
+
+    }
+
+    override fun onCollision(hitResult: HitResult) {
+        super.onCollision(hitResult)
+        if (world.isServer) {
+            world.sendEntityStatus(this, EntityStatuses.PLAY_DEATH_SOUND_OR_ADD_PROJECTILE_HIT_PARTICLES)
+            discard()
+        }
+    }
+
+    override fun onEntityHit(entityHitResult: EntityHitResult) {
+        super.onEntityHit(entityHitResult)
+        attack(entityHitResult.entity)
+    }
+
+    private fun attack(entity: Entity) {
+        entity.damage(world.damageSources.create(MagicDamageTypeCard.registryKey, this, owner as? LivingEntity), damage)
+    }
+
+    override fun handleStatus(status: Byte) {
+        super.handleStatus(status)
+        if (status == EntityStatuses.PLAY_DEATH_SOUND_OR_ADD_PROJECTILE_HIT_PARTICLES) {
+
+            world.playSound(x, y, z, SoundEventCard.MAGIC_HIT.soundEvent, SoundCategory.NEUTRAL, 0.5F, 0.90F + (world.random.nextFloat() - 0.5F) * 0.3F, true)
+
+            for (i in 0..7) {
+                world.addParticle(
+                    ParticleTypeCard.AURA.particleType,
+                    x, y, z,
+                    (random.nextDouble() - 0.5) * 0.3,
+                    (random.nextDouble() - 0.5) * 0.3,
+                    (random.nextDouble() - 0.5) * 0.3,
+                )
+            }
+        }
+
+    }
+
+}
