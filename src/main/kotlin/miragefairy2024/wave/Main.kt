bf0a31a3174174adbc03d022971fe4e5979b9403
_ <tacticsrealize2@gmail.com>
2025-03-03 09:05:20 +0900

    wave: refactor: GenerateMain

--- src/main/kotlin/miragefairy2024/wave/Main.kt
+++ src/main/kotlin/miragefairy2024/wave/Main.kt
@@ -7,67 +7,70 @@ object Main
 
 private val logger = getLogger(Main::class.java)
 
-fun main() {
+object GenerateMain {
+    @JvmStatic
+    fun main(args: Array<String>) {
 
-    convert(File("""./src/main/resources/assets/miragefairy2024/sounds/magic_hit.8b.1000ms.8000a.scr.png"""))
+        convert(File("""./src/main/resources/assets/miragefairy2024/sounds/magic_hit.8b.1000ms.8000a.scr.png"""))
 
-    //File("""./src/main/resources/assets/miragefairy2024/sounds""").listFiles()?.forEach { inputFile ->
-    //    if (inputFile.name.endsWith(".scr.png")) convert(inputFile)
-    //}
+        //File("""./src/main/resources/assets/miragefairy2024/sounds""").listFiles()?.forEach { inputFile ->
+        //    if (inputFile.name.endsWith(".scr.png")) convert(inputFile)
+        //}
 
-}
+    }
 
-fun convert(inputFile: File) {
-    logger.info("inputFile    = $inputFile")
+    fun convert(inputFile: File) {
+        logger.info("inputFile    = $inputFile")
 
-    when (inputFile.extension) {
-        "wav" -> {
-            inputFile
-                .readBytes()
-                .toWaveformAsWav()
-                .toSpectrogram(8, 1 / 4000.0)
-                .writeTo(inputFile.resolveSibling("${inputFile.nameWithoutExtension}.png"))
-        }
+        when (inputFile.extension) {
+            "wav" -> {
+                inputFile
+                    .readBytes()
+                    .toWaveformAsWav()
+                    .toSpectrogram(8, 1 / 4000.0)
+                    .writeTo(inputFile.resolveSibling("${inputFile.nameWithoutExtension}.png"))
+            }
 
-        "png" -> {
-            val m = """(.*)\.(\d+)b\.(\d+)ms\.(\d+)a\.scr\.png""".toRegex().matchEntire(inputFile.name)!!
+            "png" -> {
+                val m = """(.*)\.(\d+)b\.(\d+)ms\.(\d+)a\.scr\.png""".toRegex().matchEntire(inputFile.name)!!
 
 
-            val baseName = m.groups[1]?.value!!
-            logger.info("baseName     = $baseName")
-            val outputFile = inputFile.resolveSibling("$baseName.ogg")
-            logger.info("outputFile   = $outputFile")
+                val baseName = m.groups[1]?.value!!
+                logger.info("baseName     = $baseName")
+                val outputFile = inputFile.resolveSibling("$baseName.ogg")
+                logger.info("outputFile   = $outputFile")
 
-            val bits = m.groups[2]?.value!!.toInt()
-            logger.info("bits         = $bits")
-            val windowSize = 1 shl bits
-            logger.info("windowSize   = $windowSize")
-            val imageHeight = windowSize / 2 + 1
-            logger.info("imageHeight  = $imageHeight")
+                val bits = m.groups[2]?.value!!.toInt()
+                logger.info("bits         = $bits")
+                val windowSize = 1 shl bits
+                logger.info("windowSize   = $windowSize")
+                val imageHeight = windowSize / 2 + 1
+                logger.info("imageHeight  = $imageHeight")
 
-            val samplingRate = 48000
-            logger.info("samplingRate = $samplingRate")
+                val samplingRate = 48000
+                logger.info("samplingRate = $samplingRate")
 
-            val duration = m.groups[3]?.value!!.toDouble() * 0.001
-            logger.info("duration     = $duration")
-            val imageWidth = (samplingRate * duration + (windowSize - 1)).toInt()
-            logger.info("imageWidth   = $imageWidth")
+                val duration = m.groups[3]?.value!!.toDouble() * 0.001
+                logger.info("duration     = $duration")
+                val imageWidth = (samplingRate * duration + (windowSize - 1)).toInt()
+                logger.info("imageWidth   = $imageWidth")
 
-            //val amplifier = 4000.0
-            val amplifier = m.groups[4]?.value!!.toDouble()
-            logger.info("amplifier    = $amplifier")
+                //val amplifier = 4000.0
+                val amplifier = m.groups[4]?.value!!.toDouble()
+                logger.info("amplifier    = $amplifier")
 
-            inputFile
-                .readSpectrogram()
-                .resize(imageWidth, imageHeight)
-                .generatePhaseSimple()
-                .generatePhaseGriffinLim(20, { it.toWaveform(bits, 1.0) }, { it.toSpectrogram(bits, 1.0) })
-                .also { it.writeTo(inputFile.resolveSibling("dump.png")) }
-                .toWaveform(bits, 1 / amplifier)
-                .toWavByteArray()
-                .also { it.writeTo(inputFile.resolveSibling("dump.wav")) }
-                .toOggAsWav()
-                .writeTo(outputFile)
+                inputFile
+                    .readSpectrogram()
+                    .resize(imageWidth, imageHeight)
+                    .generatePhaseSimple()
+                    .generatePhaseGriffinLim(20, { it.toWaveform(bits, 1.0) }, { it.toSpectrogram(bits, 1.0) })
+                    .also { it.writeTo(inputFile.resolveSibling("dump.png")) }
+                    .toWaveform(bits, 1 / amplifier)
+                    .toWavByteArray()
+                    .also { it.writeTo(inputFile.resolveSibling("dump.wav")) }
+                    .toOggAsWav()
+                    .writeTo(outputFile)
+            }
         }
     }
 }
