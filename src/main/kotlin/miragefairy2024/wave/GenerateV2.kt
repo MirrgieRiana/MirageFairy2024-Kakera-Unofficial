f89ad74457269a7e3917dccee0839bee3d607d5b
_ <tacticsrealize2@gmail.com>
2025-03-06 16:50:24 +0900

    wave: refactor: GenerateV2

--- src/main/kotlin/miragefairy2024/wave/GenerateV2.kt
+++ src/main/kotlin/miragefairy2024/wave/GenerateV2.kt
@@ -15,13 +15,13 @@ object GenerateV2 {
             .readBytes()
             .toWavAsOgg()
             .toWaveformAsWav()
-            .also { waveform ->
-                if (waveform.doubleArray.size > samplesPerSecond * 10) throw RuntimeException("too long: ${inputFile.name} (${waveform.doubleArray.size.toDouble() / samplesPerSecond.toDouble()}s)")
-                logger.info("Waveform Length: ${waveform.doubleArray.size} samples") // 56256 == 56511 - 255
+            .also {
+                if (it.doubleArray.size > samplesPerSecond * 10) throw RuntimeException("too long: ${inputFile.name} (${it.doubleArray.size.toDouble() / samplesPerSecond.toDouble()}s)")
+                logger.info("Waveform Length: ${it.doubleArray.size} samples") // 56256 == 56511 - 255
             }
             .toSpectrogram(8, 1 / 800.0)
-            .also { spectrogram ->
-                logger.info("Image Size: ${spectrogram.bufferedImage.width} x ${spectrogram.bufferedImage.height}") // 56511 x 129
+            .also {
+                logger.info("Image Size: ${it.bufferedImage.width} x ${it.bufferedImage.height}") // 56511 x 129
                 // 画像の幅のうち、255は固定の部分に使われる
                 // 画像の幅から-255した部分の長さが実際のサンプル数に相当する
                 // 画像の幅が+128される度にサンプル数が+48000になってほしい
