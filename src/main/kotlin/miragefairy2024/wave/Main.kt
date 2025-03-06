2e672cd1d7e488cfe38e49c1e5e717d81f9743ee
_ <tacticsrealize2@gmail.com>
2025-03-06 16:43:08 +0900

    wave: refactor: degenerate

--- src/main/kotlin/miragefairy2024/wave/Main.kt
+++ src/main/kotlin/miragefairy2024/wave/Main.kt
@@ -192,7 +192,7 @@ object DegenerateMain {
     }
 
     private fun degenerate(inputFile: File, outputFile: File) {
-        val waveform = inputFile
+        inputFile
             .readBytes()
             .toWavAsOgg()
             .toWaveformAsWav()
@@ -200,16 +200,13 @@ object DegenerateMain {
                 if (waveform.doubleArray.size > samplesPerSecond * 10) throw RuntimeException("too long: ${inputFile.name} (${waveform.doubleArray.size.toDouble() / samplesPerSecond.toDouble()}s)")
                 logger.info("Waveform Length: ${waveform.doubleArray.size} samples") // 56256 == 56511 - 255
             }
-
-        val spectrogram = waveform.toSpectrogram(8, 1 / 800.0)
+            .toSpectrogram(8, 1 / 800.0)
             .also { spectrogram ->
                 logger.info("Image Size: ${spectrogram.bufferedImage.width} x ${spectrogram.bufferedImage.height}") // 56511 x 129
                 // 画像の幅のうち、255は固定の部分に使われる
                 // 画像の幅から-255した部分の長さが実際のサンプル数に相当する
                 // 画像の幅が+128される度にサンプル数が+48000になってほしい
             }
-
-        spectrogram
             .removePhase()
             .let {
                 it
