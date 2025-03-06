e3c1bd569d9ec320641d4f31ac5d9da3e2d3320a
_ <tacticsrealize2@gmail.com>
2025-03-06 17:43:05 +0900

    wave: refactor: GenerateV2

--- src/main/kotlin/miragefairy2024/wave/GenerateV2.kt
+++ src/main/kotlin/miragefairy2024/wave/GenerateV2.kt
@@ -9,6 +9,11 @@ object GenerateV2 {
 
     private val samplesPerSecond = 48000
     private val pixelsPerSecond = 128
+    private val bits = 8
+    private val amplifier = 1600.0
+
+    private val saveImageHeight = 256 / 2
+    private val internalImageHeight = 256 / 2 + 1
 
     fun degenerate(inputFile: File, outputFile: File) {
         inputFile
@@ -19,7 +24,7 @@ object GenerateV2 {
                 if (it.doubleArray.size > samplesPerSecond * 10) throw RuntimeException("too long: ${inputFile.name} (${it.doubleArray.size.toDouble() / samplesPerSecond.toDouble()}s)")
                 logger.info("Waveform Length: ${it.doubleArray.size} samples") // 56256 == 56511 - 255
             }
-            .toSpectrogram(8, 1 / 1600.0)
+            .toSpectrogram(bits, 1 / amplifier)
             .also {
                 logger.info("Image Size: ${it.bufferedImage.width} x ${it.bufferedImage.height}") // 56511 x 129
                 // 画像の幅のうち、255は固定の部分に使われる
@@ -27,7 +32,7 @@ object GenerateV2 {
                 // 画像の幅が+128される度にサンプル数が+48000になってほしい
             }
             .removePhase()
-            .let { it.resize((it.bufferedImage.width.toDouble() / samplesPerSecond.toDouble() * pixelsPerSecond.toDouble()).roundToInt(), 128) } // 151 x 128
+            .let { it.resize((it.bufferedImage.width.toDouble() / samplesPerSecond.toDouble() * pixelsPerSecond.toDouble()).roundToInt(), saveImageHeight) } // 151 x 128
             .toLogScale()
             .writeTo(outputFile.also { it.mkdirsParentOrThrow() })
     }
@@ -36,10 +41,10 @@ object GenerateV2 {
         inputFile
             .readSpectrogram()
             .fromLogScale()
-            .let { it.resize((it.bufferedImage.width.toDouble() / pixelsPerSecond.toDouble() * samplesPerSecond.toDouble()).roundToInt(), 129) }
+            .let { it.resize((it.bufferedImage.width.toDouble() / pixelsPerSecond.toDouble() * samplesPerSecond.toDouble()).roundToInt(), internalImageHeight) }
             .generatePhase()
-            .generatePhaseGriffinLim(5, { it.toWaveform(8, 1.0) }, { it.toSpectrogram(8, 1.0) })
-            .toWaveform(8, 1 / 1600.0)
+            .generatePhaseGriffinLim(5, { it.toWaveform(bits, 1.0) }, { it.toSpectrogram(bits, 1.0) })
+            .toWaveform(bits, 1 / amplifier)
             .toWavByteArray()
             .toOggAsWav()
             .writeTo(outputFile.also { it.mkdirsParentOrThrow() })
