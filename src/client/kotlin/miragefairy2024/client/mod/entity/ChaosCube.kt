c09d8f8c381f79aac699c09cd4a1ebac0f606ac6
_ <tacticsrealize2@gmail.com>
2025-03-18 11:25:33 +0900

    !!

---
+++ src/client/kotlin/miragefairy2024/client/mod/entity/ChaosCube.kt
@@ -0,0 +1,83 @@
+package miragefairy2024.client.mod.entity
+
+import miragefairy2024.MirageFairy2024
+import miragefairy2024.client.util.stack
+import miragefairy2024.mod.entity.ChaosCubeCard
+import miragefairy2024.mod.entity.ChaosCubeEntity
+import net.minecraft.client.model.ModelPart
+import net.minecraft.client.model.ModelPartBuilder
+import net.minecraft.client.model.ModelTransform
+import net.minecraft.client.render.VertexConsumer
+import net.minecraft.client.render.entity.EntityRendererFactory
+import net.minecraft.client.render.entity.MobEntityRenderer
+import net.minecraft.client.render.entity.model.EntityModel
+import net.minecraft.client.util.math.MatrixStack
+import net.minecraft.util.math.MathHelper
+import org.joml.Quaternionf
+import kotlin.math.atan
+import kotlin.math.sqrt
+
+class ChaosCubeEntityRenderer(context: EntityRendererFactory.Context) : MobEntityRenderer<ChaosCubeEntity, ChaosCubeEntityModel>(context, ChaosCubeEntityModel(context.getPart(ROOT.entityModelLayer)), 0.5F) {
+    companion object {
+        val ROOT = EntityModelLayerCard(ChaosCubeCard.identifier, "root", 64, 64) {
+            (0..1).forEach { x ->
+                (0..1).forEach { y ->
+                    (0..1).forEach { z ->
+                        it.addChild(
+                            "part${x * 4 + y * 2 + z}",
+                            ModelPartBuilder.create().uv(0 + x * 20, 0 + y * 20 + (1 - z) * 10).cuboid(0F, 0F, 0F, 5F, 5F, 5F),
+                            ModelTransform.pivot(-5.5F + 6F * x, -5.5F + 6F * y, -5.5F + 6F * z),
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    private val texture = MirageFairy2024.identifier("textures/entity/chaos_cube.png")
+
+    override fun getTexture(entity: ChaosCubeEntity) = texture
+}
+
+class ChaosCubeEntityModel(private val root: ModelPart) : EntityModel<ChaosCubeEntity>() {
+    companion object {
+        private val ROLL = atan(sqrt(2.0F) / 2.0F)
+        private val PITCH = 45.0F / 180F * MathHelper.PI
+    }
+
+    private val parts = (0 until 8).map { root.getChild("part$it") }.toTypedArray()
+    private var segments: Array<ChaosCubeEntity.Segment>? = null
+    private val rotations = (0 until 8).map { Quaternionf() }.toTypedArray()
+
+    override fun setAngles(entity: ChaosCubeEntity, limbAngle: Float, limbDistance: Float, animationProgress: Float, headYaw: Float, headPitch: Float) {
+        val delta = animationProgress - entity.age
+
+        val f = animationProgress * 2 * MathHelper.PI / 100
+        root.yaw = f
+
+        segments = entity.segments
+        repeat(8) { i ->
+            rotations[i].x = MathHelper.lerp(delta, entity.segments[i].prevRotation.x, entity.segments[i].rotation.x)
+            rotations[i].y = MathHelper.lerp(delta, entity.segments[i].prevRotation.y, entity.segments[i].rotation.y)
+            rotations[i].z = MathHelper.lerp(delta, entity.segments[i].prevRotation.z, entity.segments[i].rotation.z)
+            rotations[i].w = MathHelper.lerp(delta, entity.segments[i].prevRotation.w, entity.segments[i].rotation.w)
+        }
+    }
+
+    override fun render(matrices: MatrixStack, vertices: VertexConsumer, light: Int, overlay: Int, red: Float, green: Float, blue: Float, alpha: Float) {
+        if (!root.visible) return
+        matrices.stack {
+            matrices.translate(0F, 0.5F, 0F)
+            root.rotate(matrices)
+            matrices.multiply(Quaternionf().rotateZYX(ROLL, 0F, PITCH))
+
+            repeat(8) { i ->
+                val segment = segments!![i]
+                matrices.stack {
+                    matrices.multiply(rotations[i])
+                    parts[segment.partIndex].render(matrices, vertices, light, overlay, red, green, blue, alpha)
+                }
+            }
+        }
+    }
+}
--- src/main/kotlin/miragefairy2024/wave/Main.kt
+++ src/main/kotlin/miragefairy2024/wave/Main.kt
@@ -7,9 +7,13 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import mirrg.kotlin.gson.hydrogen.toJsonElement
 import mirrg.kotlin.gson.hydrogen.toJsonWrapper
+import mirrg.kotlin.hydrogen.join
+import mirrg.kotlin.hydrogen.max
 import mirrg.kotlin.slf4j.hydrogen.getFileLogger
+import java.awt.Color
 import java.awt.Dimension
 import java.io.File
+import javax.imageio.ImageIO
 import javax.swing.JButton
 import javax.swing.JFrame
 
@@ -28,6 +32,100 @@ object GenerateV1Main {
     }
 }
 
+
+object TmpMain {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val dir = File("./src/main/resources/assets/miragefairy2024/sounds")
+        val image1 = ImageIO.read(dir.resolve("無題.png"))
+        val image2 = ImageIO.read(dir.resolve("無題2.png"))
+        val conversions: List<Pair<String, (x: Int, y: Int, r: Int, g: Int, b: Int) -> Any?>> = listOf(
+            "r" to { x, y, r, g, b -> r },
+            "g" to { x, y, r, g, b -> g },
+            "b" to { x, y, r, g, b -> b },
+            "h" to { x, y, r, g, b -> Color.RGBtoHSB(r, g, b, null)[0] },
+            "s" to { x, y, r, g, b -> Color.RGBtoHSB(r, g, b, null)[1] },
+            "v" to { x, y, r, g, b -> Color.RGBtoHSB(r, g, b, null)[2] },
+            "La" to { x, y, r, g, b -> (r + g + b) / (255 * 3).toDouble() },
+            "Lm" to { x, y, r, g, b -> r max g max b },
+            "L2a" to { x, y, r, g, b -> (r * 76.0 + g * 149.0 + b * 29.0) / (255 * (76 + 149 + 29)).toDouble() },
+        )
+        println(
+            listOf(
+                "x",
+                "y",
+                *conversions.map { (name, _) -> "${name}1" }.toTypedArray(),
+                *conversions.map { (name, _) -> "${name}2" }.toTypedArray(),
+            ).join(",")
+        )
+        (0 until 16).forEach { x ->
+            (0 until 16).forEach { y ->
+                val rgb1 = image1.getRGB(x, y)
+                val rgb2 = image2.getRGB(x, y)
+                val r1 = rgb1 shr 16 and 0xFF
+                val g1 = rgb1 shr 8 and 0xFF
+                val b1 = rgb1 and 0xFF
+                val r2 = rgb2 shr 16 and 0xFF
+                val g2 = rgb2 shr 8 and 0xFF
+                val b2 = rgb2 and 0xFF
+                println(
+                    listOf(
+                        x.toString(),
+                        y.toString(),
+                        *conversions.map { (_, f) -> f(x, y, r1, g1, b1).toString() }.toTypedArray(),
+                        *conversions.map { (_, f) -> f(x, y, r2, g2, b2).toString() }.toTypedArray(),
+                    ).join(",")
+                )
+            }
+        }
+    }
+}
+
+object GenerateV2Main {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val tasks = mutableListOf<() -> Unit>()
+
+        val dir = File("./src/main/resources/assets/miragefairy2024/sounds")
+        fun generate(baseFileName: String) {
+            tasks += {
+                GenerateV2.generate(dir.resolve("$baseFileName.sc2.png"), dir.resolve("$baseFileName.ogg"), dumpWav = true)
+            }
+        }
+
+        //generate("entity_chaos_cube_ambient_1")
+        //generate("entity_chaos_cube_ambient_2")
+        //generate("entity_chaos_cube_hurt_1")
+        //generate("entity_chaos_cube_death_1")
+        //generate("001")
+        //generate("002")
+        //generate("004")
+        //generate("005")
+        //generate("006")
+        //generate("007")
+        //generate("008")
+        //generate("009")
+        //generate("010")
+        //generate("011")
+        //generate("012")
+        //generate("013")
+        generate("014")
+
+        //tasks += {
+        //    val baseFileName = "009"
+        //    GenerateV2.generate(dir.resolve("$baseFileName.scr.png"), dir.resolve("$baseFileName.ogg"), dumpWav = true)
+        //    GenerateV2.degenerate(dir.resolve("$baseFileName.ogg"), dir.resolve("$baseFileName.2.png"))
+        //}
+
+        runBlocking {
+            tasks.forEach { task ->
+                launch(Dispatchers.Default) {
+                    task()
+                }
+            }
+        }
+    }
+}
 
 private val assetsDir = File(System.getProperty("user.home")).resolve(".gradle/caches/fabric-loom/assets")
 private val outputDir = File("./build/minecraft_assets")
