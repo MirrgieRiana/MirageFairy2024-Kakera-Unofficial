e35b7f329a6c0f3bc82ca3fc5eb283bb1b31e5c1
_ <tacticsrealize2@gmail.com>
2025-03-21 15:55:33 +0900

    internal: add: MAP_TRANSLATION

--- src/main/kotlin/miragefairy2024/mod/structure/StructureModule.kt
+++ src/main/kotlin/miragefairy2024/mod/structure/StructureModule.kt
@@ -1,9 +1,15 @@
 package miragefairy2024.mod.structure
 
 import miragefairy2024.ModContext
+import miragefairy2024.util.Translation
+import miragefairy2024.util.enJa
+
+val MAP_TRANSLATION = Translation({ "item.miragefairy2024.filled_map.template" }, "%s Map", "%sの地図")
 
 context(ModContext)
 fun initStructureModule() {
+    MAP_TRANSLATION.enJa()
+
     initUnlimitedJigsaw()
     DripstoneCavesRuinCard.init()
 }
