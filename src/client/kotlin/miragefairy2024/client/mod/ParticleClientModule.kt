6b8f01ec82bfabe00fb5c2c0cd7b608c0f06ca25
_ <tacticsrealize2@gmail.com>
2025-04-01 09:55:07 +0900

    cleanup: initParticleClientModule

--- src/client/kotlin/miragefairy2024/client/mod/ParticleClientModule.kt
+++ src/client/kotlin/miragefairy2024/client/mod/ParticleClientModule.kt
@@ -22,7 +22,7 @@ fun initParticleClientModule() {
     ParticleFactoryRegistry.getInstance().register(ParticleTypeCard.DESCENDING_MAGIC.particleType, EndRodParticle::Factory)
     ParticleFactoryRegistry.getInstance().register(ParticleTypeCard.MIRAGE_FLOUR.particleType, SuspendParticle::HappyVillagerFactory)
     ParticleFactoryRegistry.getInstance().register(ParticleTypeCard.ATTRACTING_MAGIC.particleType, AttractingParticle::Factory)
-    ParticleFactoryRegistry.getInstance().register(ParticleTypeCard.AURA.particleType) { spriteProvider: SpriteProvider ->
+    ParticleFactoryRegistry.getInstance().register(ParticleTypeCard.AURA.particleType) { spriteProvider ->
         val factory = EndRodParticle.Factory(spriteProvider)
         ParticleFactory { parameters, world, x, y, z, velocityX, velocityY, velocityZ ->
             factory.createParticle(parameters, world, x, y, z, velocityX, velocityY, velocityZ)?.also { particle ->
