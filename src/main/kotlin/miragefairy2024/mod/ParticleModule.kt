e0bc260900790ca8d90844d38ae4137e96db60ee
_ <tacticsrealize2@gmail.com>
2025-04-07 16:03:46 +0900

    refactor: ParticleModule

--- src/main/kotlin/miragefairy2024/mod/ParticleModule.kt
+++ src/main/kotlin/miragefairy2024/mod/ParticleModule.kt
@@ -15,24 +15,24 @@ import net.minecraft.util.Identifier
 
 enum class ParticleTypeCard(
     path: String,
-    textureName: String,
+    textureNames: List<String>,
     alwaysSpawn: Boolean,
 ) {
-    MISSION("mission", "mission", true),
-    COLLECTING_MAGIC("collecting_magic", "magic", false),
-    DESCENDING_MAGIC("descending_magic", "magic", false),
-    MIRAGE_FLOUR("mirage_flour", "mirage_flour", false),
-    ATTRACTING_MAGIC("attracting_magic", "mission", false),
-    AURA("aura", "mission", false),
-    CHAOS_STONE("chaos_stone", "chaos_stone", false),
-    HAIMEVISKA_BLOSSOM("haimeviska_blossom", "haimeviska_blossom", false),
-    DRIPPING_HAIMEVISKA_SAP("dripping_haimeviska_sap", "minecraft:drip_hang", false),
-    FALLING_HAIMEVISKA_SAP("falling_haimeviska_sap", "minecraft:drip_fall", false),
-    LANDING_HAIMEVISKA_SAP("landing_haimeviska_sap", "minecraft:drip_land", false),
+    MISSION("mission", listOf("mission"), true),
+    COLLECTING_MAGIC("collecting_magic", listOf("magic"), false),
+    DESCENDING_MAGIC("descending_magic", listOf("magic"), false),
+    MIRAGE_FLOUR("mirage_flour", listOf("mirage_flour"), false),
+    ATTRACTING_MAGIC("attracting_magic", listOf("mission"), false),
+    AURA("aura", listOf("mission"), false),
+    CHAOS_STONE("chaos_stone", listOf("chaos_stone"), false),
+    HAIMEVISKA_BLOSSOM("haimeviska_blossom", listOf("haimeviska_blossom"), false),
+    DRIPPING_HAIMEVISKA_SAP("dripping_haimeviska_sap", listOf("minecraft:drip_hang"), false),
+    FALLING_HAIMEVISKA_SAP("falling_haimeviska_sap", listOf("minecraft:drip_fall"), false),
+    LANDING_HAIMEVISKA_SAP("landing_haimeviska_sap", listOf("minecraft:drip_land"), false),
     ;
 
     val identifier = MirageFairy2024.identifier(path)
-    val texture = if (":" in textureName) Identifier(textureName) else MirageFairy2024.identifier(textureName)
+    val textures = textureNames.map { if (":" in it) Identifier(it) else MirageFairy2024.identifier(it) }
     val particleType: DefaultParticleType = FabricParticleTypes.simple(alwaysSpawn)
 }
 
@@ -42,9 +42,7 @@ fun initParticleModule() {
         card.particleType.register(Registries.PARTICLE_TYPE, card.identifier)
         DataGenerationEvents.onGenerateParticles {
             val data = jsonObject(
-                "textures" to jsonArray(
-                    card.texture.string.jsonElement,
-                ),
+                "textures" to card.textures.map { it.string.jsonElement }.jsonArray,
             )
             it(card.identifier, data)
         }
