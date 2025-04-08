6252f9ea8a7afaad8fd1938639dfdcbe6191d013
_ <tacticsrealize2@gmail.com>
2025-04-08 16:56:41 +0900

    !!

--- src/client/kotlin/miragefairy2024/client/mod/particle/MagicSquareParticle.kt
+++ src/client/kotlin/miragefairy2024/client/mod/particle/MagicSquareParticle.kt
@@ -26,48 +26,55 @@ class MagicSquareParticle(world: ClientWorld, x: Double, y: Double, z: Double, i
         private val rotationVector: Vector3f = Vector3f(-1.0F, -1.0F, 0.0F)
     }
 
+    var delay = 0
+
     init {
         setSprite((spriteProvider as FabricSpriteProvider).sprites[index])
-        gravityStrength = 0.0F
-        maxAge = 40
-        scale = 2.0F
+        maxAge = 60
+        scale = 1.5F
     }
-
-    override fun getType(): ParticleTextureSheet = ParticleTextureSheet.PARTICLE_SHEET_TRANSLUCENT
 
     override fun buildGeometry(vertexConsumer: VertexConsumer, camera: Camera, tickDelta: Float) {
-		//if (delay <= 0) {
-			alpha = 1.0F - MathHelper.clamp((age.toFloat() + tickDelta) / maxAge.toFloat(), 0.0F, 1.0F)
-			buildGeometry(vertexConsumer, camera, tickDelta) { quaternion -> quaternion.mul(Quaternionf().rotationX(-1.0472F)) }
-			buildGeometry(vertexConsumer, camera, tickDelta) { quaternion -> quaternion.mul(Quaternionf().rotationYXZ(-Math.PI.toFloat(), 1.0472F, 0.0F)) }
-		//}
-        //super.buildGeometry(vertexConsumer, camera, tickDelta)
+        if (delay > 0) return
+        val a = age.toFloat() + tickDelta
+        alpha = when {
+            a < 0F -> 0F
+            a < 20F -> a / 20F
+            a < 40F -> 1F
+            a < 60F -> 1F - (a - 40F) / 20F
+            else -> 0F
+        }
+        buildGeometry(vertexConsumer, camera, tickDelta, Quaternionf().rotationX(-1.0472F))
+        buildGeometry(vertexConsumer, camera, tickDelta, Quaternionf().rotationYXZ(-Math.PI.toFloat(), 1.0472F, 0.0F))
     }
 
-    private fun buildGeometry(vertexConsumer: VertexConsumer, camera: Camera, tickDelta: Float, rotator: (Quaternionf) -> Unit) {
+    private fun buildGeometry(vertexConsumer: VertexConsumer, camera: Camera, tickDelta: Float, rotator: Quaternionf) {
         val quaternionf = Quaternionf().setAngleAxis(0.0F, targetVector.x(), targetVector.y(), targetVector.z())
-        rotator(quaternionf)
+        quaternionf.mul(rotator)
+        quaternionf.rotateZ(MathHelper.lerp(tickDelta, prevAngle, angle))
         quaternionf.transform(rotationVector)
+
+        val size = getSize(tickDelta)
+
+        val cameraPos = camera.pos
+        val translateX = (MathHelper.lerp(tickDelta.toDouble(), prevPosX, x) - cameraPos.getX()).toFloat()
+        val translateY = (MathHelper.lerp(tickDelta.toDouble(), prevPosY, y) - cameraPos.getY()).toFloat()
+        val translateZ = (MathHelper.lerp(tickDelta.toDouble(), prevPosZ, z) - cameraPos.getZ()).toFloat()
+
         val vector3fs = arrayOf(
             Vector3f(-1.0F, -1.0F, 0.0F),
             Vector3f(-1.0F, 1.0F, 0.0F),
             Vector3f(1.0F, 1.0F, 0.0F),
             Vector3f(1.0F, -1.0F, 0.0F),
         )
-
-        val size = getSize(tickDelta)
-        val cameraPos = camera.pos
-        val translateX = (MathHelper.lerp(tickDelta.toDouble(), prevPosX, x) - cameraPos.getX()).toFloat()
-        val translateY = (MathHelper.lerp(tickDelta.toDouble(), prevPosY, y) - cameraPos.getY()).toFloat()
-        val translateZ = (MathHelper.lerp(tickDelta.toDouble(), prevPosZ, z) - cameraPos.getZ()).toFloat()
-        repeat(4) { j ->
-            val vector3f = vector3fs[j]
+        vector3fs.forEach { vector3f ->
             vector3f.rotate(quaternionf)
             vector3f.mul(size)
             vector3f.add(translateX, translateY, translateZ)
         }
 
         val brightness = getBrightness(tickDelta)
+
         vertex(vertexConsumer, vector3fs[0], maxU, maxV, brightness)
         vertex(vertexConsumer, vector3fs[1], maxU, minV, brightness)
         vertex(vertexConsumer, vector3fs[2], minU, minV, brightness)
@@ -79,10 +86,21 @@ class MagicSquareParticle(world: ClientWorld, x: Double, y: Double, z: Double, i
     }
 
     override fun tick() {
-        prevPosX = x
-        prevPosY = y
-        prevPosZ = z
+        prevAngle = angle
+
+        if (delay > 0) {
+            delay--
+            return
+        }
+
         age++
         if (age >= maxAge) markDead()
+
+        angle += MathHelper.TAU / 60F
     }
+
+    override fun getSize(tickDelta: Float) = scale * (1F + 0.2F * MathHelper.sin((age.toFloat() + tickDelta) / 40F))
+
+    override fun getType(): ParticleTextureSheet = ParticleTextureSheet.PARTICLE_SHEET_TRANSLUCENT
+
 }
