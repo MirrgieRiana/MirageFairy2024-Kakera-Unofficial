b66846a2238b3b6cebe6e3c92db7d50bf9d10307
_ <tacticsrealize2@gmail.com>
2025-04-10 17:47:37 +0900

    !!

---
+++ src/client/kotlin/miragefairy2024/client/mod/particle/MagicSquareParticle.kt
@@ -0,0 +1,150 @@
+package miragefairy2024.client.mod.particle
+
+import miragefairy2024.client.util.registerClientPacketReceiver
+import miragefairy2024.mod.particle.MagicSquareParticleChannel
+import miragefairy2024.mod.particle.MagicSquareParticleEffect
+import mirrg.kotlin.hydrogen.max
+import net.fabricmc.fabric.api.client.particle.v1.FabricSpriteProvider
+import net.minecraft.client.MinecraftClient
+import net.minecraft.client.particle.ParticleFactory
+import net.minecraft.client.particle.ParticleTextureSheet
+import net.minecraft.client.particle.SpriteBillboardParticle
+import net.minecraft.client.particle.SpriteProvider
+import net.minecraft.client.render.Camera
+import net.minecraft.client.render.VertexConsumer
+import net.minecraft.client.world.ClientWorld
+import net.minecraft.util.math.MathHelper
+import net.minecraft.util.math.Vec3d
+import org.joml.Quaternionf
+import org.joml.Vector3f
+import kotlin.math.roundToInt
+
+fun initMagicSquareParticle() {
+    MagicSquareParticleChannel.registerClientPacketReceiver { packet ->
+        val particleManager = MinecraftClient.getInstance()?.particleManager ?: return@registerClientPacketReceiver
+        (0..6).forEach { i ->
+            particleManager.addParticle(
+                MagicSquareParticleEffect(i, packet.targetPosition, 30F * (i.toFloat() / 6F)),
+                packet.position.x,
+                packet.position.y,
+                packet.position.z,
+                0.0,
+                0.0,
+                0.0,
+            )
+        }
+    }
+}
+
+fun createMagicSquareParticleFactory() = { spriteProvider: SpriteProvider ->
+    ParticleFactory<MagicSquareParticleEffect> { parameters, world, x, y, z, _, _, _ ->
+        MagicSquareParticle(world, x, y, z, parameters.layer, parameters.targetPosition, spriteProvider).also {
+            it.alphaTicks[0] = parameters.delay
+            it.alphaTicks[1] = parameters.delay + 20F
+            it.lightTicks[0] = parameters.delay
+            it.lightTicks[1] = parameters.delay + 20F
+        }
+    }
+}
+
+class MagicSquareParticle(world: ClientWorld, x: Double, y: Double, z: Double, layer: Int, private val targetPosition: Vec3d, spriteProvider: SpriteProvider) : SpriteBillboardParticle(world, x, y, z) {
+
+    var delay = 0
+    var alphaTicks = arrayOf(0F, 10F, 70F, 80F)
+    var lightTicks = arrayOf(0F, 10F, 70F, 80F)
+
+    init {
+        val sprites = (spriteProvider as FabricSpriteProvider).sprites
+        setSprite(sprites[layer.coerceIn(sprites.indices)])
+        maxAge = 80
+        scale = 0.5F
+    }
+
+    private fun getValue(tickDelta: Float, ticks: Array<Float>): Float {
+        val a = age.toFloat() + tickDelta
+        return when {
+            a < ticks[0] -> 0F
+            a < ticks[1] -> 0F + (a - ticks[0]) / (ticks[1] - ticks[0])
+            a < ticks[2] -> 1F
+            a < ticks[3] -> 1F - (a - ticks[2]) / (ticks[3] - ticks[2])
+            else -> 0F
+        }
+    }
+
+    override fun buildGeometry(vertexConsumer: VertexConsumer, camera: Camera, tickDelta: Float) {
+        if (delay > 0) return
+        alpha = getValue(tickDelta, alphaTicks)
+        buildGeometry(vertexConsumer, camera, tickDelta, false)
+        buildGeometry(vertexConsumer, camera, tickDelta, true)
+    }
+
+    private fun buildGeometry(vertexConsumer: VertexConsumer, camera: Camera, tickDelta: Float, flip: Boolean) {
+
+        val offsetX = (targetPosition.x - x).toFloat()
+        val offsetY = (targetPosition.y - y).toFloat()
+        val offsetZ = (targetPosition.z - z).toFloat()
+
+        val yaw = MathHelper.atan2(offsetX.toDouble(), offsetZ.toDouble()).toFloat()
+        val pitch = MathHelper.atan2(-offsetY.toDouble(), MathHelper.sqrt(offsetX * offsetX + offsetZ * offsetZ).toDouble()).toFloat()
+
+        val quaternionf = Quaternionf().rotationYXZ(yaw, pitch, MathHelper.lerp(tickDelta, prevAngle, angle))
+        if (flip) quaternionf.rotateY(-MathHelper.PI)
+
+        val size = getSize(tickDelta)
+
+        val cameraPos = camera.pos
+        val translateX = (MathHelper.lerp(tickDelta.toDouble(), prevPosX, x) - cameraPos.getX()).toFloat()
+        val translateY = (MathHelper.lerp(tickDelta.toDouble(), prevPosY, y) - cameraPos.getY()).toFloat()
+        val translateZ = (MathHelper.lerp(tickDelta.toDouble(), prevPosZ, z) - cameraPos.getZ()).toFloat()
+
+        val vector3fs = arrayOf(
+            Vector3f(-1.0F, -1.0F, 0.0F),
+            Vector3f(-1.0F, 1.0F, 0.0F),
+            Vector3f(1.0F, 1.0F, 0.0F),
+            Vector3f(1.0F, -1.0F, 0.0F),
+        )
+        vector3fs.forEach { vector3f ->
+            vector3f.rotate(quaternionf)
+            vector3f.mul(size)
+            vector3f.add(translateX, translateY, translateZ)
+        }
+
+        val brightness = getBrightness(tickDelta)
+
+        vertex(vertexConsumer, vector3fs[0], maxU, maxV, brightness)
+        vertex(vertexConsumer, vector3fs[1], maxU, minV, brightness)
+        vertex(vertexConsumer, vector3fs[2], minU, minV, brightness)
+        vertex(vertexConsumer, vector3fs[3], minU, maxV, brightness)
+    }
+
+    private fun vertex(vertexConsumer: VertexConsumer, pos: Vector3f, u: Float, v: Float, light: Int) {
+        vertexConsumer.vertex(pos.x().toDouble(), pos.y().toDouble(), pos.z().toDouble()).texture(u, v).color(red, green, blue, alpha).light(light).next()
+    }
+
+    override fun tick() {
+        if (delay > 0) {
+            delay--
+            return
+        }
+
+        age++
+        if (age >= maxAge) markDead()
+
+        prevAngle = angle
+        angle += MathHelper.TAU / 60F
+    }
+
+    override fun getSize(tickDelta: Float) = scale * (1F - 0.2F * MathHelper.cos((age.toFloat() + tickDelta) / 80F * MathHelper.TAU))
+
+    override fun getBrightness(tint: Float): Int {
+        val brightness = super.getBrightness(tint)
+        val oldSkyLight = (brightness shr 20) and 0xF
+        val oldBlockLight = (brightness shr 4) and 0xF
+        val skyLight = oldSkyLight
+        val blockLight = oldBlockLight max (15F * getValue(tint, lightTicks)).roundToInt()
+        return (skyLight shl 20) or (blockLight shl 4)
+    }
+
+    override fun getType(): ParticleTextureSheet = ParticleTextureSheet.PARTICLE_SHEET_TRANSLUCENT
+
+}
---
+++ src/main/kotlin/miragefairy2024/mod/particle/MagicSquareParticle.kt
@@ -0,0 +1,85 @@
+package miragefairy2024.mod.particle
+
+import com.mojang.brigadier.StringReader
+import com.mojang.serialization.Codec
+import com.mojang.serialization.codecs.RecordCodecBuilder
+import miragefairy2024.MirageFairy2024
+import miragefairy2024.util.Channel
+import miragefairy2024.util.string
+import net.minecraft.network.PacketByteBuf
+import net.minecraft.particle.ParticleEffect
+import net.minecraft.particle.ParticleType
+import net.minecraft.util.math.Vec3d
+
+class MagicSquareParticleType(alwaysSpawn: Boolean) : ParticleType<MagicSquareParticleEffect>(alwaysSpawn, MagicSquareParticleEffect.FACTORY) {
+    companion object {
+        val CODEC: Codec<MagicSquareParticleEffect> = RecordCodecBuilder.create { instance ->
+            instance.group(
+                Codec.INT.fieldOf("layer").forGetter(MagicSquareParticleEffect::layer),
+                Vec3d.CODEC.fieldOf("targetPosition").forGetter(MagicSquareParticleEffect::targetPosition),
+                Codec.FLOAT.fieldOf("delay").forGetter(MagicSquareParticleEffect::delay),
+            ).apply(instance, ::MagicSquareParticleEffect)
+        }
+    }
+
+    override fun getCodec() = CODEC
+}
+
+class MagicSquareParticleEffect(val layer: Int, val targetPosition: Vec3d, val delay: Float) : ParticleEffect {
+    companion object {
+        val FACTORY = object : ParticleEffect.Factory<MagicSquareParticleEffect> {
+            override fun read(type: ParticleType<MagicSquareParticleEffect>, buf: PacketByteBuf): MagicSquareParticleEffect {
+                val layer = buf.readInt()
+                val targetPositionX = buf.readDouble()
+                val targetPositionY = buf.readDouble()
+                val targetPositionZ = buf.readDouble()
+                val delay = buf.readFloat()
+                return MagicSquareParticleEffect(layer, Vec3d(targetPositionX, targetPositionY, targetPositionZ), delay)
+            }
+
+            override fun read(type: ParticleType<MagicSquareParticleEffect>, reader: StringReader): MagicSquareParticleEffect {
+                val layer = reader.readInt()
+                val targetPositionX = reader.readDouble()
+                val targetPositionY = reader.readDouble()
+                val targetPositionZ = reader.readDouble()
+                val delay = reader.readFloat()
+                return MagicSquareParticleEffect(layer, Vec3d(targetPositionX, targetPositionY, targetPositionZ), delay)
+            }
+        }
+    }
+
+    override fun getType() = ParticleTypeCard.MAGIC_SQUARE.particleType
+
+    override fun write(buf: PacketByteBuf) {
+        buf.writeInt(layer)
+        buf.writeDouble(targetPosition.x)
+        buf.writeDouble(targetPosition.y)
+        buf.writeDouble(targetPosition.z)
+        buf.writeFloat(delay)
+    }
+
+    override fun asString() = ParticleTypeCard.MAGIC_SQUARE.identifier.string
+}
+
+object MagicSquareParticleChannel : Channel<MagicSquareParticlePacket>(MirageFairy2024.identifier("magic_square_particle")) {
+    override fun writeToBuf(buf: PacketByteBuf, packet: MagicSquareParticlePacket) {
+        buf.writeDouble(packet.position.x)
+        buf.writeDouble(packet.position.y)
+        buf.writeDouble(packet.position.z)
+        buf.writeDouble(packet.targetPosition.x)
+        buf.writeDouble(packet.targetPosition.y)
+        buf.writeDouble(packet.targetPosition.z)
+    }
+
+    override fun readFromBuf(buf: PacketByteBuf): MagicSquareParticlePacket {
+        val positionX = buf.readDouble()
+        val positionY = buf.readDouble()
+        val positionZ = buf.readDouble()
+        val targetPositionX = buf.readDouble()
+        val targetPositionY = buf.readDouble()
+        val targetPositionZ = buf.readDouble()
+        return MagicSquareParticlePacket(Vec3d(positionX, positionY, positionZ), Vec3d(targetPositionX, targetPositionY, targetPositionZ))
+    }
+}
+
+class MagicSquareParticlePacket(val position: Vec3d, val targetPosition: Vec3d)
