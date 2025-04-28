2e31f1adf57e5e2e453ba07270485b096975acb1
_ <tacticsrealize2@gmail.com>
2025-04-28 10:27:01 +0900

    !!

--- src/main/kotlin/miragefairy2024/mod/particle/MagicSquareParticle.kt
+++ src/main/kotlin/miragefairy2024/mod/particle/MagicSquareParticle.kt
@@ -1,19 +1,27 @@
 package miragefairy2024.mod.particle
 
-import com.mojang.brigadier.StringReader
 import com.mojang.serialization.Codec
 import com.mojang.serialization.MapCodec
 import com.mojang.serialization.codecs.RecordCodecBuilder
 import miragefairy2024.MirageFairy2024
 import miragefairy2024.util.Channel
-import miragefairy2024.util.string
+import mirrg.kotlin.hydrogen.unit
 import net.minecraft.core.particles.ParticleType
 import net.minecraft.network.FriendlyByteBuf
+import net.minecraft.network.RegistryFriendlyByteBuf
+import net.minecraft.network.codec.ByteBufCodecs
+import net.minecraft.network.codec.StreamCodec
+import net.minecraft.world.phys.Vec3
 import net.minecraft.core.particles.ParticleOptions as ParticleEffect
 import net.minecraft.world.phys.Vec3 as Vec3d
 
-class MagicSquareParticleType(alwaysSpawn: Boolean) : ParticleType<MagicSquareParticleEffect>(alwaysSpawn, MagicSquareParticleEffect.FACTORY) {
+class MagicSquareParticleType(alwaysSpawn: Boolean) : ParticleType<MagicSquareParticleEffect>(alwaysSpawn) {
     companion object {
+        private val VEC3_STREAM_CODEC: StreamCodec<RegistryFriendlyByteBuf, Vec3> = object : StreamCodec<RegistryFriendlyByteBuf, Vec3> {
+            override fun decode(byteBuf: RegistryFriendlyByteBuf) = byteBuf.readVec3()
+            override fun encode(byteBuf: RegistryFriendlyByteBuf, vec3: Vec3) = unit { byteBuf.writeVec3(vec3) }
+        }
+
         val CODEC: MapCodec<MagicSquareParticleEffect> = RecordCodecBuilder.mapCodec { instance ->
             instance.group(
                 Codec.INT.fieldOf("layer").forGetter(MagicSquareParticleEffect::layer),
@@ -21,45 +29,23 @@ class MagicSquareParticleType(alwaysSpawn: Boolean) : ParticleType<MagicSquarePa
                 Codec.FLOAT.fieldOf("delay").forGetter(MagicSquareParticleEffect::delay),
             ).apply(instance, ::MagicSquareParticleEffect)
         }
+        val STREAM_CODEC: StreamCodec<RegistryFriendlyByteBuf, MagicSquareParticleEffect> = StreamCodec.composite(
+            ByteBufCodecs.VAR_INT,
+            { it.layer },
+            VEC3_STREAM_CODEC,
+            { it.targetPosition },
+            ByteBufCodecs.FLOAT,
+            { it.delay },
+            ::MagicSquareParticleEffect,
+        )
     }
 
     override fun codec() = CODEC
+    override fun streamCodec() = STREAM_CODEC
 }
 
 class MagicSquareParticleEffect(val layer: Int, val targetPosition: Vec3d, val delay: Float) : ParticleEffect {
-    companion object {
-        val FACTORY = object : ParticleEffect.Deserializer<MagicSquareParticleEffect> {
-            override fun fromNetwork(type: ParticleType<MagicSquareParticleEffect>, buf: FriendlyByteBuf): MagicSquareParticleEffect {
-                val layer = buf.readInt()
-                val targetPositionX = buf.readDouble()
-                val targetPositionY = buf.readDouble()
-                val targetPositionZ = buf.readDouble()
-                val delay = buf.readFloat()
-                return MagicSquareParticleEffect(layer, Vec3d(targetPositionX, targetPositionY, targetPositionZ), delay)
-            }
-
-            override fun fromCommand(type: ParticleType<MagicSquareParticleEffect>, reader: StringReader): MagicSquareParticleEffect {
-                val layer = reader.readInt()
-                val targetPositionX = reader.readDouble()
-                val targetPositionY = reader.readDouble()
-                val targetPositionZ = reader.readDouble()
-                val delay = reader.readFloat()
-                return MagicSquareParticleEffect(layer, Vec3d(targetPositionX, targetPositionY, targetPositionZ), delay)
-            }
-        }
-    }
-
     override fun getType() = ParticleTypeCard.MAGIC_SQUARE.particleType
-
-    override fun writeToNetwork(buf: FriendlyByteBuf) {
-        buf.writeInt(layer)
-        buf.writeDouble(targetPosition.x)
-        buf.writeDouble(targetPosition.y)
-        buf.writeDouble(targetPosition.z)
-        buf.writeFloat(delay)
-    }
-
-    override fun writeToString() = ParticleTypeCard.MAGIC_SQUARE.identifier.string
 }
 
 object MagicSquareParticleChannel : Channel<MagicSquareParticlePacket>(MirageFairy2024.identifier("magic_square_particle")) {
