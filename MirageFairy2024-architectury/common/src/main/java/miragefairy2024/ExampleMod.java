cdb6d5615a41f2b099f0bead7bf71465a80661fa
_ <tacticsrealize2@gmail.com>
2025-04-30 22:50:58 +0900

    add: MirageFairy2024-architectury

---
+++ MirageFairy2024-architectury/common/src/main/java/miragefairy2024/ExampleMod.java
@@ -0,0 +1,9 @@
+package miragefairy2024;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "miragefairy2024";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
