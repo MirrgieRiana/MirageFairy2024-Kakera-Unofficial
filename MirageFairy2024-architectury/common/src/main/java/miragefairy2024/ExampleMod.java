b69d622d2e289f438421984601022bf2eafa7e62
_ <tacticsrealize2@gmail.com>
2025-05-01 19:28:30 +0900

    remove: template

--- MirageFairy2024-architectury/common/src/main/java/miragefairy2024/ExampleMod.java
+++
@@ -1,9 +0,0 @@
-package miragefairy2024;
-
-public final class ExampleMod {
-    public static final String MOD_ID = "miragefairy2024";
-
-    public static void init() {
-        // Write common init code here.
-    }
-}
