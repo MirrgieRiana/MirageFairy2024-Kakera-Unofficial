1a309f05e85ab7fe41b31a1ca21f2965ae636743
_ <tacticsrealize2@gmail.com>
2025-05-02 15:06:44 +0900

    build: -> kts

---
+++ MirageFairy2024-architectury/fabric/build.gradle.kts
@@ -0,0 +1,58 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import net.fabricmc.loom.task.RemapJarTask
+
+plugins {
+    id("com.github.johnrengelman.shadow")
+}
+
+architectury {
+    platformSetupLoomIde()
+    fabric()
+}
+
+configurations {
+    val common by creating {
+        isCanBeResolved = true
+        isCanBeConsumed = false
+    }
+    getByName("compileClasspath").extendsFrom(common)
+    getByName("runtimeClasspath").extendsFrom(common)
+    getByName("developmentFabric").extendsFrom(common)
+
+    // Files in this configuration will be bundled into your mod using the Shadow plugin.
+    // Don't use the `shadow` configuration from the plugin itself as it's meant for excluding files.
+    val shadowBundle by creating {
+        isCanBeResolved = true
+        isCanBeConsumed = false
+    }
+}
+
+dependencies {
+    modImplementation("net.fabricmc:fabric-loader:${rootProject.findProperty("fabric_loader_version") as String}")
+
+    // Fabric API. This is technically optional, but you probably want it anyway.
+    modImplementation("net.fabricmc.fabric-api:fabric-api:${rootProject.findProperty("fabric_api_version") as String}")
+
+    // Architectury API. This is optional, and you can comment it out if you don't need it.
+    modImplementation("dev.architectury:architectury-fabric:${rootProject.findProperty("architectury_api_version") as String}")
+
+    "common"(project(path = ":common", configuration = "namedElements")) { isTransitive = false }
+    "shadowBundle"(project(path = ":common", configuration = "transformProductionFabric"))
+}
+
+tasks.named<ProcessResources>("processResources") {
+    inputs.property("version", project.version)
+
+    filesMatching("fabric.mod.json") {
+        expand("version" to project.version)
+    }
+}
+
+tasks.named<ShadowJar>("shadowJar") {
+    configurations = listOf(project.configurations.getByName("shadowBundle"))
+    archiveClassifier.set("dev-shadow")
+}
+
+tasks.named<RemapJarTask>("remapJar") {
+    inputFile.set(tasks.named<ShadowJar>("shadowJar").flatMap { it.archiveFile })
+}
