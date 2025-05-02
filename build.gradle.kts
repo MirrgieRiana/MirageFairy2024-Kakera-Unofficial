e4a4288df0a6eae87fb854995fbf9f210b55a107
_ <tacticsrealize2@gmail.com>
2025-05-03 01:48:05 +0900

    build: move: dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -64,20 +64,6 @@ loom {
 }
 
 repositories {
-    // ここからアーティファクトを取得するリポジトリを追加します。
-    // Loom は Minecraft とライブラリを自動的にダウンロードするために必須の Maven リポジトリを追加するため、
-    // 他の MOD に依存する場合にのみこれを使用してください。
-    // リポジトリの詳細については、↓を参照してください。
-    // https://docs.gradle.org/current/userguide/declaring_repositories.html
-
-    maven("https://maven.shedaniel.me") // RoughlyEnoughItems
-
-    maven("https://maven.blamejared.com") // FauxCustomEntityData-fabric-1.20.2 //// 不安定なので lib/maven に格納
-
-    maven("https://maven.wispforest.io") // owo-lib
-
-    maven("https://maven.minecraftforge.net/") // com.github.glitchfiend:TerraBlender-fabric
-
     maven("https://maven.parchmentmc.org") // mapping
 }
 
@@ -88,29 +74,6 @@ dependencies {
         officialMojangMappings()
         parchment("org.parchmentmc.data:parchment-${project.properties["minecraft_version"] as String}:${project.properties["parchment_mappings"] as String}@zip")
     })
-    "modImplementation"("net.fabricmc:fabric-loader:${project.properties["fabric_loader_version"] as String}")
-
-    // ファブリック API。 これは技術的にはオプションですが、おそらくそれでも必要になるでしょう。
-    "modImplementation"("net.fabricmc.fabric-api:fabric-api:${project.properties["fabric_api_version"] as String}")
-    "modImplementation"("net.fabricmc:fabric-language-kotlin:${project.properties["fabric_kotlin_version"] as String}")
-    // 次の行のコメントを解除して、非推奨のファブリック API モジュールを有効にします。
-    // これらは Fabric API の製品版ディストリビューションに含まれており、後で都合の良いときに MOD を最新のモジュールに更新できるようになります。
-
-    // modImplementation("net.fabricmc.fabric-api:fabric-api-deprecated:${project.fabric_api_version}")
-
-    "modRuntimeOnly"("me.shedaniel:RoughlyEnoughItems-fabric:16.0.799")
-    "modCompileOnly"("me.shedaniel:RoughlyEnoughItems-api-fabric:16.0.799")
-    "modCompileOnly"("me.shedaniel:RoughlyEnoughItems-default-plugin-fabric:16.0.799")
-    "modCompileOnly"("me.shedaniel.cloth:basic-math:0.6.1")
-
-    "modImplementation"("com.faux.fauxcustomentitydata:FauxCustomEntityData-fabric-1.21.1:13.0.1")
-
-    "modImplementation"("io.wispforest:owo-lib:0.12.15.4+1.21")
-    include("io.wispforest:owo-sentinel:0.12.15.4+1.21")
-
-    "modApi"("me.shedaniel.cloth:cloth-config:15.0.140")
-
-    "modImplementation"("com.github.glitchfiend:TerraBlender-fabric:1.21.1-4.1.0.8")
 }
 
 tasks.withType<JavaCompile>().configureEach {
--- fabric/build.gradle.kts
+++ fabric/build.gradle.kts
@@ -53,11 +53,32 @@ configurations {
     }
 }
 
+repositories {
+    // ここからアーティファクトを取得するリポジトリを追加します。
+    // Loom は Minecraft とライブラリを自動的にダウンロードするために必須の Maven リポジトリを追加するため、
+    // 他の MOD に依存する場合にのみこれを使用してください。
+    // リポジトリの詳細については、↓を参照してください。
+    // https://docs.gradle.org/current/userguide/declaring_repositories.html
+
+    maven("https://maven.shedaniel.me") // RoughlyEnoughItems
+
+    maven("https://maven.blamejared.com") // FauxCustomEntityData-fabric-1.20.2 //// 不安定なので lib/maven に格納
+
+    maven("https://maven.wispforest.io") // owo-lib
+
+    maven("https://maven.minecraftforge.net/") // com.github.glitchfiend:TerraBlender-fabric
+}
+
 dependencies {
     modImplementation("net.fabricmc:fabric-loader:${rootProject.findProperty("fabric_loader_version") as String}")
 
-    // Fabric API. This is technically optional, but you probably want it anyway.
+    // ファブリック API。 これは技術的にはオプションですが、おそらくそれでも必要になるでしょう。
     modImplementation("net.fabricmc.fabric-api:fabric-api:${rootProject.findProperty("fabric_api_version") as String}")
+    "modImplementation"("net.fabricmc:fabric-language-kotlin:${project.properties["fabric_kotlin_version"] as String}")
+    // 次の行のコメントを解除して、非推奨のファブリック API モジュールを有効にします。
+    // これらは Fabric API の製品版ディストリビューションに含まれており、後で都合の良いときに MOD を最新のモジュールに更新できるようになります。
+
+    // modImplementation("net.fabricmc.fabric-api:fabric-api-deprecated:${project.fabric_api_version}")
 
     // Architectury API. This is optional, and you can comment it out if you don't need it.
     modImplementation("dev.architectury:architectury-fabric:${rootProject.findProperty("architectury_api_version") as String}")
@@ -66,6 +87,20 @@ dependencies {
     "shadowBundle"(project(path = ":common", configuration = "transformProductionFabric"))
     implementation(project(path = ":mirrg.kotlin"))
     "shadowBundle"(project(path = ":mirrg.kotlin")) { isTransitive = false }
+
+    "modRuntimeOnly"("me.shedaniel:RoughlyEnoughItems-fabric:16.0.799")
+    "modCompileOnly"("me.shedaniel:RoughlyEnoughItems-api-fabric:16.0.799")
+    "modCompileOnly"("me.shedaniel:RoughlyEnoughItems-default-plugin-fabric:16.0.799")
+    "modCompileOnly"("me.shedaniel.cloth:basic-math:0.6.1")
+
+    "modImplementation"("com.faux.fauxcustomentitydata:FauxCustomEntityData-fabric-1.21.1:13.0.1")
+
+    "modImplementation"("io.wispforest:owo-lib:0.12.15.4+1.21")
+    include("io.wispforest:owo-sentinel:0.12.15.4+1.21")
+
+    "modApi"("me.shedaniel.cloth:cloth-config:15.0.140")
+
+    "modImplementation"("com.github.glitchfiend:TerraBlender-fabric:1.21.1-4.1.0.8")
 }
 
 tasks.named<ProcessResources>("processResources") {
