dd899163f99506e8d8122d7499880dab8fc3ed7a
_ <tacticsrealize2@gmail.com>
2025-05-02 17:04:36 +0900

    architectury: build: add: task: showSourceSets

--- MirageFairy2024-architectury/build.gradle.kts
+++ MirageFairy2024-architectury/build.gradle.kts
@@ -82,3 +82,20 @@ subprojects {
         }
     }
 }
+
+tasks.register("showSourceSets") {
+    doLast {
+        allprojects.forEach { project ->
+            println("# Project: '${project.name}'")
+            project.sourceSets.asMap.forEach { (sourceSetName, sourceSet) ->
+                println("## Source Set: '$sourceSetName'")
+                sourceSet.allSource.srcDirs.forEach { file ->
+                    println("Src: $file")
+                }
+                sourceSet.compileClasspath.forEach { file ->
+                    if (file.isDirectory) println("Classpath: $file")
+                }
+            }
+        }
+    }
+}
