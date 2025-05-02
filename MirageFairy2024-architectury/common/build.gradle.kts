8818659ca5e10a1a7f46e3c9183887a53fc18ccb
_ <tacticsrealize2@gmail.com>
2025-05-02 15:06:44 +0900

    build: -> kts

--- MirageFairy2024-architectury/common/build.gradle.kts
+++ MirageFairy2024-architectury/common/build.gradle.kts
@@ -1,13 +1,13 @@
 architectury {
-    common rootProject.enabled_platforms.split(',')
+    common((rootProject.findProperty("enabled_platforms") as String).split(","))
 }
 
 dependencies {
     // We depend on Fabric Loader here to use the Fabric @Environment annotations,
     // which get remapped to the correct annotations on each platform.
     // Do NOT use other classes from Fabric Loader.
-    modImplementation "net.fabricmc:fabric-loader:$rootProject.fabric_loader_version"
+    modImplementation("net.fabricmc:fabric-loader:${rootProject.findProperty("fabric_loader_version") as String}")
 
     // Architectury API. This is optional, and you can comment it out if you don't need it.
-    modImplementation "dev.architectury:architectury:$rootProject.architectury_api_version"
+    modImplementation("dev.architectury:architectury:${rootProject.findProperty("architectury_api_version") as String}")
 }
