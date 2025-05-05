609813c6b500105f0fa6979c32920ef52cadb22b
_ <tacticsrealize2@gmail.com>
2025-05-05 10:59:09 +0900

    build: add: dummy neoforge mod

---
+++ common/src/client/kotlin/miragefairy2024/common/CommonClientKotlinDummy.kt
@@ -0,0 +1,4 @@
+package miragefairy2024.common
+
+// neoforge:runClient時の　NoSuchFileException: ...\MirageFairy2024\common\build\classes\kotlin\client　対策
+class CommonClientKotlinDummy
---
+++ neoforge/src/main/kotlin/miragefairy2024/MirageFairy2024.kt
@@ -0,0 +1,16 @@
+package miragefairy2024
+
+import mirrg.kotlin.slf4j.hydrogen.getLogger
+import net.neoforged.fml.common.Mod
+
+@Mod(MirageFairy2024.MOD_ID)
+class MirageFairy2024 {
+    companion object {
+        const val MOD_ID = "miragefairy2024"
+    }
+
+    init {
+        // TODO
+        getLogger(MirageFairy2024::class.java).info("Hello, World!")
+    }
+}
