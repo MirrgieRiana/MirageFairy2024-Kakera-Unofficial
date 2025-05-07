f7612290f2d6e30e22fe4dc7e49632b626788625
_ <tacticsrealize2@gmail.com>
2025-05-08 07:52:30 +0900

    !!

--- neoforge/src/main/kotlin/miragefairy2024/neoforge/MirageFairy2024Mod.kt
+++ neoforge/src/main/kotlin/miragefairy2024/neoforge/MirageFairy2024Mod.kt
@@ -1,6 +1,7 @@
 package miragefairy2024.neoforge
 
 import miragefairy2024.MirageFairy2024
+import miragefairy2024.ModContext
 import miragefairy2024.ModEvents
 import miragefairy2024.Modules
 import miragefairy2024.client.ClientProxyImpl
@@ -18,9 +19,17 @@ import miragefairy2024.client.mod.initPlacedItemClientModule
 import miragefairy2024.client.mod.initSoundEventClientModule
 import miragefairy2024.client.mod.particle.initParticleClientModule
 import miragefairy2024.clientProxy
+import net.minecraft.core.registries.Registries
+import net.minecraft.resources.ResourceLocation
+import net.minecraft.world.item.DyeColor
+import net.minecraft.world.level.block.BedBlock
+import net.minecraft.world.level.block.Block
+import net.minecraft.world.level.block.state.BlockBehaviour
 import net.neoforged.api.distmarker.Dist
+import net.neoforged.bus.api.SubscribeEvent
 import net.neoforged.fml.common.Mod
 import net.neoforged.fml.loading.FMLEnvironment
+import net.neoforged.neoforge.registries.RegisterEvent
 
 @Mod(MirageFairy2024.MOD_ID)
 class MirageFairy2024Mod {
@@ -48,5 +57,33 @@ class MirageFairy2024Mod {
         Modules.init()
         ModEvents.onRegistration.fire { it() }
         ModEvents.onInitialize.fire { it() }
+    }
+
+    companion object {
+        class Promise<T : Any> : () -> T {
+            lateinit var value: T
+            override fun invoke() = value
+        }
+
+        val block1 = Promise<Block>()
+    }
+
+    @SubscribeEvent
+    fun register(event: RegisterEvent) {
+
+        event.register(Registries.BLOCK) { registry ->
+            val identifier = ResourceLocation.fromNamespaceAndPath(MirageFairy2024.MOD_ID, "block1")
+            block1.value = BedBlock(DyeColor.RED, BlockBehaviour.Properties.of())
+            registry.register(identifier, block1.value)
+        }
+
+        with(ModContext()) {
+            ModEvents.onInitialize {
+
+                val block = block1()
+
+            }
+        }
+
     }
 }
