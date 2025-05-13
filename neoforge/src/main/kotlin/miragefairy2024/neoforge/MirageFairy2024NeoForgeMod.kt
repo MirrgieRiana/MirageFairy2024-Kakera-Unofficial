216027c0b101b56991f8281be1c91785388e7fda
_ <tacticsrealize2@gmail.com>
2025-05-13 17:37:46 +0900

    fix: MirageFairy2024NeoForgeMod

--- neoforge/src/main/kotlin/miragefairy2024/neoforge/MirageFairy2024NeoForgeMod.kt
+++ neoforge/src/main/kotlin/miragefairy2024/neoforge/MirageFairy2024NeoForgeMod.kt
@@ -1,9 +1,9 @@
 package miragefairy2024.neoforge
 
 import kotlinx.coroutines.CompletableDeferred
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.yield
 import miragefairy2024.MirageFairy2024
 import miragefairy2024.ModEvents
 import miragefairy2024.Modules
@@ -26,7 +26,8 @@ import miragefairy2024.util.Registration
 import miragefairy2024.util.RegistryEvents
 import net.minecraft.core.Registry
 import net.neoforged.api.distmarker.Dist
-import net.neoforged.bus.api.SubscribeEvent
+import net.neoforged.bus.api.IEventBus
+import net.neoforged.fml.ModContainer
 import net.neoforged.fml.common.Mod
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent
@@ -34,7 +35,7 @@ import net.neoforged.fml.loading.FMLEnvironment
 import net.neoforged.neoforge.registries.RegisterEvent
 
 @Mod(MirageFairy2024.MOD_ID)
-class MirageFairy2024NeoForgeMod {
+class MirageFairy2024NeoForgeMod(modEventBus: IEventBus, modContainer: ModContainer) {
     init {
         Modules.init()
         if (FMLEnvironment.dist == Dist.CLIENT) {
@@ -54,35 +55,33 @@ class MirageFairy2024NeoForgeMod {
 
             clientProxy = ClientProxyImpl()
         }
-    }
 
-    @SubscribeEvent
-    fun register(event: RegisterEvent) {
-        RegistryEvents.registrations.forEach { registration ->
-            fun <T : Any, U : T> f(registration: Registration<T, U>) {
-                if (event.registry == registration.registry) {
-                    val value = runBlocking {
-                        val deferred = CompletableDeferred<U>()
-                        launch(Dispatchers.Main.immediate) {
-                            deferred.complete(registration.creator())
+        modEventBus.addListener(RegisterEvent::class.java) { event ->
+            RegistryEvents.registrations.forEach { registration ->
+                fun <T : Any, U : T> f(registration: Registration<T, U>) {
+                    if (event.registry == registration.registry) {
+                        val value = runBlocking {
+                            val deferred = CompletableDeferred<U>()
+
+                            launch {
+                                deferred.complete(registration.creator())
+                            }
+                            yield()
+
+                            if (deferred.isActive) throw IllegalStateException("Illegal suspend for initialization: ${registration.identifier}")
+                            deferred.await()
                         }
-                        if (deferred.isActive) throw IllegalStateException("Illegal suspend for initialization: ${registration.identifier}")
-                        deferred.await()
+                        registration.complete(value, Registry.registerForHolder(registration.registry, registration.identifier, value))
                     }
-                    registration.complete(value, Registry.registerForHolder(registration.registry, registration.identifier, value))
                 }
+                f(registration)
             }
-            f(registration)
         }
-    }
-
-    @SubscribeEvent
-    fun register(event: FMLCommonSetupEvent) {
-        ModEvents.onInitialize.fire { it() }
-    }
-
-    @SubscribeEvent
-    fun register(event: FMLClientSetupEvent) {
-        ModEvents.onClientInit.fire { it() }
+        modEventBus.addListener(FMLCommonSetupEvent::class.java) {
+            ModEvents.onInitialize.fire { it() }
+        }
+        modEventBus.addListener(FMLClientSetupEvent::class.java) {
+            ModEvents.onClientInit.fire { it() }
+        }
     }
 }
