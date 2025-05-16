42bc6a823c4ee115a1dbf86e5edc7982ca725438
_ <tacticsrealize2@gmail.com>
2025-05-17 03:20:15 +0900

    fix: getBlockColorProvider

--- common/src/client/kotlin/miragefairy2024/client/ClientProxyImpl.kt
+++ common/src/client/kotlin/miragefairy2024/client/ClientProxyImpl.kt
@@ -48,9 +48,8 @@ class ClientProxyImpl : ClientProxy {
     override fun getClientPlayer(): PlayerEntity? = MinecraftClient.getInstance().player
 
     override fun getBlockColorProvider(block: Block): BlockColorProvider? {
-        val provider = ColorProviderRegistry.BLOCK.get(block) ?: return null
         return BlockColorProvider { blockState, world, blockPos, tintIndex ->
-            provider.getColor(blockState, world as BlockRenderView?, blockPos, tintIndex)
+            MinecraftClient.getInstance().blockColors.getColor(blockState, world as BlockRenderView?, blockPos, tintIndex)
         }
     }
 
