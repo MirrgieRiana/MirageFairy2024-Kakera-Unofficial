a8b722b6f0dedc771ed2cebd22220b42254760eb
_ <tacticsrealize2@gmail.com>
2025-05-18 23:51:43 +0900

    internal: FabricModule: add: currentServer

--- fabric/src/main/kotlin/miragefairy2024/fabric/FabricModule.kt
+++ fabric/src/main/kotlin/miragefairy2024/fabric/FabricModule.kt
@@ -1,8 +1,22 @@
 package miragefairy2024.fabric
 
 import miragefairy2024.ModContext
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents
+import net.minecraft.server.MinecraftServer
+
+private var initialized = false
+
+var currentServer: MinecraftServer? = null
 
 context(ModContext)
 fun initFabricModule() {
+    if (initialized) return
+    initialized = true
 
+    ServerLifecycleEvents.SERVER_STARTING.register {
+        currentServer = it
+    }
+    ServerLifecycleEvents.SERVER_STOPPED.register {
+        currentServer = null
+    }
 }
