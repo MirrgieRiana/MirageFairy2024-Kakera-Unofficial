384135a9617bd2cc0407c6e8a32621464ec2dd5e
_ <tacticsrealize2@gmail.com>
2025-05-22 01:47:22 +0900

    !!

--- common/src/main/kotlin/miragefairy2024/mod/haimeviska/HaimeviskaModule.kt
+++ common/src/main/kotlin/miragefairy2024/mod/haimeviska/HaimeviskaModule.kt
@@ -1,9 +1,24 @@
 package miragefairy2024.mod.haimeviska
 
+import miragefairy2024.MirageFairy2024
 import miragefairy2024.ModContext
+import miragefairy2024.mod.rootAdvancement
+import miragefairy2024.util.AdvancementCard
+import miragefairy2024.util.EnJa
+import miragefairy2024.util.createItemStack
+
+val haimeviskaAdvancement = AdvancementCard(
+    identifier = MirageFairy2024.identifier("haimeviska"),
+    context = AdvancementCard.Sub { rootAdvancement.await() },
+    icon = { HaimeviskaBlockCard.HOLLOW_LOG.item().createItemStack() },
+    name = EnJa("The Experiencing Tree", ""), !! TODO
+    description = EnJa("Explore the overworld to find the Faiy Tree Haimeviska", "地上を探検して精樹ハイメヴィスカを探す"),
+    criterion = AdvancementCard.hasItem { HaimeviskaBlockCard.LOG.item() },
+)
 
 context(ModContext)
 fun initHaimeviskaModule() {
     initHaimeviskaBlocks()
     initHaimeviskaWorldGens()
+    haimeviskaAdvancement.init()
 }
