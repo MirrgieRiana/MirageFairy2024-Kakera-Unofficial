948a77d329b95491cda8bdc6f9bf0aad5dab6b0b
_ <tacticsrealize2@gmail.com>
2025-05-22 21:23:53 +0900

    !!

--- common/src/main/kotlin/miragefairy2024/mod/haimeviska/HaimeviskaModule.kt
+++ common/src/main/kotlin/miragefairy2024/mod/haimeviska/HaimeviskaModule.kt
@@ -10,7 +10,7 @@ import miragefairy2024.util.createItemStack
 val haimeviskaAdvancement = AdvancementCard(
     identifier = MirageFairy2024.identifier("haimeviska"),
     context = AdvancementCard.Sub { rootAdvancement.await() },
-    icon = { HaimeviskaBlockCard.HOLLOW_LOG.item().createItemStack() },
+    icon = { HaimeviskaBlockCard.DRIPPING_LOG.item().createItemStack() },
     name = EnJa("What is it like to be a plant?", "植物として生きるとはどのようなことか"),
     description = EnJa("Explore the overworld to find Haimeviska the fairy tree", "地上を探検して精樹ハイメヴィスカを探す"),
     criterion = AdvancementCard.hasItem { HaimeviskaBlockCard.LOG.item() },
