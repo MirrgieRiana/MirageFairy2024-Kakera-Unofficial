3b40071b82960f55f365ea94ff3958fa27c03f89
MirrgieRiana <tacticsrealize2@gmail.com>
2025-05-26 10:03:54 +0900

    internal: add: ResourceKey.toHolderSetCodec

--- common/src/main/kotlin/miragefairy2024/util/Codec.kt
+++ common/src/main/kotlin/miragefairy2024/util/Codec.kt
@@ -2,9 +2,13 @@ package miragefairy2024.util
 
 import com.mojang.serialization.Codec
 import io.netty.buffer.ByteBuf
+import net.minecraft.core.HolderSet
+import net.minecraft.core.Registry
+import net.minecraft.core.RegistryCodecs
 import net.minecraft.network.RegistryFriendlyByteBuf
 import net.minecraft.network.codec.ByteBufCodecs
 import net.minecraft.network.codec.StreamCodec
+import net.minecraft.resources.ResourceKey
 import net.minecraft.world.item.ItemStack
 import net.minecraft.world.item.component.ItemContainerContents
 import java.time.Instant
@@ -26,3 +30,5 @@ val ITEMS_STREAM_CODEC: StreamCodec<RegistryFriendlyByteBuf, List<ItemStack>> = 
  * @see net.minecraft.server.level.ServerPlayer.openMenu
  */
 fun dummyUnitStreamCodec(): StreamCodec<ByteBuf, Unit> = ByteBufCodecs.VAR_INT.map({ }, { 0 })
+
+fun <T> ResourceKey<Registry<T>>.toHolderSetCodec(): Codec<HolderSet<T>> = RegistryCodecs.homogeneousList(this)
