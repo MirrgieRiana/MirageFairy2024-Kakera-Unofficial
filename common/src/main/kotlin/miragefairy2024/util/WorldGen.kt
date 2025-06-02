e41cbb4595d913fae256cfa1306777ef841c06d7
_ <tacticsrealize2@gmail.com>
2025-06-02 10:05:39 +0900

    internal: add: rangedNetherFlower

--- common/src/main/kotlin/miragefairy2024/util/WorldGen.kt
+++ common/src/main/kotlin/miragefairy2024/util/WorldGen.kt
@@ -164,6 +164,13 @@ val flower: List<PlacementModifier>
     )
 
 context(PlacementModifiersScope)
+fun rangedNetherFlower(minY: Int, maxY: Int): List<PlacementModifier> = listOf(
+    HeightRangePlacementModifier.uniform(YOffset.absolute(minY), YOffset.absolute(maxY)),
+    CountMultilayerPlacementModifier.of(1),
+    BiomePlacementModifier.biome(),
+)
+
+context(PlacementModifiersScope)
 val netherFlower
     get(): List<PlacementModifier> = listOf(
         CountMultilayerPlacementModifier.of(1),
