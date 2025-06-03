375a4a17e0f7eaab54b1ba6d3b36278e2ed23d88
_ <tacticsrealize2@gmail.com>
2025-06-04 08:35:17 +0900

    !!

--- common/src/main/kotlin/miragefairy2024/mod/magicplant/contents/magicplants/Sarracenia.kt
+++ common/src/main/kotlin/miragefairy2024/mod/magicplant/contents/magicplants/Sarracenia.kt
@@ -1,10 +1,32 @@
 package miragefairy2024.mod.magicplant.contents.magicplants
 
 import com.mojang.serialization.MapCodec
+import miragefairy2024.util.EnJa
+import miragefairy2024.util.createCuboidShape
+import net.minecraft.world.level.block.SoundType
 import net.minecraft.world.level.block.state.properties.BlockStateProperties
 import net.minecraft.world.level.block.state.properties.IntegerProperty
+import net.minecraft.world.level.material.MapColor
 
 object SarraceniaCard : SimpleMagicPlantCard<SarraceniaBlock>() {
+    override fun getBlockPath() = "sarracenia"
+    override val blockName = EnJa("Sarracenia", "瓶子草サラセニア")
+    override fun getItemPath() = "sarracenia_bulb"
+    override val itemName = EnJa("Sarracenia Bulb", "サラセニアの球根")
+    override val tier = 2
+    override val poem = EnJa("TODO", "TODO") // TODO
+    override val classification = EnJa("Order Miragales, family Veropedaceae", "妖花目ヴェロペダ科")
+
+    override val ageProperty: IntegerProperty = BlockStateProperties.AGE_3
+    override fun createBlock() = SarraceniaBlock(createCommonSettings().breakInstantly().mapColor(MapColor.NETHER).sound(SoundType.CROP))
+
+    override val outlineShapes = listOf(
+        createCuboidShape(3.0, 5.0),
+        createCuboidShape(4.0, 7.0),
+        createCuboidShape(7.0, 9.0),
+        createCuboidShape(7.0, 16.0),
+    )
+
 
 }
 
