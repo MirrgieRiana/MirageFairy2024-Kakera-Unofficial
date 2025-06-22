cb697278173e1506df459e8e9357c52762cab5ec
_ <tacticsrealize2@gmail.com>
2025-06-22 15:54:55 +0900

    internal: add: BlockCallback.GET_DROPS_BY_ENTITY

--- common/src/main/java/miragefairy2024/mixins/api/BlockCallback.java
+++ common/src/main/java/miragefairy2024/mixins/api/BlockCallback.java
@@ -3,12 +3,16 @@ package miragefairy2024.mixins.api;
 import net.fabricmc.fabric.api.event.Event;
 import net.fabricmc.fabric.api.event.EventFactory;
 import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class BlockCallback {
     public final static Event<AfterBreak> AFTER_BREAK = EventFactory.createArrayBacked(AfterBreak.class, callbacks -> (world, player, pos, state, blockEntity, tool) -> {
@@ -17,7 +21,19 @@ public class BlockCallback {
         }
     });
 
+    public final static Event<GetDropsByEntity> GET_DROPS_BY_ENTITY = EventFactory.createArrayBacked(GetDropsByEntity.class, callbacks -> (state, level, pos, blockEntity, entity, tool, drops) -> {
+        List<ItemStack> newDrops = drops;
+        for (GetDropsByEntity callback : callbacks) {
+            newDrops = callback.getDrops(state, level, pos, blockEntity, entity, tool, newDrops);
+        }
+        return newDrops;
+    });
+
     public interface AfterBreak {
         void afterBreak(Level world, Player player, BlockPos pos, BlockState state, @Nullable BlockEntity blockEntity, ItemStack tool);
+    }
+
+    public interface GetDropsByEntity {
+        List<ItemStack> getDrops(BlockState state, ServerLevel level, BlockPos pos, @Nullable BlockEntity blockEntity, @Nullable Entity entity, ItemStack tool, List<ItemStack> drops);
     }
 }
--- common/src/main/java/miragefairy2024/mixins/mixin/BlockMixin.java
+++ common/src/main/java/miragefairy2024/mixins/mixin/BlockMixin.java
@@ -2,6 +2,8 @@ package miragefairy2024.mixins.mixin;
 
 import miragefairy2024.mixins.api.BlockCallback;
 import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
@@ -13,11 +15,19 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.List;
 
 @Mixin(Block.class)
 public class BlockMixin {
     @Inject(method = "playerDestroy", at = @At("TAIL"))
     private void afterBreak(Level world, Player player, BlockPos pos, BlockState state, @Nullable BlockEntity blockEntity, ItemStack tool, CallbackInfo ci) {
         BlockCallback.AFTER_BREAK.invoker().afterBreak(world, player, pos, state, blockEntity, tool);
+    }
+
+    @Inject(method = "getDrops(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/entity/BlockEntity;Lnet/minecraft/world/entity/Entity;Lnet/minecraft/world/item/ItemStack;)Ljava/util/List;", at = @At("RETURN"), cancellable = true)
+    private static void getDrops(BlockState state, ServerLevel level, BlockPos pos, @Nullable BlockEntity blockEntity, @Nullable Entity entity, ItemStack tool, CallbackInfoReturnable<List<ItemStack>> cir) {
+        cir.setReturnValue(BlockCallback.GET_DROPS_BY_ENTITY.invoker().getDrops(state, level, pos, blockEntity, entity, tool, cir.getReturnValue()));
     }
 }
