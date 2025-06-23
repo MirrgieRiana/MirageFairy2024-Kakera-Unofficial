d5bcb245ef240b3625a86d269ce11b6dfd73b2db
_ <tacticsrealize2@gmail.com>
2025-06-23 15:58:14 +0900

    refactor: MirageFlower

--- common/src/main/kotlin/miragefairy2024/mod/magicplant/contents/magicplants/MirageFlower.kt
+++ common/src/main/kotlin/miragefairy2024/mod/magicplant/contents/magicplants/MirageFlower.kt
@@ -43,17 +43,10 @@ import net.minecraft.world.level.block.state.properties.IntegerProperty as IntPr
 import net.minecraft.world.level.levelgen.feature.configurations.RandomPatchConfiguration as RandomPatchFeatureConfig
 import net.minecraft.world.level.levelgen.feature.configurations.SimpleBlockConfiguration as SimpleBlockFeatureConfig
 
-object MirageFlowerCard : SimpleMagicPlantCard<MirageFlowerBlock>() {
-    override fun getBlockPath() = "mirage_flower"
-    override val blockName = EnJa("Mirage Flower", "妖花ミラージュ")
-    override fun getItemPath() = "mirage_bulb"
-    override val itemName = EnJa("Mirage Bulb", "妖花ミラージュの球根")
-    override val tier = 1
-    override val poem = EnJa("Evolution to escape extermination", "可憐にして人畜無害たる魔物。")
+abstract class AbstractMirageFlowerCard<B : SimpleMagicPlantBlock> : SimpleMagicPlantCard<B>() {
     override val classification = EnJa("Order Miragales, family Miragaceae", "妖花目ミラージュ科")
 
     override val ageProperty: IntProperty = BlockStateProperties.AGE_3
-    override fun createBlock() = MirageFlowerBlock(createCommonSettings().breakInstantly().mapColor(MapColor.DIAMOND).sound(BlockSoundGroup.GLASS))
 
     override val outlineShapes = listOf(
         createCuboidShape(3.0, 5.0),
@@ -62,18 +55,12 @@ object MirageFlowerCard : SimpleMagicPlantCard<MirageFlowerBlock>() {
         createCuboidShape(6.0, 16.0),
     )
 
-    override val drops = listOf(MaterialCard.MIRAGE_FLOUR.item, MaterialCard.MIRAGE_LEAVES.item, MaterialCard.FAIRY_CRYSTAL.item)
-    override fun getFruitDrops(count: Int, random: Random) = getMirageFlour(count, random)
-    override fun getLeafDrops(count: Int, random: Random) = listOf(MaterialCard.MIRAGE_LEAVES.item().createItemStack(count))
-    override fun getRareDrops(count: Int, random: Random) = listOf(MaterialCard.FAIRY_CRYSTAL.item().createItemStack(count))
-
     override val family = MirageFairy2024.identifier("mirage")
     override val possibleTraits = setOf(
         TraitCard.ETHER_RESPIRATION.trait, // エーテル呼吸
         TraitCard.PHOTOSYNTHESIS.trait, // 光合成
         TraitCard.PHAEOSYNTHESIS.trait, // 闇合成
         TraitCard.OSMOTIC_ABSORPTION.trait, // 養分吸収
-        TraitCard.CRYSTAL_ABSORPTION.trait, // 鉱物吸収
         TraitCard.AIR_ADAPTATION.trait, // 空間適応
         TraitCard.COLD_ADAPTATION.trait, // 低温適応
         TraitCard.WARM_ADAPTATION.trait, // 中温適応
@@ -86,27 +73,47 @@ object MirageFlowerCard : SimpleMagicPlantCard<MirageFlowerBlock>() {
         TraitCard.LEAVES_PRODUCTION.trait, // 葉面生成
         TraitCard.RARE_PRODUCTION.trait, // 希少品生成
         TraitCard.EXPERIENCE_PRODUCTION.trait, // 経験値生成
-        TraitCard.FAIRY_BLESSING.trait, // 妖精の祝福
         TraitCard.FOUR_LEAFED.trait, // 四つ葉
+        TraitCard.SPINY_LEAVES.trait, // 棘のある葉
+        TraitCard.HEATING_MECHANISM.trait, // 発熱機構
+        TraitCard.ETHER_PREDATION.trait, // エーテル捕食
+        TraitCard.PROSPERITY_OF_SPECIES.trait, // 種の繁栄
+        TraitCard.CROSSBREEDING.trait, // 交雑
+        TraitCard.PLANTS_WITH_SELF_AWARENESS.trait, // 自我を持つ植物
+    )
+}
+
+object MirageFlowerCard : AbstractMirageFlowerCard<MirageFlowerBlock>() {
+    override fun getBlockPath() = "mirage_flower"
+    override val blockName = EnJa("Mirage Flower", "妖花ミラージュ")
+    override fun getItemPath() = "mirage_bulb"
+    override val itemName = EnJa("Mirage Bulb", "妖花ミラージュの球根")
+    override val tier = 1
+    override val poem = EnJa("Evolution to escape extermination", "可憐にして人畜無害たる魔物。")
+
+    override fun createBlock() = MirageFlowerBlock(createCommonSettings().breakInstantly().mapColor(MapColor.DIAMOND).sound(BlockSoundGroup.GLASS))
+
+    override val drops = listOf(MaterialCard.MIRAGE_FLOUR.item, MaterialCard.MIRAGE_LEAVES.item, MaterialCard.FAIRY_CRYSTAL.item)
+    override fun getFruitDrops(count: Int, random: Random) = getMirageFlour(count, random)
+    override fun getLeafDrops(count: Int, random: Random) = listOf(MaterialCard.MIRAGE_LEAVES.item().createItemStack(count))
+    override fun getRareDrops(count: Int, random: Random) = listOf(MaterialCard.FAIRY_CRYSTAL.item().createItemStack(count))
+
+    override val possibleTraits = super.possibleTraits + setOf(
+        TraitCard.CRYSTAL_ABSORPTION.trait, // 鉱物吸収
+        TraitCard.FAIRY_BLESSING.trait, // 妖精の祝福
         TraitCard.NODED_STEM.trait, // 節状の茎
         TraitCard.FRUIT_OF_KNOWLEDGE.trait, // 禁断の果実
         //TraitCard.GOLDEN_APPLE.trait, // 金のリンゴ
-        TraitCard.SPINY_LEAVES.trait, // 棘のある葉
         TraitCard.DESERT_GEM.trait, // 砂漠の宝石
-        TraitCard.HEATING_MECHANISM.trait, // 発熱機構
         TraitCard.WATERLOGGING_TOLERANCE.trait, // 浸水耐性
         TraitCard.ADVERSITY_FLOWER.trait, // 高嶺の花
         TraitCard.FLESHY_LEAVES.trait, // 肉厚の葉
         TraitCard.NATURAL_ABSCISSION.trait, // 自然落果
         TraitCard.CARNIVOROUS_PLANT.trait, // 食虫植物
-        TraitCard.ETHER_PREDATION.trait, // エーテル捕食
         TraitCard.PAVEMENT_FLOWERS.trait, // アスファルトに咲く花
-        TraitCard.PROSPERITY_OF_SPECIES.trait, // 種の繁栄
         //TraitCard.PHANTOM_FLOWER.trait, // 幻の花
         //TraitCard.ETERNAL_TREASURE.trait, // 悠久の秘宝
         //TraitCard.TREASURE_OF_XARPA.trait, // シャルパの秘宝
-        TraitCard.CROSSBREEDING.trait, // 交雑
-        TraitCard.PLANTS_WITH_SELF_AWARENESS.trait, // 自我を持つ植物
         //TraitCard.FLOWER_OF_THE_END.trait, // 終焉の花
     )
 
@@ -167,24 +174,15 @@ class MirageFlowerBlock(settings: Properties) : SimpleMagicPlantBlock(MirageFlow
     override fun getAgeProperty(): IntProperty = BlockStateProperties.AGE_3
 }
 
-object PhantomFlowerCard : SimpleMagicPlantCard<PhantomFlowerBlock>() {
+object PhantomFlowerCard : AbstractMirageFlowerCard<PhantomFlowerBlock>() {
     override fun getBlockPath() = "phantom_flower"
     override val blockName = EnJa("Phantom Flower", "幻花ファントム")
     override fun getItemPath() = "phantom_bulb"
     override val itemName = EnJa("Phantom Bulb", "幻花ファントムの球根")
     override val tier = 3
     override val poem = EnJa("Illusory telepathy", "――おいでよ、僕たちのところへ")
-    override val classification = EnJa("Order Miragales, family Miragaceae", "妖花目ミラージュ科")
 
-    override val ageProperty: IntProperty = BlockStateProperties.AGE_3
     override fun createBlock() = PhantomFlowerBlock(createCommonSettings().breakInstantly().mapColor(MapColor.COLOR_PINK).sound(BlockSoundGroup.GLASS))
-
-    override val outlineShapes = listOf(
-        createCuboidShape(3.0, 5.0),
-        createCuboidShape(6.0, 12.0),
-        createCuboidShape(6.0, 15.0),
-        createCuboidShape(6.0, 16.0),
-    )
 
     override val baseGrowth = 1.0 / 8.0
     override val baseFruitGeneration = 9.0
@@ -194,46 +192,22 @@ object PhantomFlowerCard : SimpleMagicPlantCard<PhantomFlowerBlock>() {
     override fun getLeafDrops(count: Int, random: Random) = listOf(MaterialCard.PHANTOM_LEAVES.item().createItemStack(count))
     override fun getRareDrops(count: Int, random: Random) = listOf(MaterialCard.PHANTOM_DROP.item().createItemStack(count))
 
-    override val family = MirageFairy2024.identifier("mirage")
-    override val possibleTraits = setOf(
-        TraitCard.ETHER_RESPIRATION.trait, // エーテル呼吸
-        TraitCard.PHOTOSYNTHESIS.trait, // 光合成
-        TraitCard.PHAEOSYNTHESIS.trait, // 闇合成
-        TraitCard.OSMOTIC_ABSORPTION.trait, // 養分吸収
+    override val possibleTraits = super.possibleTraits + setOf(
         //TraitCard.CRYSTAL_ABSORPTION.trait, // 鉱物吸収
-        TraitCard.AIR_ADAPTATION.trait, // 空間適応
-        TraitCard.COLD_ADAPTATION.trait, // 低温適応
-        TraitCard.WARM_ADAPTATION.trait, // 中温適応
-        TraitCard.HOT_ADAPTATION.trait, // 高温適応
-        TraitCard.ARID_ADAPTATION.trait, // 乾燥適応
-        TraitCard.MESIC_ADAPTATION.trait, // 中湿適応
-        TraitCard.HUMID_ADAPTATION.trait, // 湿潤適応
-        TraitCard.SEEDS_PRODUCTION.trait, // 種子生成
-        TraitCard.FRUITS_PRODUCTION.trait, // 果実生成
-        TraitCard.LEAVES_PRODUCTION.trait, // 葉面生成
-        TraitCard.RARE_PRODUCTION.trait, // 希少品生成
-        TraitCard.EXPERIENCE_PRODUCTION.trait, // 経験値生成
         //TraitCard.FAIRY_BLESSING.trait, // 妖精の祝福
-        TraitCard.FOUR_LEAFED.trait, // 四つ葉
         //TraitCard.NODED_STEM.trait, // 節状の茎
         //TraitCard.FRUIT_OF_KNOWLEDGE.trait, // 禁断の果実
         //TraitCard.GOLDEN_APPLE.trait, // 金のリンゴ
-        TraitCard.SPINY_LEAVES.trait, // 棘のある葉
         //TraitCard.DESERT_GEM.trait, // 砂漠の宝石
-        TraitCard.HEATING_MECHANISM.trait, // 発熱機構
         //TraitCard.WATERLOGGING_TOLERANCE.trait, // 浸水耐性
         //TraitCard.ADVERSITY_FLOWER.trait, // 高嶺の花
         //TraitCard.FLESHY_LEAVES.trait, // 肉厚の葉
         //TraitCard.NATURAL_ABSCISSION.trait, // 自然落果
         //TraitCard.CARNIVOROUS_PLANT.trait, // 食虫植物
-        TraitCard.ETHER_PREDATION.trait, // エーテル捕食
         //TraitCard.PAVEMENT_FLOWERS.trait, // アスファルトに咲く花
-        TraitCard.PROSPERITY_OF_SPECIES.trait, // 種の繁栄
         TraitCard.PHANTOM_FLOWER.trait, // 幻の花
         //TraitCard.ETERNAL_TREASURE.trait, // 悠久の秘宝
         //TraitCard.TREASURE_OF_XARPA.trait, // シャルパの秘宝
-        TraitCard.CROSSBREEDING.trait, // 交雑
-        TraitCard.PLANTS_WITH_SELF_AWARENESS.trait, // 自我を持つ植物
         TraitCard.FLOWER_OF_THE_END.trait, // 終焉の花
     )
 
