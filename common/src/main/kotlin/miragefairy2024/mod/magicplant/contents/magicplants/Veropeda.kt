7baf5069021714243d373f5fbfe1dfe96d9e987e
_ <tacticsrealize2@gmail.com>
2025-06-23 16:12:34 +0900

    refactor: Veropeda

--- common/src/main/kotlin/miragefairy2024/mod/magicplant/contents/magicplants/Veropeda.kt
+++ common/src/main/kotlin/miragefairy2024/mod/magicplant/contents/magicplants/Veropeda.kt
@@ -40,17 +40,10 @@ import net.minecraft.world.level.block.state.properties.IntegerProperty as IntPr
 import net.minecraft.world.level.levelgen.feature.configurations.RandomPatchConfiguration as RandomPatchFeatureConfig
 import net.minecraft.world.level.levelgen.feature.configurations.SimpleBlockConfiguration as SimpleBlockFeatureConfig
 
-object VeropedaCard : SimpleMagicPlantCard<VeropedaBlock>() {
-    override fun getBlockPath() = "veropeda"
-    override val blockName = EnJa("Veropeda", "呪草ヴェロペダ")
-    override fun getItemPath() = "veropeda_bulb"
-    override val itemName = EnJa("Veropeda Bulb", "呪草ヴェロペダの球根")
-    override val tier = 1
-    override val poem = EnJa("Contains strong acids made from insects", "毒を喰らい、毒と化す。")
+abstract class AbstractVeropedaCard<B : SimpleMagicPlantBlock> : SimpleMagicPlantCard<B>() {
     override val classification = EnJa("Order Miragales, family Veropedaceae", "妖花目ヴェロペダ科")
 
-    override val ageProperty: IntProperty = BlockStateProperties.AGE_3
-    override fun createBlock() = VeropedaBlock(createCommonSettings().breakInstantly().mapColor(MapColor.NETHER).sound(BlockSoundGroup.CROP))
+    override val ageProperty: IntegerProperty = BlockStateProperties.AGE_3
 
     override val outlineShapes = listOf(
         createCuboidShape(3.0, 5.0),
@@ -58,10 +51,6 @@ object VeropedaCard : SimpleMagicPlantCard<VeropedaBlock>() {
         createCuboidShape(7.0, 9.0),
         createCuboidShape(7.0, 16.0),
     )
-
-    override val drops = listOf(MaterialCard.VEROPEDA_BERRIES.item, MaterialCard.VEROPEDA_LEAF.item)
-    override fun getFruitDrops(count: Int, random: Random) = listOf(MaterialCard.VEROPEDA_BERRIES.item().createItemStack(count))
-    override fun getLeafDrops(count: Int, random: Random) = listOf(MaterialCard.VEROPEDA_LEAF.item().createItemStack(count))
 
     override val family = MirageFairy2024.identifier("veropeda")
     override val possibleTraits = setOf(
@@ -91,6 +80,23 @@ object VeropedaCard : SimpleMagicPlantCard<VeropedaBlock>() {
         TraitCard.PROSPERITY_OF_SPECIES.trait, // 種の繁栄
         TraitCard.CROSSBREEDING.trait, // 交雑
     )
+}
+
+object VeropedaCard : AbstractVeropedaCard<VeropedaBlock>() {
+    override fun getBlockPath() = "veropeda"
+    override val blockName = EnJa("Veropeda", "呪草ヴェロペダ")
+    override fun getItemPath() = "veropeda_bulb"
+    override val itemName = EnJa("Veropeda Bulb", "呪草ヴェロペダの球根")
+    override val tier = 1
+    override val poem = EnJa("Contains strong acids made from insects", "毒を喰らい、毒と化す。")
+
+    override fun createBlock() = VeropedaBlock(createCommonSettings().breakInstantly().mapColor(MapColor.NETHER).sound(BlockSoundGroup.CROP))
+
+    override val drops = listOf(MaterialCard.VEROPEDA_BERRIES.item, MaterialCard.VEROPEDA_LEAF.item)
+    override fun getFruitDrops(count: Int, random: Random) = listOf(MaterialCard.VEROPEDA_BERRIES.item().createItemStack(count))
+    override fun getLeafDrops(count: Int, random: Random) = listOf(MaterialCard.VEROPEDA_LEAF.item().createItemStack(count))
+
+    override val possibleTraits = super.possibleTraits + setOf()
 
     val VEROPEDA_CLUSTER_CONFIGURED_FEATURE_KEY = Registries.CONFIGURED_FEATURE with MirageFairy2024.identifier("veropeda_cluster")
     val LARGE_VEROPEDA_CLUSTER_CONFIGURED_FEATURE_KEY = Registries.CONFIGURED_FEATURE with MirageFairy2024.identifier("large_veropeda_cluster")
@@ -141,57 +147,21 @@ class VeropedaBlock(settings: Properties) : SimpleMagicPlantBlock(VeropedaCard, 
     override fun getAgeProperty(): IntProperty = BlockStateProperties.AGE_3
 }
 
-object SarraceniaCard : SimpleMagicPlantCard<SarraceniaBlock>() {
+object SarraceniaCard : AbstractVeropedaCard<SarraceniaBlock>() {
     override fun getBlockPath() = "sarracenia"
     override val blockName = EnJa("Sarracenia", "瓶子草サラセニア")
     override fun getItemPath() = "sarracenia_bulb"
     override val itemName = EnJa("Sarracenia Bulb", "瓶子草サラセニアの球根")
     override val tier = 1
     override val poem = EnJa("Waiting for a flying creature...", "妖精たちの憩いの場。")
-    override val classification = EnJa("Order Miragales, family Veropedaceae", "妖花目ヴェロペダ科")
 
-    override val ageProperty: IntegerProperty = BlockStateProperties.AGE_3
     override fun createBlock() = SarraceniaBlock(createCommonSettings().breakInstantly().mapColor(MapColor.NETHER).sound(SoundType.CROP))
-
-    override val outlineShapes = listOf(
-        createCuboidShape(3.0, 5.0),
-        createCuboidShape(4.0, 7.0),
-        createCuboidShape(7.0, 9.0),
-        createCuboidShape(7.0, 16.0),
-    )
 
     override val drops = listOf(MaterialCard.SARRACENIA_LEAF.item, MaterialCard.FAIRY_SCALES.item)
     override fun getLeafDrops(count: Int, random: RandomSource) = listOf(MaterialCard.SARRACENIA_LEAF.item().createItemStack(count))
     override fun getRareDrops(count: Int, random: RandomSource) = listOf(MaterialCard.FAIRY_SCALES.item().createItemStack(count))
 
-    override val family = MirageFairy2024.identifier("veropeda")
-    override val possibleTraits = setOf(
-        TraitCard.PHOTOSYNTHESIS.trait, // 光合成
-        TraitCard.PHAEOSYNTHESIS.trait, // 闇合成
-        TraitCard.OSMOTIC_ABSORPTION.trait, // 養分吸収
-        TraitCard.COLD_ADAPTATION.trait, // 低温適応
-        TraitCard.WARM_ADAPTATION.trait, // 中温適応
-        TraitCard.HOT_ADAPTATION.trait, // 高温適応
-        TraitCard.ARID_ADAPTATION.trait, // 乾燥適応
-        TraitCard.MESIC_ADAPTATION.trait, // 中湿適応
-        TraitCard.HUMID_ADAPTATION.trait, // 湿潤適応
-        TraitCard.SEEDS_PRODUCTION.trait, // 種子生成
-        TraitCard.FRUITS_PRODUCTION.trait, // 果実生成
-        TraitCard.LEAVES_PRODUCTION.trait, // 葉面生成
-        TraitCard.RARE_PRODUCTION.trait, // 希少品生成
-        TraitCard.EXPERIENCE_PRODUCTION.trait, // 経験値生成
-        TraitCard.FOUR_LEAFED.trait, // 四つ葉
-        TraitCard.FRUIT_OF_KNOWLEDGE.trait, // 禁断の果実
-        TraitCard.GOLDEN_APPLE.trait, // 金のリンゴ
-        TraitCard.SPINY_LEAVES.trait, // 棘のある葉
-        TraitCard.ADVERSITY_FLOWER.trait, // 高嶺の花
-        TraitCard.FLESHY_LEAVES.trait, // 肉厚の葉
-        TraitCard.NATURAL_ABSCISSION.trait, // 自然落果
-        TraitCard.CARNIVOROUS_PLANT.trait, // 食虫植物
-        TraitCard.PAVEMENT_FLOWERS.trait, // アスファルトに咲く花
-        TraitCard.PROSPERITY_OF_SPECIES.trait, // 種の繁栄
-        TraitCard.CROSSBREEDING.trait, // 交雑
-    )
+    override val possibleTraits = super.possibleTraits + setOf()
 
     val SARRACENIA_CLUSTER_CONFIGURED_FEATURE_KEY = Registries.CONFIGURED_FEATURE with MirageFairy2024.identifier("sarracenia_cluster")
     val SARRACENIA_CLUSTER_PLACED_FEATURE_KEY = Registries.PLACED_FEATURE with MirageFairy2024.identifier("sarracenia_cluster")
