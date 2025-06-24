b245f5469713a1e13db1235afb93c0cec3145286
_ <tacticsrealize2@gmail.com>
2025-06-24 21:01:28 +0900

    creative: ADD: dump_biome_attributes

--- common/src/main/kotlin/miragefairy2024/mod/CommonModule.kt
+++ common/src/main/kotlin/miragefairy2024/mod/CommonModule.kt
@@ -12,16 +12,27 @@ import miragefairy2024.util.AdvancementCardType
 import miragefairy2024.util.EnJa
 import miragefairy2024.util.ItemGroupCard
 import miragefairy2024.util.createItemStack
+import miragefairy2024.util.get
+import miragefairy2024.util.humidityCategory
 import miragefairy2024.util.register
+import miragefairy2024.util.registerClientDebugItem
+import miragefairy2024.util.temperatureCategory
+import miragefairy2024.util.text
+import miragefairy2024.util.toTextureSource
+import miragefairy2024.util.translate
+import miragefairy2024.util.writeAction
+import mirrg.kotlin.hydrogen.join
 import net.fabricmc.fabric.api.recipe.v1.ingredient.CustomIngredient
 import net.fabricmc.fabric.api.recipe.v1.ingredient.CustomIngredientSerializer
 import net.minecraft.core.component.DataComponents
+import net.minecraft.core.registries.Registries
 import net.minecraft.network.RegistryFriendlyByteBuf
 import net.minecraft.network.codec.StreamCodec
 import net.minecraft.world.item.ItemStack
 import net.minecraft.world.item.Items
 import net.minecraft.world.item.alchemy.PotionContents
 import net.minecraft.world.item.alchemy.Potions
+import net.minecraft.world.level.block.Blocks
 
 val mirageFairy2024ItemGroupCard = ItemGroupCard(
     MirageFairy2024.identifier("miragefairy2024"), "MF24KU", "MF24KU",
@@ -49,6 +60,23 @@ fun initCommonModule() {
         val item = itemStack.item as? ModifyItemEnchantmentsHandler ?: return@registerModifyItemEnchantmentsHandler
         item.modifyItemEnchantments(itemStack, mutableItemEnchantments, enchantmentLookup)
     }
+
+    registerClientDebugItem("dump_biome_attributes", Blocks.OAK_SAPLING.toTextureSource(), 0xFFFF00FF.toInt()) { world, player, _, _ ->
+        val lines = mutableListOf<List<String>>()
+        world.registryAccess()[Registries.BIOME].listElements().forEach { biome ->
+            lines += listOf(
+                text { translate(biome.key().location().toLanguageKey("biome")) }.string,
+                "${biome.humidityCategory}",
+                "${biome.temperatureCategory}",
+            )
+        }
+        val table = listOf(
+            listOf("Biome", "Humidity", "Temperature"),
+            *lines.toTypedArray(),
+        )
+        writeAction(player, "dump_biome_attributes.csv", table.map { line -> line.join(",") + "\n" }.join(""))
+    }
+
 }
 
 object WaterBottleIngredient : CustomIngredient {
