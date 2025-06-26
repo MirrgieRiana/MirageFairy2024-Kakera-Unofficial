72339b4e2e3a2ff0297af08959bb09125f670246
_ <tacticsrealize2@gmail.com>
2025-06-26 15:44:27 +0900

    CHANGE: MagicPlantBlock: base humidity bonus

--- common/src/main/kotlin/miragefairy2024/mod/magicplant/MagicPlantBlock.kt
+++ common/src/main/kotlin/miragefairy2024/mod/magicplant/MagicPlantBlock.kt
@@ -108,7 +108,7 @@ abstract class MagicPlantBlock(private val configuration: MagicPlantCard<*>, set
             val temperature = traitEffects[TraitEffectKeyCard.TEMPERATURE.traitEffectKey]
             val humidity = traitEffects[TraitEffectKeyCard.HUMIDITY.traitEffectKey]
             val growthBoost = traitEffects[TraitEffectKeyCard.GROWTH_BOOST.traitEffectKey]
-            val actualGrowthAmount = world.random.randomInt(configuration.baseGrowth * 0.2 * nutrition * temperature * (0.2 + humidity) * (1 + growthBoost) * speed)
+            val actualGrowthAmount = world.random.randomInt(configuration.baseGrowth * 0.2 * nutrition * temperature * humidity * (1 + growthBoost) * speed)
             val newBlockState = getBlockStateAfterGrowth(blockState, actualGrowthAmount)
             if (newBlockState != blockState) {
                 world.setBlock(blockPos, newBlockState, UPDATE_CLIENTS)
