7f57638844aef1373aca1c2aec69ca1282dbcead
_ <tacticsrealize2@gmail.com>
2025-07-08 23:49:10 +0900

    refactor: HaimeviskaBlocks

--- common/src/main/kotlin/miragefairy2024/mod/haimeviska/HaimeviskaBlocks.kt
+++ common/src/main/kotlin/miragefairy2024/mod/haimeviska/HaimeviskaBlocks.kt
@@ -77,6 +77,7 @@ class HaimeviskaBlockCard(
     val poemList: PoemList,
     blockCreator: suspend () -> Block,
     val initializer: context(ModContext)(HaimeviskaBlockCard) -> Unit,
+    val extraInitializer: context(ModContext) HaimeviskaBlockCard.() -> Unit,
 ) {
     companion object {
         val entries = mutableListOf<HaimeviskaBlockCard>()
@@ -86,103 +87,192 @@ class HaimeviskaBlockCard(
             "haimeviska_leaves", EnJa("Haimeviska Leaves", "ハイメヴィスカの葉"),
             PoemList(1).poem(EnJa("All original flowers are consumed by ivy", "妖精になれる花、なれない花。")),
             { HaimeviskaLeavesBlock(createLeavesSettings()) }, ::initLeavesHaimeviskaBlock,
-        )
+        ) {
+            block.registerLootTableGeneration { it, _ ->
+                it.createLeavesDrops(block(), SAPLING.block(), 0.05F / 4F, 0.0625F / 4F, 0.083333336F / 4F, 0.1F / 4F)
+            }
+        }
         val LOG = !HaimeviskaBlockCard(
             "haimeviska_log", EnJa("Haimeviska Log", "ハイメヴィスカの原木"),
             PoemList(1)
                 .poem(EnJa("Symbiosis with parasitic Mirages", "妖精の滲み込んだ樹。"))
                 .description(EnJa("Can be incised with a sword", "剣を使って傷を付けられる")),
             { HaimeviskaLogBlock(createLogSettings()) }, initLogHaimeviskaBlock(null),
-        )
+        ) {
+            block.registerDefaultLootTableGeneration()
+        }
         val WOOD = !HaimeviskaBlockCard(
             "haimeviska_wood", EnJa("Haimeviska Wood", "ハイメヴィスカの木"),
             PoemList(1).poem(EnJa("Hydraulic communication system", "ウィスプたちの集合知。")),
             { PillarBlock(createLogSettings(wood = true)) }, initLogHaimeviskaBlock({ LOG }, wood = true),
-        )
+        ) {
+            block.registerDefaultLootTableGeneration()
+        }
         val STRIPPED_LOG = !HaimeviskaBlockCard(
             "stripped_haimeviska_log", EnJa("Stripped Haimeviska Log", "樹皮を剥いだハイメヴィスカの原木"),
             PoemList(1).poem(EnJa("Something lacking the essence", "ぬぐわれたペルソナ。")),
             { PillarBlock(createLogSettings(stripped = true)) }, initLogHaimeviskaBlock(null, stripped = true),
-        )
+        ) {
+            block.registerDefaultLootTableGeneration()
+        }
         val STRIPPED_WOOD = !HaimeviskaBlockCard(
             "stripped_haimeviska_wood", EnJa("Stripped Haimeviska Wood", "樹皮を剥いだハイメヴィスカの木"),
             PoemList(1).poem(EnJa("Loss of self", "寄生蔦からの解放。")),
             { PillarBlock(createLogSettings(stripped = true, wood = true)) }, initLogHaimeviskaBlock({ STRIPPED_LOG }, stripped = true, wood = true),
-        )
+        ) {
+            block.registerDefaultLootTableGeneration()
+        }
         val INCISED_LOG = !HaimeviskaBlockCard(
             "incised_haimeviska_log", EnJa("Incised Haimeviska Log", "傷の付いたハイメヴィスカの原木"),
             PoemList(1)
                 .poem(EnJa("Do fairy trees have qualia of pain?", "動物を守るということ。"))
                 .description(EnJa("Produces sap over time", "時間経過で樹液を生産")),
             { IncisedHaimeviskaLogBlock(createSpecialLogSettings()) }, ::initHorizontalFacingLogHaimeviskaBlock,
-        )
+        ) {
+            block.registerLootTableGeneration { provider, _ ->
+                LootTable(
+                    LootPool(ItemLootPoolEntry(item())) {
+                        `when`(provider.hasSilkTouch())
+                    },
+                    LootPool(ItemLootPoolEntry(LOG.item())) {
+                        `when`(provider.doesNotHaveSilkTouch())
+                    },
+                ) {
+                    provider.applyExplosionDecay(block(), this)
+                }
+            }
+        }
         val DRIPPING_LOG = !HaimeviskaBlockCard(
             "dripping_haimeviska_log", EnJa("Dripping Haimeviska Log", "滴るハイメヴィスカの原木"),
             PoemList(1)
                 .poem(EnJa("A spirit named 'glucose'", "霊界より降りしもの。"))
                 .description(EnJa("Harvest sap when used", "使用時、樹液を収穫")),
             { DrippingHaimeviskaLogBlock(createSpecialLogSettings()) }, ::initHorizontalFacingLogHaimeviskaBlock,
-        )
+        ) {
+            block.registerLootTableGeneration { provider, registries ->
+                LootTable(
+                    LootPool(ItemLootPoolEntry(item())) {
+                        `when`(provider.hasSilkTouch())
+                    },
+                    LootPool(ItemLootPoolEntry(LOG.item())) {
+                        `when`(provider.doesNotHaveSilkTouch())
+                    },
+                    LootPool(ItemLootPoolEntry(MaterialCard.HAIMEVISKA_SAP.item()) {
+                        apply(ApplyBonusLootFunction.addUniformBonusCount(registries[Registries.ENCHANTMENT, Enchantments.FORTUNE]))
+                    }) {
+                        `when`(provider.doesNotHaveSilkTouch())
+                    },
+                    LootPool(ItemLootPoolEntry(MaterialCard.HAIMEVISKA_ROSIN.item()) {
+                        apply(ApplyBonusLootFunction.addUniformBonusCount(registries[Registries.ENCHANTMENT, Enchantments.FORTUNE], 2))
+                    }) {
+                        `when`(provider.doesNotHaveSilkTouch())
+                        `when`(RandomChanceLootCondition.randomChance(0.01F))
+                    },
+                ) {
+                    provider.applyExplosionDecay(block(), this)
+                }
+            }
+        }
         val HOLLOW_LOG = !HaimeviskaBlockCard(
             "hollow_haimeviska_log", EnJa("Hollow Haimeviska Log", "ハイメヴィスカの樹洞"),
             PoemList(1).poem(EnJa("Auric conceptual attractor", "限界巡回アステリア。")),
             { HollowHaimeviskaLogBlock(createSpecialLogSettings()) }, ::initHorizontalFacingLogHaimeviskaBlock,
-        )
+        ) {
+            block.registerLootTableGeneration { provider, registries ->
+                LootTable(
+                    LootPool(ItemLootPoolEntry(item())) {
+                        `when`(provider.hasSilkTouch())
+                    },
+                    LootPool(ItemLootPoolEntry(LOG.item())) {
+                        `when`(provider.doesNotHaveSilkTouch())
+                    },
+                    LootPool(ItemLootPoolEntry(MaterialCard.FRACTAL_WISP.item()) {
+                        apply(ApplyBonusLootFunction.addUniformBonusCount(registries[Registries.ENCHANTMENT, Enchantments.FORTUNE]))
+                    }) {
+                        `when`(provider.doesNotHaveSilkTouch())
+                    },
+                ) {
+                    provider.applyExplosionDecay(block(), this)
+                }
+            }
+        }
         val PLANKS = !HaimeviskaBlockCard(
             "haimeviska_planks", EnJa("Haimeviska Planks", "ハイメヴィスカの板材"),
             PoemList(1).poem(EnJa("Flexible and friendly, good for interior", "考える、壁。")),
             { Block(createPlankSettings()) }, ::initPlanksHaimeviskaBlock,
-        )
+        ) {
+            block.registerDefaultLootTableGeneration()
+        }
         val SLAB = !HaimeviskaBlockCard(
             "haimeviska_slab", EnJa("Haimeviska Slab", "ハイメヴィスカのハーフブロック"),
             PoemList(1).poem(EnJa("Searching for another personality.", "半人前の側頭葉。")),
             { SlabBlock(createPlankSettings()) }, ::initPlanksSlabHaimeviskaBlock,
-        )
+        ) {
+            block.registerLootTableGeneration { it, _ -> it.createSlabItemTable(block()) }
+        }
         val STAIRS = !HaimeviskaBlockCard(
             "haimeviska_stairs", EnJa("Haimeviska Stairs", "ハイメヴィスカの階段"),
             PoemList(1).poem(EnJa("Step that pierces the sky", "情緒体を喰らう頂となれ。")),
             { StairBlock(PLANKS.block.await().defaultBlockState(), createPlankSettings()) }, ::initPlanksStairsHaimeviskaBlock,
-        )
+        ) {
+            block.registerDefaultLootTableGeneration()
+        }
         val FENCE = !HaimeviskaBlockCard(
             "haimeviska_fence", EnJa("Haimeviska Fence", "ハイメヴィスカのフェンス"),
             PoemList(1).poem(EnJa("Personality flowing through the xylem", "樹のなかに住む。")),
             { FenceBlock(createPlankSettings()) }, ::initPlanksFenceHaimeviskaBlock,
-        )
+        ) {
+            block.registerDefaultLootTableGeneration()
+        }
         val FENCE_GATE = !HaimeviskaBlockCard(
             "haimeviska_fence_gate", EnJa("Haimeviska Fence Gate", "ハイメヴィスカのフェンスゲート"),
             PoemList(1).poem(EnJa("It chose this path of its own will", "知性の邂逅。")),
             { FenceGateBlock(HAIMEVISKA_WOOD_TYPE, createPlankSettings(sound = false).forceSolidOn()) }, ::initPlanksFenceGateHaimeviskaBlock,
-        )
+        ) {
+            block.registerDefaultLootTableGeneration()
+        }
         val BUTTON = !HaimeviskaBlockCard(
             "haimeviska_button", EnJa("Haimeviska Button", "ハイメヴィスカのボタン"),
             PoemList(1).poem(EnJa("What is this soft and warm thing?", "指先の感触。")),
             { ButtonBlock(HAIMEVISKA_BLOCK_SET_TYPE, 30, AbstractBlock.Properties.of().noCollission().strength(0.5F).pushReaction(PistonBehavior.DESTROY)) }, ::initPlanksButtonHaimeviskaBlock,
-        )
+        ) {
+            block.registerDefaultLootTableGeneration()
+        }
         val PRESSURE_PLATE = !HaimeviskaBlockCard(
             "haimeviska_pressure_plate", EnJa("Haimeviska Pressure Plate", "ハイメヴィスカの感圧板"),
             PoemList(1).poem(EnJa("Creature with the name of a machine", "感応と感覚の違い。")),
             { PressurePlateBlock(HAIMEVISKA_BLOCK_SET_TYPE, createBaseWoodSetting(sound = false).forceSolidOn().noCollission().strength(0.5F).pushReaction(PistonBehavior.DESTROY)) }, ::initPlanksPressurePlateHaimeviskaBlock,
-        )
+        ) {
+            block.registerDefaultLootTableGeneration()
+        }
         val BRICKS = !HaimeviskaBlockCard(
             "haimeviska_bricks", EnJa("Haimeviska Bricks", "ハイメヴィスカレンガ"),
             PoemList(1).poem(EnJa("An ecosystem called 'civilization'", "人がもたらした原生林。")),
             { Block(createPlankSettings()) }, ::initPlanksHaimeviskaBlock,
-        )
+        ) {
+            block.registerDefaultLootTableGeneration()
+        }
         val BRICKS_SLAB = !HaimeviskaBlockCard(
             "haimeviska_bricks_slab", EnJa("Haimeviska Bricks Slab", "ハイメヴィスカレンガのハーフブロック"),
             PoemList(1).poem(EnJa("Extremely modularized memory", "ひとまわり細かくなった私。")),
             { SlabBlock(createPlankSettings()) }, ::initPlanksSlabHaimeviskaBlock,
-        )
+        ) {
+            block.registerLootTableGeneration { it, _ -> it.createSlabItemTable(block()) }
+        }
         val BRICKS_STAIRS = !HaimeviskaBlockCard(
             "haimeviska_bricks_stairs", EnJa("Haimeviska Bricks Stairs", "ハイメヴィスカレンガの階段"),
             PoemList(1).poem(EnJa("Forgotten paths of the technology", "生体工学の歩み。")),
             { StairBlock(BRICKS.block.await().defaultBlockState(), createPlankSettings()) }, ::initPlanksStairsHaimeviskaBlock,
-        )
+        ) {
+            block.registerDefaultLootTableGeneration()
+        }
         val SAPLING = !HaimeviskaBlockCard(
             "haimeviska_sapling", EnJa("Haimeviska Sapling", "ハイメヴィスカの苗木"),
             PoemList(1).poem(EnJa("Assembling molecules with Ergs", "第二の葉緑体。")),
             { SaplingBlock(createTreeGrower(MirageFairy2024.identifier("haimeviska_sapling")), createSaplingSettings()) }, ::initSaplingHaimeviskaBlock,
-        )
+        ) {
+            block.registerDefaultLootTableGeneration()
+        }
     }
 
     val identifier = MirageFairy2024.identifier(path)
@@ -226,6 +316,7 @@ fun initHaimeviskaBlocks() {
         card.item.registerPoemGeneration(card.poemList)
 
         card.initializer(this@ModContext, card)
+        card.extraInitializer(this@ModContext, card)
     }
 
     run {
@@ -260,78 +351,6 @@ fun initHaimeviskaBlocks() {
             RecipeProvider.generateRecipes(it, family, FeatureFlagSet.of(FeatureFlags.VANILLA))
         }
     }
-
-    // ドロップ
-    HaimeviskaBlockCard.LEAVES.block.registerLootTableGeneration { it, _ ->
-        it.createLeavesDrops(HaimeviskaBlockCard.LEAVES.block(), HaimeviskaBlockCard.SAPLING.block(), 0.05F / 4F, 0.0625F / 4F, 0.083333336F / 4F, 0.1F / 4F)
-    }
-    HaimeviskaBlockCard.LOG.block.registerDefaultLootTableGeneration()
-    HaimeviskaBlockCard.STRIPPED_LOG.block.registerDefaultLootTableGeneration()
-    HaimeviskaBlockCard.WOOD.block.registerDefaultLootTableGeneration()
-    HaimeviskaBlockCard.STRIPPED_WOOD.block.registerDefaultLootTableGeneration()
-    HaimeviskaBlockCard.INCISED_LOG.block.registerLootTableGeneration { provider, _ ->
-        LootTable(
-            LootPool(ItemLootPoolEntry(HaimeviskaBlockCard.INCISED_LOG.item())) {
-                `when`(provider.hasSilkTouch())
-            },
-            LootPool(ItemLootPoolEntry(HaimeviskaBlockCard.LOG.item())) {
-                `when`(provider.doesNotHaveSilkTouch())
-            },
-        ) {
-            provider.applyExplosionDecay(HaimeviskaBlockCard.INCISED_LOG.block(), this)
-        }
-    }
-    HaimeviskaBlockCard.DRIPPING_LOG.block.registerLootTableGeneration { provider, registries ->
-        LootTable(
-            LootPool(ItemLootPoolEntry(HaimeviskaBlockCard.DRIPPING_LOG.item())) {
-                `when`(provider.hasSilkTouch())
-            },
-            LootPool(ItemLootPoolEntry(HaimeviskaBlockCard.LOG.item())) {
-                `when`(provider.doesNotHaveSilkTouch())
-            },
-            LootPool(ItemLootPoolEntry(MaterialCard.HAIMEVISKA_SAP.item()) {
-                apply(ApplyBonusLootFunction.addUniformBonusCount(registries[Registries.ENCHANTMENT, Enchantments.FORTUNE]))
-            }) {
-                `when`(provider.doesNotHaveSilkTouch())
-            },
-            LootPool(ItemLootPoolEntry(MaterialCard.HAIMEVISKA_ROSIN.item()) {
-                apply(ApplyBonusLootFunction.addUniformBonusCount(registries[Registries.ENCHANTMENT, Enchantments.FORTUNE], 2))
-            }) {
-                `when`(provider.doesNotHaveSilkTouch())
-                `when`(RandomChanceLootCondition.randomChance(0.01F))
-            },
-        ) {
-            provider.applyExplosionDecay(HaimeviskaBlockCard.DRIPPING_LOG.block(), this)
-        }
-    }
-    HaimeviskaBlockCard.HOLLOW_LOG.block.registerLootTableGeneration { provider, registries ->
-        LootTable(
-            LootPool(ItemLootPoolEntry(HaimeviskaBlockCard.HOLLOW_LOG.item())) {
-                `when`(provider.hasSilkTouch())
-            },
-            LootPool(ItemLootPoolEntry(HaimeviskaBlockCard.LOG.item())) {
-                `when`(provider.doesNotHaveSilkTouch())
-            },
-            LootPool(ItemLootPoolEntry(MaterialCard.FRACTAL_WISP.item()) {
-                apply(ApplyBonusLootFunction.addUniformBonusCount(registries[Registries.ENCHANTMENT, Enchantments.FORTUNE]))
-            }) {
-                `when`(provider.doesNotHaveSilkTouch())
-            },
-        ) {
-            provider.applyExplosionDecay(HaimeviskaBlockCard.HOLLOW_LOG.block(), this)
-        }
-    }
-    HaimeviskaBlockCard.PLANKS.block.registerDefaultLootTableGeneration()
-    HaimeviskaBlockCard.SLAB.block.registerLootTableGeneration { it, _ -> it.createSlabItemTable(HaimeviskaBlockCard.SLAB.block()) }
-    HaimeviskaBlockCard.STAIRS.block.registerDefaultLootTableGeneration()
-    HaimeviskaBlockCard.FENCE.block.registerDefaultLootTableGeneration()
-    HaimeviskaBlockCard.FENCE_GATE.block.registerDefaultLootTableGeneration()
-    HaimeviskaBlockCard.BUTTON.block.registerDefaultLootTableGeneration()
-    HaimeviskaBlockCard.PRESSURE_PLATE.block.registerDefaultLootTableGeneration()
-    HaimeviskaBlockCard.BRICKS.block.registerDefaultLootTableGeneration()
-    HaimeviskaBlockCard.BRICKS_SLAB.block.registerLootTableGeneration { it, _ -> it.createSlabItemTable(HaimeviskaBlockCard.BRICKS_SLAB.block()) }
-    HaimeviskaBlockCard.BRICKS_STAIRS.block.registerDefaultLootTableGeneration()
-    HaimeviskaBlockCard.SAPLING.block.registerDefaultLootTableGeneration()
 
     // レシピ
     HaimeviskaBlockCard.LEAVES.item.registerComposterInput(0.3F)
