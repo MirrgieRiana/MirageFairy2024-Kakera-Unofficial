8dbe9bc074c2c1235bfcb56e962f232323451a12
_ <tacticsrealize2@gmail.com>
2025-08-05 07:47:57 +0900

    cnealnup: remove: ITEMS_CODEC, ITEMS_STREAM_CODEC

--- common/src/main/kotlin/miragefairy2024/util/Codec.kt
+++ common/src/main/kotlin/miragefairy2024/util/Codec.kt
@@ -11,7 +11,6 @@ import net.minecraft.network.codec.ByteBufCodecs
 import net.minecraft.network.codec.StreamCodec
 import net.minecraft.resources.ResourceKey
 import net.minecraft.world.item.ItemStack
-import net.minecraft.world.item.component.ItemContainerContents
 import java.time.Instant
 import java.util.Optional
 
@@ -21,8 +20,6 @@ fun <B : ByteBuf, V> StreamCodec<B, V>.list(): StreamCodec<B, List<V>> = this.ap
 val INSTANT_CODEC: Codec<Instant> = Codec.LONG.xmap(Instant::ofEpochMilli, Instant::toEpochMilli)
 val INSTANT_STREAM_CODEC: StreamCodec<ByteBuf, Instant> = ByteBufCodecs.VAR_LONG.map(Instant::ofEpochMilli, Instant::toEpochMilli)
 
-val ITEMS_CODEC: Codec<List<ItemStack>> = ItemContainerContents.CODEC.xmap({ it.stream().toList() }, { ItemContainerContents.fromItems(it) })
-val ITEMS_STREAM_CODEC: StreamCodec<RegistryFriendlyByteBuf, List<ItemStack>> = ItemContainerContents.STREAM_CODEC.map({ it.stream().toList() }, { ItemContainerContents.fromItems(it) })
 
 data class ItemStacks(val itemStacks: List<ItemStack>) {
     companion object {
