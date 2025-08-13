daf693bf931a4f2868cd6c9451d957ae86721438
_ <tacticsrealize2@gmail.com>
2025-08-13 12:38:52 +0900

    FIX: Attachment: ログイン時・ディメンション変更時にAttachmentが消えるバグ

--- common/src/main/kotlin/miragefairy2024/util/Attachment.kt
+++ common/src/main/kotlin/miragefairy2024/util/Attachment.kt
@@ -3,6 +3,8 @@ package miragefairy2024.util
 import miragefairy2024.ModContext
 import net.fabricmc.fabric.api.attachment.v1.AttachmentTarget
 import net.fabricmc.fabric.api.attachment.v1.AttachmentType
+import net.fabricmc.fabric.api.entity.event.v1.ServerEntityWorldChangeEvents
+import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents
 
 operator fun <T> AttachmentTarget.get(type: AttachmentType<T>) = AttachmentDelegate(this, type)
 
@@ -21,7 +23,11 @@ fun <T> AttachmentDelegate<T>.getOrCreate(): T {
 }
 
 fun <T> AttachmentDelegate<T>.set(data: T?) {
-    this.value.setAttached(this.type, data)
+    if (data != null) {
+        this.value.setAttached(this.type, data)
+    } else {
+        this.value.removeAttached(this.type)
+    }
 }
 
 fun <T> AttachmentDelegate<T>.modify(block: (T?) -> T?) {
@@ -42,4 +48,17 @@ fun <T> AttachmentDelegate<T>.sync() {
 
 
 context(ModContext)
-fun AttachmentType<*>.register() = this // AttachmentTypeはインスタンス化した時点で登録成立
+fun AttachmentType<*>.register() {
+    // AttachmentTypeはインスタンス化した時点で登録成立
+
+    if (this.isSynced) {
+        ServerPlayConnectionEvents.JOIN.register { handler, _, server ->
+            server.execute {
+                AttachmentDelegate(handler.player, this).sync()
+            }
+        }
+        ServerEntityWorldChangeEvents.AFTER_PLAYER_CHANGE_WORLD.register { player, _, _ ->
+            AttachmentDelegate(player, this).sync()
+        }
+    }
+}
