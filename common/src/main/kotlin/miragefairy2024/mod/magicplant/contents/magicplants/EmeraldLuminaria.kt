7c1eb274275e7930cfddd26d5633397010ae78ad
_ <tacticsrealize2@gmail.com>
2025-08-14 13:58:13 +0900

    CHANGE: EmeraldLuminaria: spawn rate

--- common/src/main/kotlin/miragefairy2024/mod/magicplant/contents/magicplants/EmeraldLuminaria.kt
+++ common/src/main/kotlin/miragefairy2024/mod/magicplant/contents/magicplants/EmeraldLuminaria.kt
@@ -90,7 +90,7 @@ object EmeraldLuminariaCard : AbstractLuminariaCard<EmeraldLuminariaBlock>() {
         Feature.FLOWER {
             configuredFeature("cluster", { RandomPatchFeatureConfig(1, 0, 0, PlacedFeatures.onlyWhenEmpty(Feature.SIMPLE_BLOCK, SimpleBlockFeatureConfig(it))) }) {
                 placedFeature("cluster", { per(128) + flower(square, surface) }) { +ConventionalBiomeTags.IS_JUNGLE + +ConventionalBiomeTags.IS_SWAMP }  // 地上
-                placedFeature("underground_cluster", { count(32) + flower(square, underground) }) { +Biomes.LUSH_CAVES } // 地下
+                placedFeature("underground_cluster", { count(16) + flower(square, underground) }) { +Biomes.LUSH_CAVES } // 地下
             }
         }
     }
